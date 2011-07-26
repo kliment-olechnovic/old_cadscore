c65f325267b3a987e6a126e7467276d1fc43b56a
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-07-26 15:31:20 +0300

    Refactored collision detection. Added floating point handler.

--- src/SpheresCollisionDetection.h
+++ src/SpheresCollisionDetection.h
@@ -1,18 +1,21 @@
 #ifndef SPHERESCOLLISIONDETECTION_H_
 #define SPHERESCOLLISIONDETECTION_H_
 
+template<typename SpheresMapType>
 class SpheresCollisionDetection
 {
 public:
-	template<typename SpheresMapType>
-	static bool check_for_collision(const SpheresMapType& spheres_map, const typename SpheresMapType::Sphere& sphere, bool only_containment)
+	typedef SpheresMapType SpheresMap;
+	typedef typename SpheresMap::Sphere Sphere;
+	typedef typename SpheresMap::Set Set;
+
+	static bool check_for_collision(const SpheresMap& spheres_map, const Sphere& sphere, bool only_containment)
 	{
-		typedef typename SpheresMapType::Set Set;
 		const Set candidates=spheres_map.find_potential_intersections(sphere);
 		for(typename Set::const_iterator it=candidates.begin();it!=candidates.end();it++)
 		{
 			const typename SpheresMapType::Sphere* s=(*it);
-			if(only_containment ? s->contains(sphere) : s->distance_to(sphere)<0)
+			if(only_containment ? s->contains(sphere) : s->intersects_with(sphere)<0)
 			{
 				return true;
 			}
@@ -20,13 +23,13 @@ public:
 		return false;
 	}
 
-	template<typename SpheresMapType, typename SpheresContainerType>
-	static SpheresContainerType filter_colliders(const SpheresMapType& spheres_map, const SpheresContainerType& candidates, bool only_containment)
+	template<typename SpheresContainerType>
+	static Set filter_colliders(const SpheresMap& spheres_map, const SpheresContainerType& candidates, bool only_containment)
 	{
-		SpheresContainerType result;
+		Set result;
 		for(typename SpheresContainerType::const_iterator it=candidates.begin();it!=candidates.end();it++)
 		{
-			const typename SpheresMapType::Sphere* s=(*it);
+			const Sphere* s=(*it);
 			if(check_for_any_collision(spheres_map, (*s), only_containment))
 			{
 				result.insert(s);
