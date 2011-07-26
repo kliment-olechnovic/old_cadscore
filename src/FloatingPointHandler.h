27f451cd140b6e30744c840abac6caeb056c0bca
kliment <kliment@localhost>
2011-07-26 15:31:20 +0300

    Refactored collision detection. Added floating point handler.

---
+++ src/FloatingPointHandler.h
@@ -0,0 +1,28 @@
+#ifndef FLOATINGPOINTHANDLER_H_
+#define FLOATINGPOINTHANDLER_H_
+
+class FloatingPointHandler
+{
+public:
+	static double epsilon()
+	{
+		return 0.00000001;
+	}
+
+	static bool equal(double a, double b)
+	{
+		return (((a-b)<epsilon()) && ((b-a)<epsilon()));
+	}
+
+	static bool less(double a, double b)
+	{
+		return ((a+epsilon())<b);
+	}
+
+	static bool greater(double a, double b)
+	{
+		return ((a-epsilon())>b);
+	}
+};
+
+#endif /* FLOATINGPOINTHANDLER_H_ */
---
+++ src/Sphere.h
@@ -0,0 +1,10 @@
+#ifndef SPHERE_H_
+#define SPHERE_H_
+
+class Sphere
+{
+public:
+private:
+};
+
+#endif /* SPHERE_H_ */
