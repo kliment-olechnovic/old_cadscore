43091eff406c29cbdfa598f1d3b512330c48cd05
kliment <kliment@localhost>
2011-07-27 15:01:43 +0300

    Tested collision detection compilation.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@ struct TestSphere
 	double y() const { return 1; }
 	double z() const { return 1; }
 	double r() const { return 1; }
+	bool intersects_with(const TestSphere& s) const { return true; }
+	bool contains(const TestSphere& s) const { return true; }
 };
 
 int main()
@@ -16,6 +18,7 @@ int main()
 	TestSphere ts;
 	sm.add(&ts);
 	sm.find_potential_intersections(ts);
+	SpheresCollisionDetection< SpheresMap<TestSphere> >::check_for_collision(sm, ts, false);
 	sm.remove(&ts);
 	std::cout << "Hi!\n";
 	return 0;
