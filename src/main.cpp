e0fedae890af48f41c37fee26aab32a4e679d9de
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-08-25 18:41:30 +0300

    Added emmental class.

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
-#include "StandardTypeProxies.h"
-#include "SpheresMap.h"
+#include <cstdlib>
 
-template<typename FloatingPointNumberType>
-struct TestSphere
+#include "standard_container_types_proxies.h"
+#include "Emmental.h"
+
+struct Sphere
 {
-	typedef FloatingPointNumberType FloatType;
-
-	double x() const { return 1; }
-	double y() const { return 1; }
-	double z() const { return 1; }
-	double r() const { return 1; }
+	double x;
+	double y;
+	double z;
+	double r;
 };
 
 int main()
 {
-	SpheresMap< TestSphere<double>, StandardTypeProxies::UnorderedSetTypeProxy, StandardTypeProxies::OrderedMapTypeProxy > sm(1.0, 1.0, 10.0);
-	TestSphere<double> ts;
-	sm.add(&ts);
-	sm.find_neighbours(ts);
-	sm.remove(&ts);
-	std::cout << "Hi!\n";
+	Emmental<Sphere, double, UnorderedMapTypeProxy, ListTypeProxy, UnorderedSetTypeProxy> emmental(0.5);
+	std::vector<Sphere> spheres;
+	const int N=50000;
+	spheres.reserve(N);
+	for(int i=0;i<N;i++)
+	{
+		Sphere s;
+		s.x=(rand()%100);
+		s.y=(rand()%100);
+		s.z=(rand()%100);
+		s.r=1.4;
+		spheres.push_back(s);
+		emmental.add(&spheres[i]);
+	}
+	for(int i=0;i<N*10;i++)
+	{
+		Sphere s=spheres[i%N];
+		s.r*=2;
+		emmental.intersect(s);
+	}
 	return 0;
 }
