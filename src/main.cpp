fa6c59a7c960b957a21e4ef3b94f5b2b9ca234ae
kliment <kliment@localhost>
2011-08-31 16:53:45 +0300

    Added advanced tuple class.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <cstdlib>
 
-#include "atoms/atoms.h"
+//#include "atoms/atoms.h"
 
 #include "../../contactus/src/baltymus/utils/BlockTimer.h"
 
+#include "apollo/tuples.h"
+
 int main()
 {
-	typedef Atom Sphere;
+//	typedef Atom Sphere;
+//	std::vector<Sphere> spheres=read_atoms_from_PDB_file_stream(std::cin);
+//	std::cout << spheres.size() << " spheres read\n";
 
-	std::vector<Sphere> spheres=read_atoms_from_PDB_file_stream(std::cin);
-	std::cout << spheres.size() << " spheres read\n";
+	Quadruple q(make_triple(3,1,2),0);
+	std::cout << q.exclude(0).str() << "\n";
 
 	return 0;
 }
