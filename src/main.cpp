4d2160cf19fc5c0ee2eff6ab73e3dbd405e104b1
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-09-01 16:25:01 +0300

    Added distances graph construction.

--- src/main.cpp
+++ src/main.cpp
@@ -2,42 +2,69 @@
 #include <cstdlib>
 #include <tr1/unordered_map>
 #include <map>
+#include <cmath>
 
-//#include "atoms/atoms.h"
-
+#include "atoms/atoms.h"
 #include "../../contactus/src/baltymus/utils/BlockTimer.h"
 
-#include "apollo/tuples.h"
+template<typename ContainerType, typename OperatorType>
+std::vector< std::vector<std::size_t> > construct_graph(const ContainerType& objects, const OperatorType& op)
+{
+	std::vector< std::vector<std::size_t> > graph(objects.size());
+	for(std::size_t i=0;i<objects.size();i++)
+	{
+		for(std::size_t j=i+1;j<objects.size();j++)
+		{
+			if(op(objects[i], objects[j]))
+			{
+				graph[i].push_back(j);
+				graph[j].push_back(i);
+			}
+		}
+	}
+	return graph;
+}
+
+template<typename SphereType>
+struct DistanceBetweenSpheres
+{
+	typedef SphereType ObjectType;
+
+	double operator()(const SphereType& a, const SphereType& b) const
+	{
+		const double dx=(a.x-b.x);
+		const double dy=(a.y-b.y);
+		const double dz=(a.z-b.z);
+		return (sqrt(dx*dx+dy*dy+dz*dz)-a.r-b.r);
+	}
+};
+
+template<typename OperatorType>
+struct TresholdChecker
+{
+	typedef typename OperatorType::ObjectType ObjectType;
+
+	TresholdChecker(const double treshold) : treshold(treshold)
+	{
+	}
+
+	double operator()(const ObjectType& a, const ObjectType& b) const
+	{
+		return (OperatorType()(a, b)<=treshold);
+	}
+
+	double treshold;
+};
 
 int main()
 {
-//	typedef Atom Sphere;
-//	std::vector<Sphere> spheres=read_atoms_from_PDB_file_stream(std::cin);
-//	std::cout << spheres.size() << " spheres read\n";
+	typedef Atom Sphere;
+	std::vector<Sphere> spheres=read_atoms_from_PDB_file_stream(std::cin);
+	std::cout << spheres.size() << " spheres read\n";
 
 	utils::BlockTimer bt("Time");
 
-	typedef Quadruple Element;
-
-	typedef std::tr1::unordered_map< Element, int, TupleHashOperator<Element> >Map;
-//	typedef std::map< Element, int > Map;
-	Map map;
-	for(int i=0;i<1099900;i++)
-	{
-		Element t=make_quadruple(rand()%1000,rand()%1000,rand()%1000,rand()%1000);
-		map[t]=i;
-	}
-
-//	for(Map::const_iterator it=map.begin();it!=map.end();++it)
-//	{
-//		std::cout << it->first.str() << "\n";
-//	}
-
-	Element a=make_quadruple(10,20,2,1);
-	Element b=a;
-	a.set(0, 5);
-	std::cout << a.str() << "\n";
-	std::cout << b.str() << "\n";
+	std::vector< std::vector<std::size_t> > graph=construct_graph(spheres, TresholdChecker< DistanceBetweenSpheres<Sphere> >(2.8));
 
 	return 0;
 }
