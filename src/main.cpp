b988a1b9a0fc9910da4cec4e9edfeca98ed657c7
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-09-05 12:09:31 +0300

    Added bounding spheres construction.

--- src/main.cpp
+++ src/main.cpp
@@ -68,6 +68,39 @@ void print_spheres(const Iterator first, const Iterator last)
 	}
 }
 
+template<typename SphereType>
+double special_distance_between_spheres(const SphereType& a, const SphereType& b)
+{
+	const double dx=(a.x-b.x);
+	const double dy=(a.y-b.y);
+	const double dz=(a.z-b.z);
+	return (sqrt(dx*dx+dy*dy+dz*dz)+b.r);
+}
+
+template<typename SphereType>
+std::vector<SphereType> form_clusters(const std::vector<SphereType>& spheres, const std::vector<size_t> centers)
+{
+	std::vector<SphereType> clusters;
+	for(std::size_t i=0;i<centers.size();i++)
+	{
+		clusters.push_back(spheres[centers[i]]);
+	}
+	for(std::size_t i=0;i<spheres.size();i++)
+	{
+		std::size_t min_dist_j=0;
+		for(std::size_t j=1;j<clusters.size();j++)
+		{
+			if( special_distance_between_spheres(clusters[j], spheres[i]) <
+					special_distance_between_spheres(clusters[min_dist_j], spheres[i]))
+			{
+				min_dist_j=j;
+			}
+		}
+		clusters[min_dist_j].r=std::max(clusters[min_dist_j].r, special_distance_between_spheres(clusters[min_dist_j], spheres[i]));
+	}
+	return clusters;
+}
+
 int main()
 {
 	typedef Atom Sphere;
@@ -83,19 +116,13 @@ int main()
 
 	{
 		utils::BlockTimer bt("Bucketing time");
-		std::vector<std::size_t> buckets=subdivide_graph(graph);
+		std::vector<std::size_t> buckets=subdivide_graph(graph, 2);
 		std::clog << buckets.size() << " buckets\n";
 
-		std::vector<Sphere> centers;
-		for(std::size_t i=0;i<buckets.size();i++)
-		{
-			Sphere s=spheres[buckets[i]];
-			s.r+=2.8*2;
-			centers.push_back(s);
-		}
+		std::vector<Sphere> centers=form_clusters(spheres, buckets);
 		std::cout << "LIST\n";
 		print_spheres(centers.begin(), centers.end());
-		print_spheres(spheres.begin(), spheres.end());
+//		print_spheres(spheres.begin(), spheres.end());
 	}
 
 	return 0;
