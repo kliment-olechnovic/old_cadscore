8882e9fdcf906f8d3be30a05bc3de8036a05ead0
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-09-08 15:24:39 +0300

    Refactored, used tr1::bind.

--- src/apollo/distances.h
+++ src/apollo/distances.h
@@ -54,24 +54,6 @@ bool sphere_contains_sphere(const SphereType& a, const SphereType& b)
 	return less(maximal_distance_from_point_to_sphere(a,b), a.r);
 }
 
-template<typename FirstType, typename SecondType, typename DistanceFunctor>
-std::vector<std::size_t> sort_objects_by_distances(const FirstType& a, const SecondType& b, const DistanceFunctor& distance_functor)
-{
-	std::vector< std::pair<double, std::size_t> > distances;
-	distances.reserve(b.size());
-	for(std::size_t i=0;i<b.size();i++)
-	{
-		distances.push_back(std::make_pair(distance_functor(a, b[i]), i));
-	}
-	std::vector<std::size_t> result;
-	result.reserve(distances.size());
-	for(std::size_t i=0;i<distances.size();i++)
-	{
-		result.push_back(distances[i].second);
-	}
-	return result;
-}
-
 }
 
 #endif /* SPHERES_H_ */
