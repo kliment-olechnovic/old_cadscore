e155cc7cc2c5a856ba8869c621b8920acc77eebd
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-09-09 17:08:54 +0300

    Started testing Apollo.

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,18 @@
 
 #include "apollo/apollonius_graph.h"
 
+template<typename SphereType>
+void print_sphere(const SphereType& sphere)
+{
+	std::cout << "SPHERE " << sphere.r << " " << sphere.x << " " << sphere.y << " " << sphere.z << "\n";
+}
+
 template<typename Iterator>
 void print_spheres(const Iterator first, const Iterator last)
 {
 	for(Iterator it=first;it!=last;++it)
 	{
-		std::cout << "SPHERE " << it->r << " " << it->x << " " << it->y << " " << it->z << "\n";
+		print_sphere(*it);
 	}
 }
 
@@ -43,6 +49,18 @@ int main()
 
 //		std::cout << "LIST\n";
 //		print_spheres(clusters_spheres.begin(), clusters_spheres.end());
+	}
+
+	{
+		utils::BlockTimer bt("Apollo time");
+		std::pair< apollo::Quadruple, Sphere > first_quadruple=apollo::apollonius_graph<Sphere>::find_first_quadruple(spheres, 1.4*5, 100);
+
+		std::cout << "LIST\n";
+		print_sphere(first_quadruple.second);
+		for(int i=0;i<4;i++)
+		{
+			print_sphere(spheres[first_quadruple.first.get(i)]);
+		}
 	}
 
 	return 0;
