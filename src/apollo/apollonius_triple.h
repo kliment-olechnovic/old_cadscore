a5a858f8818bff1e66a407813a13975f0b9e9548
kliment <kliment@localhost>
2011-10-03 17:33:18 +0300

    Improved Apollonius triple.

--- src/apollo/apollonius_triple.h
+++ src/apollo/apollonius_triple.h
@@ -15,23 +15,11 @@ class ApolloniusTriple
 {
 public:
 	ApolloniusTriple(const SimpleSphere& a, const SimpleSphere& b, const SimpleSphere& c) :
-		spheres_(make_vector(a, b, c)),
+		spheres_(make_sorted_spheres_vector(a, b, c)),
 		tangent_planes_(construct_tangent_planes(a, b, c)),
-		bounding_sphere_(construct_bounding_sphere(spheres_))
+		bounding_sphere_(construct_bounding_sphere(spheres_)),
+		cone_end_(tangent_planes_.empty() ? find_cone_end(spheres_) : 0)
 	{
-	}
-
-	template<typename InputSphereType>
-	bool sphere_may_contain_doubling_sphere(const InputSphereType& x) const
-	{
-		for(std::size_t i=0;i<tangent_planes_.size();i++)
-		{
-			if(halfspace_of_sphere(tangent_planes_[i].first, tangent_planes_[i].second, x)==1)
-			{
-				return false;
-			}
-		}
-		return true;
 	}
 
 	template<typename InputSphereType>
@@ -39,7 +27,45 @@ public:
 	{
 		if(sphere_intersects_sphere(x, bounding_sphere_))
 		{
-			return sphere_may_contain_doubling_sphere(x);
+			if(!tangent_planes_.empty())
+			{
+				for(std::size_t i=0;i<tangent_planes_.size();i++)
+				{
+					if(halfspace_of_sphere(tangent_planes_[i].first, tangent_planes_[i].second, x)==1)
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+			else
+			{
+				return stick_intersects_sphere(spheres_[0], spheres_[cone_end_], x);
+			}
+		}
+		return false;
+	}
+
+	template<typename InputSphereType>
+	bool sphere_is_inner_sphere(const InputSphereType& x) const
+	{
+		if(sphere_contains_sphere(bounding_sphere_, x))
+		{
+			if(!tangent_planes_.empty())
+			{
+				for(std::size_t i=0;i<tangent_planes_.size();i++)
+				{
+					if(halfspace_of_sphere(tangent_planes_[i].first, tangent_planes_[i].second, x)!=-1)
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+			else
+			{
+				return stick_contains_sphere(spheres_[0], spheres_[cone_end_], x);
+			}
 		}
 		return false;
 	}
@@ -54,14 +80,24 @@ private:
 	std::vector<SimpleSphere> spheres_;
 	std::vector< std::pair<SimplePoint, SimplePoint> > tangent_planes_;
 	SimpleSphere bounding_sphere_;
+	std::size_t cone_end_;
 
-	static std::vector<SimpleSphere> make_vector(const SimpleSphere& a, const SimpleSphere& b, const SimpleSphere& c)
+	struct SpheresSortingOperator
+	{
+		 bool operator()(const SimpleSphere& a, const SimpleSphere& b) const
+		 {
+			 return (a.r>b.r);
+		 }
+	};
+
+	static std::vector<SimpleSphere> make_sorted_spheres_vector(const SimpleSphere& a, const SimpleSphere& b, const SimpleSphere& c)
 	{
 		std::vector<SimpleSphere> spheres;
 		spheres.reserve(3);
 		spheres.push_back(a);
 		spheres.push_back(b);
 		spheres.push_back(c);
+		std::sort(spheres.begin(), spheres.end(), SpheresSortingOperator());
 		return spheres;
 	}
 
@@ -87,9 +123,25 @@ private:
 		double r=0;
 		for(std::size_t i=0;i<spheres.size();i++)
 		{
-			r=std::max(r, distance_from_point_to_point(mass_center, spheres[i])+spheres[i].r);
+			r=std::max(r, maximal_distance_from_point_to_sphere(mass_center, spheres[i]));
 		}
 		return SimpleSphere(mass_center.x, mass_center.y, mass_center.z, r);
+	}
+
+	static std::size_t find_cone_end(const std::vector<SimpleSphere>& sorted_spheres)
+	{
+		if(sorted_spheres.size()==3)
+		{
+			if(stick_contains_sphere(sorted_spheres[0], stick_contains_sphere[1], stick_contains_sphere[2]))
+			{
+				return 1;
+			}
+			else if(stick_contains_sphere(sorted_spheres[0], stick_contains_sphere[2], stick_contains_sphere[1]))
+			{
+				return 2;
+			}
+		}
+		return 0;
 	}
 };
 
