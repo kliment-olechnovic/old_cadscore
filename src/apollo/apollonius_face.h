f43a22b8b7559046ec1fc4d442621ac273fe9388
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-10-05 10:17:47 +0300

    Added invariants checking.

--- src/apollo/apollonius_face.h
+++ src/apollo/apollonius_face.h
@@ -2,6 +2,7 @@
 #define APOLLONIUS_FACE_H_
 
 #include <vector>
+#include <stdexcept>
 
 #include "tuples.h"
 #include "points_basic_operations.h"
@@ -23,7 +24,7 @@ public:
 			const std::vector<Sphere>& spheres,
 			const Triple& abc_ids,
 			const std::size_t d1_id,
-			const SimpleSphere& d1_tangent_sphere_) :
+			const SimpleSphere& d1_tangent_sphere) :
 				spheres_(spheres),
 				abc_ids_(abc_ids),
 				a_(spheres_[abc_ids_.get(0)]),
@@ -31,14 +32,26 @@ public:
 				c_(spheres_[abc_ids_.get(2)]),
 				d1_id_(d1_id),
 				d1_(spheres_[d1_id_]),
-				d1_tangent_sphere_(d1_tangent_sphere_),
+				d1_tangent_sphere_(d1_tangent_sphere),
 				d2_id_(npos),
 				d2_(NULL),
 				d2_tangent_sphere_(SimpleSphere(0,0,0,0)),
 				tangent_planes_(construct_spheres_tangent_planes(a_, b_, c_)),
 				tangent_stick_(tangent_planes_.empty() ? select_tangent_stick(a_, b_, c_) : std::make_pair(NULL, NULL))
 	{
-		//
+		if(!(sphere_touches_sphere(d1_tangent_sphere_, a_) &&
+				sphere_touches_sphere(d1_tangent_sphere_, b_) &&
+				sphere_touches_sphere(d1_tangent_sphere_, c_) &&
+				sphere_touches_sphere(d1_tangent_sphere_, d1_)))
+		{
+			throw std::logic_error("Invalid d1 tangent sphere");
+		}
+
+		if(!((tangent_planes_.size()==2) ||
+				(tangent_planes_.empty() && tangent_stick_.first!=NULL && tangent_stick_.second!=NULL)))
+		{
+			throw std::logic_error("Invalid tangency information");
+		}
 	}
 
 private:
