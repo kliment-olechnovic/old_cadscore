3ab2286d7f381a0299b5d17948493a33d05419eb
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-10-17 14:28:34 +0300

    Refactored.

--- src/protein_contacts.cpp
+++ src/protein_contacts.cpp
@@ -4,7 +4,7 @@
 #include "contacto/inter_atom_contact.h"
 #include "contacto/inter_residue_contacts.h"
 
-std::vector<contacto::InterAtomContact> inter_atom_contacts_from_surface_areas(const std::vector<apollo::ContactSurface::SurfaceArea>& surface_areas)
+std::vector<contacto::InterAtomContact> construct_inter_atom_contacts_from_surface_areas(const std::vector<apollo::ContactSurface::SurfaceArea>& surface_areas)
 {
 	std::vector<contacto::InterAtomContact> inter_atom_contacts;
 	std::size_t contacts_count=0;
@@ -24,6 +24,16 @@ std::vector<contacto::InterAtomContact> inter_atom_contacts_from_surface_areas(c
 	return inter_atom_contacts;
 }
 
+std::vector<contacto::InterAtomContact> construct_inter_atom_contacts_from_atoms_graph(
+		const std::vector<protein::Atom>& atoms,
+		const std::vector< std::vector<std::size_t> >& graph,
+		const std::size_t subdivision_depth,
+		const double probe_radius)
+{
+	return construct_inter_atom_contacts_from_surface_areas(
+			apollo::ContactSurface::calculate_surface_areas(atoms, graph, subdivision_depth, probe_radius));
+}
+
 std::map< std::pair<protein::ResidueID, protein::ResidueID>, std::map<std::string, double> > collect_contacts_between_residues(
 		const std::vector<protein::Atom>& atoms,
 		const std::vector<contacto::InterAtomContact>& inter_atom_contacts)
@@ -31,18 +41,5 @@ std::map< std::pair<protein::ResidueID, protein::ResidueID>, std::map<std::strin
 	return contacto::collect_contacts_between_residues<protein::Atom, protein::ResidueID>(
 			atoms,
 			inter_atom_contacts
-	);
-}
-
-std::map< std::pair<protein::ResidueID, protein::ResidueID>, std::map<std::string, double> > collect_contacts_between_residues(
-		const std::vector<protein::Atom>& atoms,
-		const std::vector< std::vector<std::size_t> >& graph,
-		const std::size_t subdivision_depth,
-		const double probe_radius)
-{
-	return collect_contacts_between_residues(
-			atoms,
-			inter_atom_contacts_from_surface_areas(
-					apollo::ContactSurface::calculate_surface_areas(atoms, graph, subdivision_depth, probe_radius))
 	);
 }
--- src/protein_contacts.h
+++ src/protein_contacts.h
@@ -8,14 +8,14 @@
 #include "protein/atom.h"
 #include "protein/residue_id.h"
 
-std::map< std::pair<protein::ResidueID, protein::ResidueID>, std::map<std::string, double> > collect_contacts_between_residues(
-		const std::vector<protein::Atom>& atoms,
-		const std::vector<contacto::InterAtomContact>& inter_atom_contacts);
-
-std::map< std::pair<protein::ResidueID, protein::ResidueID>, std::map<std::string, double> > collect_contacts_between_residues(
+std::vector<contacto::InterAtomContact> construct_inter_atom_contacts_from_atoms_graph(
 		const std::vector<protein::Atom>& atoms,
 		const std::vector< std::vector<std::size_t> >& graph,
 		const std::size_t subdivision_depth,
 		const double probe_radius);
+
+std::map< std::pair<protein::ResidueID, protein::ResidueID>, std::map<std::string, double> > collect_contacts_between_residues(
+		const std::vector<protein::Atom>& atoms,
+		const std::vector<contacto::InterAtomContact>& inter_atom_contacts);
 
 #endif /* PROTEIN_CONTACTS_H_ */
