43f97bef4a3f6af6f5568bd2c9977bc7f8326f96
kliment <kliment@localhost>
2011-10-17 14:52:21 +0300

    Overloaded streams operators.

--- src/protein/atom.h
+++ src/protein/atom.h
@@ -2,6 +2,7 @@
 #define ATOM_H_
 
 #include <string>
+#include <iostream>
 
 namespace protein
 {
@@ -32,6 +33,34 @@ struct Atom
 				y==a.y &&
 				z==a.z &&
 				r==a.r));
+	}
+
+	friend std::ostream& operator<<(std::ostream &output, const Atom &atom)
+	{
+		output << atom.chain_id << " ";
+		output << atom.atom_number << " ";
+		output << atom.residue_number << " ";
+		output << atom.residue_name << " ";
+		output << atom.atom_name << " ";
+		output << atom.x << " ";
+		output << atom.y << " ";
+		output << atom.z << " ";
+		output << atom.r;
+		return output;
+	}
+
+	friend std::istream& operator>>(std::istream &input, Atom &atom)
+	{
+		input >> atom.chain_id;
+		input >> atom.atom_number;
+		input >> atom.residue_number;
+		input >> atom.residue_name;
+		input >> atom.atom_name;
+		input >> atom.x;
+		input >> atom.y;
+		input >> atom.z;
+		input >> atom.r;
+		return input;
 	}
 };
 
