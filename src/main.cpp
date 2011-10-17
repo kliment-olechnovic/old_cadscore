40af514a22ba05cc26a2b21d33362313432b8976
kliment <kliment@localhost>
2011-10-17 14:28:34 +0300

    Refactored.

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,8 @@ int main()
 	const std::pair< std::vector<protein::Atom>, std::vector< std::vector<std::size_t> > > protein_graph=construct_protein_graph(unrefined_atoms);
 	const std::vector<protein::Atom>& atoms=protein_graph.first;
 	const std::vector< std::vector<std::size_t> >& graph=protein_graph.second;
-	const std::map< std::pair<protein::ResidueID, protein::ResidueID>, std::map<std::string, double> > contacts_between_residues=collect_contacts_between_residues(atoms, graph, 3, 1.4);
+	const std::vector<contacto::InterAtomContact> inter_atom_contacts=construct_inter_atom_contacts_from_atoms_graph(atoms, graph, 3, 1.4);
+	const std::map< std::pair<protein::ResidueID, protein::ResidueID>, std::map<std::string, double> > contacts_between_residues=collect_contacts_between_residues(atoms, inter_atom_contacts);
 
 	std::clog << contacts_between_residues.size() << "\n";
 
