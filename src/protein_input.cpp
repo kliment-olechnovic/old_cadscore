eb300fc20270db08b84cd5ca61579af97de2873a
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-10-18 10:14:47 +0300

    Added more command line options.

--- src/protein_input.cpp
+++ src/protein_input.cpp
@@ -1,14 +1,15 @@
 #include "protein_input.h"
 
+#include <string>
 #include <iostream>
 #include <fstream>
 
 #include "protein/atoms_reading.h"
 
-std::vector<protein::Atom> read_protein_atoms(const bool include_heteroatoms, const bool include_water)
+std::vector<protein::Atom> read_protein_atoms(const std::string& radius_classes_file_name, const std::string& radius_members_file_name, const bool include_heteroatoms, const bool include_water)
 {
-	std::ifstream radius_classes_stream("resources/vdwr_classes.txt");
-	std::ifstream radius_members_stream("resources/vdwr_members.txt");
+	std::ifstream radius_classes_stream(radius_classes_file_name.c_str());
+	std::ifstream radius_members_stream(radius_members_file_name.c_str());
 	const protein::VanDerWaalsRadiusAssigner radius_assigner(radius_classes_stream, radius_members_stream);
 	return protein::AtomsReading::read_atoms_from_PDB_file_stream(std::cin, radius_assigner, include_heteroatoms, include_water);
 }
--- src/protein_input.h
+++ src/protein_input.h
@@ -5,6 +5,6 @@
 
 #include "protein/atom.h"
 
-std::vector<protein::Atom> read_protein_atoms(const bool include_heteroatoms, const bool include_water);
+std::vector<protein::Atom> read_protein_atoms(const std::string& radius_classes_file_name, const std::string& radius_members_file_name, const bool include_heteroatoms, const bool include_water);
 
 #endif /* PROTEIN_INPUT_H_ */
