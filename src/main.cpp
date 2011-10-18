1d1a495cd515c7754d3aa7815796eee17b636ede
kliment <kliment@localhost>
2011-10-18 10:03:28 +0300

    Added command line options handling.

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,25 @@
 #include "protein_graph.h"
 #include "protein_contacts.h"
 
+#include "auxiliaries/command_line_options.h"
 #include "auxiliaries/vector_io.h"
 
-int main()
+int main(const int argc, const char** argv)
 {
 	std::cin.exceptions(std::istream::badbit);
 	std::cout.exceptions(std::ostream::badbit);
 
 	try
 	{
+		auxiliaries::CommandLineOptions clo(argc, argv);
+		const std::size_t subdivision_depth=clo.arg<std::size_t>("-depth");
+		const double probe_radius=clo.arg<double>("-probe");
+
 		const std::vector<protein::Atom> unrefined_atoms=read_protein_atoms(false, false);
 		const std::pair< std::vector<protein::Atom>, std::vector< std::vector<std::size_t> > > protein_graph=construct_protein_graph(unrefined_atoms);
 		const std::vector<protein::Atom>& atoms=protein_graph.first;
 		const std::vector< std::vector<std::size_t> >& graph=protein_graph.second;
-		const std::vector<contacto::InterAtomContact> inter_atom_contacts=construct_inter_atom_contacts_from_atoms_graph(atoms, graph, 3, 1.4);
+		const std::vector<contacto::InterAtomContact> inter_atom_contacts=construct_inter_atom_contacts_from_atoms_graph(atoms, graph, subdivision_depth, probe_radius);
 
 		auxiliaries::print_vector(atoms);
 		auxiliaries::print_vector(inter_atom_contacts);
