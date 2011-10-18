ca64b250f80505105d0a1620d1b55dd6c3798310
kliment <kliment@localhost>
2011-10-18 10:47:20 +0300

    Added running scripts.

---
+++ scripts/single_run.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+INPUT_FILE=$1
+INPUT_FILE_BASE=$(basename $INPUT_FILE)
+OUTPUT_PREFIX=$2
+CONTACTS_OUTPUT_FILE=$OUTPUT_PREFIX"/contacts_"$INPUT_FILE_BASE
+LOG_OUTPUT_FILE=$OUTPUT_PREFIX"/log_"$INPUT_FILE_BASE
+
+./voroprot2 \
+-radius-classes ./resources/vdwr_classes.txt \
+-radius-members ./resources/vdwr_members.txt \
+-depth 3 \
+-probe 1.4 \
+< $INPUT_FILE \
+1> $CONTACTS_OUTPUT_FILE \
+2> $LOG_OUTPUT_FILE
