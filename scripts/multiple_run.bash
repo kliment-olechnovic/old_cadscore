a6cad5246b49c40884f54532f5c2fe7df379e868
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-10-19 11:11:53 +0300

    Improved scripts.

--- scripts/multiple_run.bash
+++ scripts/multiple_run.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 INPUT_PATH=$1
-OUTPUT_PREFIX=$2
+CONTACTS_OUTPUT_PREFIX=$2
+LOG_OUTPUT_PREFIX=$3
 
 for f in `find $INPUT_PATH -name "*" -type f`
 do
 # echo "Processing $f"
-  ./single_run.bash $f $OUTPUT_PREFIX
+  ./single_run.bash $f $CONTACTS_OUTPUT_PREFIX $LOG_OUTPUT_PREFIX
 done
