46fc868f07b9ec3ff09e93cf48974e27bb2ff86a
kliment <kliment@localhost>
2011-10-20 10:32:00 +0300

    Improved script.

--- scripts/single_run.bash
+++ scripts/single_run.bash
@@ -7,6 +7,7 @@ CONTACTS_OUTPUT_FILE=$CONTACTS_OUTPUT_PREFIX"/contacts_"$INPUT_FILE_BASE
 LOG_OUTPUT_PREFIX=$3
 LOG_OUTPUT_FILE=$LOG_OUTPUT_PREFIX"/log_"$INPUT_FILE_BASE
 
+timeout 10s \
 ./voroprot2 \
 --mode ciac \
 --radius-classes ./resources/vdwr_classes.txt \
@@ -16,3 +17,6 @@ LOG_OUTPUT_FILE=$LOG_OUTPUT_PREFIX"/log_"$INPUT_FILE_BASE
 < $INPUT_FILE \
 1> $CONTACTS_OUTPUT_FILE \
 2> $LOG_OUTPUT_FILE
+
+RETURN_VALUE=$?
+[ $RETURN_VALUE -ne 0 ] && echo "Failed to process $INPUT_FILE"
