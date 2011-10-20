87303ceea277fe6adcb2a599ae3b82242f6a65d4
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-10-20 14:36:27 +0300

    Improved maps combination.

--- src/contacto/inter_residue_contacts_basic_operations.h
+++ src/contacto/inter_residue_contacts_basic_operations.h
@@ -38,25 +38,27 @@ std::map< InterResidueContactID<ResidueID>, InterResidueContactAreas > collect_i
 template<typename Key, typename Value1, typename Value2>
 std::map< Key, std::pair<Value1, Value2> > combine_two_maps(const std::map<Key, Value1>& map_1, const std::map<Key, Value2>& map_2)
 {
-	std::map< Key, std::pair<Value1, Value2> > combined_map;
+	typedef std::map< Key, std::pair<Value1, Value2> > CombinedMap;
+	CombinedMap combined_map;
 	typename std::map<Key, Value1>::const_iterator it_1=map_1.begin();
 	typename std::map<Key, Value2>::const_iterator it_2=map_2.begin();
+	typename CombinedMap::iterator prev=combined_map.begin();
 	while(it_1!=map_1.end() || it_1!=map_2.end())
 	{
 		if(it_1!=map_1.end() && it_1!=map_2.end() && it_1->first==it_2->first)
 		{
-			combined_map[it_1->first]=std::make_pair(it_1->second, it_2->second);
+			prev=combined_map.insert(prev, std::make_pair(it_1->first, std::make_pair(it_1->second, it_2->second)));
 			++it_1;
 			++it_2;
 		}
 		else if(it_1!=map_1.end() && (it_2==map_2.end() || it_1->first<it_2->first))
 		{
-			combined_map[it_1->first]=std::make_pair(it_1->second, Value2());
+			prev=combined_map.insert(prev, std::make_pair(it_1->first, std::make_pair(it_1->second, Value2())));
 			++it_1;
 		}
 		else if(it_2!=map_2.end() && (it_1==map_1.end() || it_2->first<it_1->first))
 		{
-			combined_map[it_2->first]=std::make_pair(Value1(), it_2->second);
+			prev=combined_map.insert(prev, std::make_pair(it_2->first, std::make_pair(Value1(), it_2->second)));
 			++it_2;
 		}
 	}
