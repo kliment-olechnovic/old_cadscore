74afc3dec92d3fb8ddbdaa7e8bd1a2cec7c8acf2
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-10-21 13:04:10 +0300

    Added file header logic.

--- src/main_collect_atoms.cpp
+++ src/main_collect_atoms.cpp
@@ -1,6 +1,7 @@
 #include "protein_input.h"
 
 #include "auxiliaries/command_line_options.h"
+#include "auxiliaries/file_header.h"
 #include "auxiliaries/vector_io.h"
 
 void main_collect_atoms(const auxiliaries::CommandLineOptions& clo)
@@ -11,5 +12,6 @@ void main_collect_atoms(const auxiliaries::CommandLineOptions& clo)
 	const bool include_water=clo.isopt("--include-water");
 
 	const std::vector<protein::Atom> atoms=read_protein_atoms(radius_classes_file_name, radius_members_file_name, include_heteroatoms, include_water);
+	auxiliaries::print_file_header("atoms");
 	auxiliaries::print_vector(atoms);
 }
--- src/main_construct_inter_residue_contacts.cpp
+++ src/main_construct_inter_residue_contacts.cpp
@@ -1,13 +1,17 @@
 #include "protein_inter_residue_contacts.h"
 
 #include "auxiliaries/command_line_options.h"
+#include "auxiliaries/file_header.h"
 #include "auxiliaries/vector_io.h"
 #include "auxiliaries/map_io.h"
 
 void main_construct_inter_residue_contacts(const auxiliaries::CommandLineOptions& clo)
 {
+	auxiliaries::assert_file_header("atoms");
 	const std::vector<protein::Atom> atoms=auxiliaries::read_vector<protein::Atom>();
+	auxiliaries::assert_file_header("contacts");
 	const std::vector<contacto::InterAtomContact> inter_atom_contacts=auxiliaries::read_vector<contacto::InterAtomContact>();
 	const std::map< contacto::InterResidueContactID<protein::ResidueID>, contacto::InterResidueContactAreas > inter_residue_contacts=construct_inter_residue_contacts_from_inter_atom_contacts(atoms, inter_atom_contacts);
+	auxiliaries::print_file_header("residue_contacts");
 	auxiliaries::print_map(inter_residue_contacts);
 }
