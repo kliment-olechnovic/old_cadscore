072840222d7bb04443b4c09c533c5c9bb1d8e372
kliment <kliment@localhost>
2011-10-22 16:35:11 +0300

    Refactored atoms reading.

--- src/main_collect_atoms.cpp
+++ src/main_collect_atoms.cpp
@@ -1,8 +1,20 @@
-#include "protein_input.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#include "protein/atoms_reading.h"
 
 #include "auxiliaries/command_line_options.h"
 #include "auxiliaries/file_header.h"
 #include "auxiliaries/vector_io.h"
+
+std::vector<protein::Atom> read_protein_atoms(const std::string& radius_classes_file_name, const std::string& radius_members_file_name, const bool include_heteroatoms, const bool include_water)
+{
+	std::ifstream radius_classes_stream(radius_classes_file_name.c_str());
+	std::ifstream radius_members_stream(radius_members_file_name.c_str());
+	const protein::VanDerWaalsRadiusAssigner radius_assigner(radius_classes_stream, radius_members_stream);
+	return protein::AtomsReading::read_atoms_from_PDB_file_stream(std::cin, radius_assigner, include_heteroatoms, include_water);
+}
 
 void main_collect_atoms(const auxiliaries::CommandLineOptions& clo)
 {
@@ -11,7 +23,11 @@ void main_collect_atoms(const auxiliaries::CommandLineOptions& clo)
 	const bool include_heteroatoms=clo.isopt("--include-heteroatoms");
 	const bool include_water=clo.isopt("--include-water");
 
-	const std::vector<protein::Atom> atoms=read_protein_atoms(radius_classes_file_name, radius_members_file_name, include_heteroatoms, include_water);
+	const std::vector<protein::Atom> atoms=read_protein_atoms(
+			radius_classes_file_name,
+			radius_members_file_name,
+			include_heteroatoms, include_water);
+
 	auxiliaries::print_file_header("atoms");
 	auxiliaries::print_vector(atoms);
 }
