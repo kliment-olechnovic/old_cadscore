006dc0cdd70a32f0b96516cb51bc31957c2a61c9
kliment <kliment@localhost>
2011-10-22 11:00:44 +0300

    Adjusted multirun script to take only casp first models

--- scripts/multiple_run.bash
+++ scripts/multiple_run.bash
@@ -4,7 +4,7 @@ INPUT_PATH=$1
 CONTACTS_OUTPUT_PREFIX=$2
 LOG_OUTPUT_PREFIX=$3
 
-for f in `find $INPUT_PATH -name "*" -type f`
+for f in `find $INPUT_PATH -name "*_1-*" -type f`
 do
 # echo "Processing $f"
   ./single_run.bash $f $CONTACTS_OUTPUT_PREFIX $LOG_OUTPUT_PREFIX
