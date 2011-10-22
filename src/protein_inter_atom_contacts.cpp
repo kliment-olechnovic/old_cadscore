0499daaacefbb3cf940cee740d96854a5bf99240
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-10-22 21:42:26 +0300

    Refactored.

--- src/protein_inter_atom_contacts.cpp
+++ src/protein_inter_atom_contacts.cpp
@@ -2,32 +2,12 @@
 
 #include "apollo/contact_surface.h"
 
-std::vector<contacto::InterAtomContact> construct_inter_atom_contacts_from_surface_areas(const std::vector<apollo::ContactSurface::SurfaceArea>& surface_areas)
-{
-	std::vector<contacto::InterAtomContact> inter_atom_contacts;
-	std::size_t contacts_count=0;
-	for(std::size_t i=0;i<surface_areas.size();i++)
-	{
-		contacts_count+=surface_areas[i].size();
-	}
-	inter_atom_contacts.reserve(contacts_count);
-	for(std::size_t i=0;i<surface_areas.size();i++)
-	{
-		const apollo::ContactSurface::SurfaceArea& surface_area=surface_areas[i];
-		for(apollo::ContactSurface::SurfaceArea::const_iterator it=surface_area.begin();it!=surface_area.end();it++)
-		{
-			inter_atom_contacts.push_back(contacto::InterAtomContact(i, it->first, it->second));
-		}
-	}
-	return inter_atom_contacts;
-}
-
 std::vector<contacto::InterAtomContact> construct_inter_atom_contacts_from_atoms_graph(
 		const std::vector<protein::Atom>& atoms,
 		const std::vector< std::vector<std::size_t> >& graph,
 		const std::size_t subdivision_depth,
 		const double probe_radius)
 {
-	return construct_inter_atom_contacts_from_surface_areas(
+	return apollo::ContactSurface::construct_inter_sphere_contacts_from_surface_areas<contacto::InterAtomContact>(
 			apollo::ContactSurface::calculate_surface_areas(atoms, graph, subdivision_depth, probe_radius));
 }
