56ac77136b2c7a9207832f8b42191c65244fe637
kliment <kliment@localhost>
2011-10-23 22:06:47 +0300

    Added scoring modes.

--- src/main_calculate_contact_area_difference_profile.cpp
+++ src/main_calculate_contact_area_difference_profile.cpp
@@ -6,6 +6,7 @@
 #include "contacto/inter_residue_contacts_combination.h"
 #include "contacto/residue_contact_area_difference_profile.h"
 #include "contacto/residue_contact_area_difference_basic_scoring_functors.h"
+#include "contacto/utilities.h"
 
 #include "auxiliaries/command_line_options.h"
 #include "auxiliaries/file_header.h"
@@ -15,6 +16,7 @@
 void main_calculate_contact_area_difference_profile(const auxiliaries::CommandLineOptions& clo)
 {
 	const bool intersect_sequences=clo.isopt("--intersect-sequences");
+	const int scoring_mode=clo.arg<int>("--scoring-mode", 0, 2);
 
 	auxiliaries::assert_file_header("atoms");
 	const std::vector<protein::Atom> atoms_1=auxiliaries::read_vector<protein::Atom>();
@@ -34,7 +36,26 @@ void main_calculate_contact_area_difference_profile(const auxiliaries::CommandLi
 			contacto::construct_inter_residue_contacts<protein::Atom, protein::ResidueID>(atoms_1, inter_atom_contacts_1),
 			contacto::construct_inter_residue_contacts<protein::Atom, protein::ResidueID>(atoms_2, inter_atom_contacts_2));
 
-	const std::map<protein::ResidueID, contacto::ResidueContactAreaDifferenceScore> residue_contact_area_difference_profile=contacto::construct_residue_contact_area_difference_profile<protein::ResidueID, contacto::BoundedDifferenceProducer, contacto::SimpleReferenceProducer>(combined_inter_residue_contacts, residue_ids_1);
+	std::map<protein::ResidueID, contacto::ResidueContactAreaDifferenceScore> residue_contact_area_difference_profile;
+	if(scoring_mode==0)
+	{
+		residue_contact_area_difference_profile=
+				contacto::construct_residue_contact_area_difference_profile<protein::ResidueID, contacto::BoundedDifferenceProducer, contacto::SimpleReferenceProducer>(combined_inter_residue_contacts, residue_ids_1);
+	}
+	else if(scoring_mode==1)
+	{
+		residue_contact_area_difference_profile=
+				contacto::construct_residue_contact_area_difference_profile<protein::ResidueID, contacto::SimpleDifferenceProducer, contacto::SimpleReferenceProducer>(combined_inter_residue_contacts, residue_ids_1);
+	}
+	else if(scoring_mode==2)
+	{
+		residue_contact_area_difference_profile=
+				contacto::construct_residue_contact_area_difference_profile<protein::ResidueID, contacto::SimpleDifferenceProducer, contacto::SummingReferenceProducer>(combined_inter_residue_contacts, residue_ids_1);
+	}
+	else
+	{
+		throw std::runtime_error("Invalid scoring mode");
+	}
 
 	auxiliaries::print_file_header("cad_profile");
 	auxiliaries::print_map(residue_contact_area_difference_profile);
