7f9d3a6f0c8d85023df95b2fc1220889c64e7d71
kliment <kliment@localhost>
2011-10-23 21:56:39 +0300

    Addede "use min" option.

--- src/main_calculate_contact_area_difference_global_score.cpp
+++ src/main_calculate_contact_area_difference_global_score.cpp
@@ -8,10 +8,12 @@
 
 void main_calculate_contact_area_difference_global_score(const auxiliaries::CommandLineOptions& clo)
 {
+	const bool use_min=clo.isopt("--use-min");
+
 	auxiliaries::assert_file_header("cad_profile");
 	const std::map<protein::ResidueID, contacto::ResidueContactAreaDifferenceScore> residue_contact_area_difference_profile=auxiliaries::read_map<protein::ResidueID, contacto::ResidueContactAreaDifferenceScore>();
 
-	const contacto::ResidueContactAreaDifferenceScore global_score=contacto::calculate_global_contact_area_difference_score_from_profile(residue_contact_area_difference_profile, false);
+	const contacto::ResidueContactAreaDifferenceScore global_score=contacto::calculate_global_contact_area_difference_score_from_profile(residue_contact_area_difference_profile, use_min);
 
 	auxiliaries::print_file_header("global_score");
 	std::cout << global_score << "\n";
