610f00e68adbe43511eccb7838206b8f8f84704a
kliment <kliment@localhost>
2011-10-23 21:30:57 +0300

    Refactored scoring functors.

--- src/main_calculate_contact_area_difference_profile.cpp
+++ src/main_calculate_contact_area_difference_profile.cpp
@@ -1,5 +1,3 @@
-#include <cmath>
-
 #include "protein/atom.h"
 #include "protein/residue_id.h"
 #include "protein/residue_ids_collection.h"
@@ -7,27 +5,12 @@
 #include "contacto/inter_residue_contacts_construction.h"
 #include "contacto/inter_residue_contacts_combination.h"
 #include "contacto/residue_contact_area_difference_profile.h"
+#include "contacto/residue_contact_area_difference_basic_scoring_functors.h"
 
 #include "auxiliaries/command_line_options.h"
 #include "auxiliaries/file_header.h"
 #include "auxiliaries/vector_io.h"
 #include "auxiliaries/map_io.h"
-
-struct DifferenceProducer
-{
-	double operator()(const double target_area, const double model_area) const
-	{
-		return std::min(fabs(target_area-model_area), target_area);
-	}
-};
-
-struct ReferenceProducer
-{
-	double operator()(const double target_area, const double model_area) const
-	{
-		return target_area;
-	}
-};
 
 void main_calculate_contact_area_difference_profile(const auxiliaries::CommandLineOptions& clo)
 {
@@ -49,7 +32,7 @@ void main_calculate_contact_area_difference_profile(const auxiliaries::CommandLi
 			contacto::construct_inter_residue_contacts<protein::Atom, protein::ResidueID>(atoms_1, inter_atom_contacts_1),
 			contacto::construct_inter_residue_contacts<protein::Atom, protein::ResidueID>(atoms_2, inter_atom_contacts_2));
 
-	const std::map<protein::ResidueID, contacto::ResidueContactAreaDifferenceScore> residue_contact_area_difference_profile=contacto::construct_residue_contact_area_difference_profile<protein::ResidueID, DifferenceProducer, ReferenceProducer>(combined_inter_residue_contacts, residue_ids_1);
+	const std::map<protein::ResidueID, contacto::ResidueContactAreaDifferenceScore> residue_contact_area_difference_profile=contacto::construct_residue_contact_area_difference_profile<protein::ResidueID, contacto::BoundedDifferenceProducer, contacto::SimpleReferenceProducer>(combined_inter_residue_contacts, residue_ids_1);
 
 	auxiliaries::print_file_header("cad_profile");
 	auxiliaries::print_map(residue_contact_area_difference_profile);
