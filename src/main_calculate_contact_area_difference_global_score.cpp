bcd0a5f0b0f448e0bb22c62078840e6acb200c43
kliment <kliment@localhost>
2011-10-23 22:14:42 +0300

    Made options policy more strict.

--- src/main_calculate_contact_area_difference_global_score.cpp
+++ src/main_calculate_contact_area_difference_global_score.cpp
@@ -8,12 +8,12 @@
 
 void main_calculate_contact_area_difference_global_score(const auxiliaries::CommandLineOptions& clo)
 {
-	const bool use_min=clo.isopt("--use-min");
+	const int use_min=clo.arg<int>("--use-min", 0, 1);
 
 	auxiliaries::assert_file_header("cad_profile");
 	const std::map<protein::ResidueID, contacto::ResidueContactAreaDifferenceScore> residue_contact_area_difference_profile=auxiliaries::read_map<protein::ResidueID, contacto::ResidueContactAreaDifferenceScore>();
 
-	const contacto::ResidueContactAreaDifferenceScore global_score=contacto::calculate_global_contact_area_difference_score_from_profile(residue_contact_area_difference_profile, use_min);
+	const contacto::ResidueContactAreaDifferenceScore global_score=contacto::calculate_global_contact_area_difference_score_from_profile(residue_contact_area_difference_profile, (use_min==1));
 
 	auxiliaries::print_file_header("global_score");
 	std::cout << global_score << "\n";
