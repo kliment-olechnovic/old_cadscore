d5d1d936f7cacbf623b870395d08b63a6b3235ce
kliment <kliment@localhost>
2011-10-24 11:20:15 +0300

    Updated scripts.

--- scripts/calc_all_scores.bash
+++ scripts/calc_all_scores.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cat $1 $2 | ./scripts/calc_score.bash 0 0 0 bounded
-cat $1 $2 | ./scripts/calc_score.bash 0 1 0 simple
-cat $1 $2 | ./scripts/calc_score.bash 0 1 1 late_bounded
-cat $1 $2 | ./scripts/calc_score.bash 0 2 0 abag_totr_0
-cat $1 $2 | ./scripts/calc_score.bash 1 2 0 abag_totr_1
+cat $1 $2 | ./calc_score.bash 0 0 0 bounded
+cat $1 $2 | ./calc_score.bash 0 1 0 simple
+cat $1 $2 | ./calc_score.bash 0 1 1 late_bounded
+cat $1 $2 | ./calc_score.bash 0 2 0 abag_totr_0
+cat $1 $2 | ./calc_score.bash 1 2 0 abag_totr_1
