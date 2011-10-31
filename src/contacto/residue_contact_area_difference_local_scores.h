9b2b1ada75ba6b5ef0120ff3fffd821bafc2a495
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-10-31 14:27:11 +0200

    Refactored local scores construction.

---
+++ src/contacto/residue_contact_area_difference_local_scores.h
@@ -0,0 +1,103 @@
+#ifndef CONTACTO_RESIDUE_CONTACT_AREA_DIFFERENCE_LOCAL_SCORES_H_
+#define CONTACTO_RESIDUE_CONTACT_AREA_DIFFERENCE_LOCAL_SCORES_H_
+
+#include <map>
+#include <string>
+
+#include "residue_contact_area_difference_score.h"
+
+namespace contacto
+{
+
+template<typename ResidueID>
+std::map<ResidueID, double> construct_local_scores_from_profile(const std::map<ResidueID, contacto::ResidueContactAreaDifferenceScore>& profile, const std::string& category)
+{
+	std::map<ResidueID, double> local_scores;
+	typename std::map<ResidueID, double>::iterator insertion_it=local_scores.begin();
+	for(typename std::map<ResidueID, contacto::ResidueContactAreaDifferenceScore>::const_iterator it=profile.begin();it!=profile.end();++it)
+	{
+		const ResidueID& residue_id=it->first;
+		const contacto::ResidueContactAreaDifferenceScore& residue_score=it->second;
+
+		if(it!=profile.begin())
+		{
+			typename std::map<ResidueID, contacto::ResidueContactAreaDifferenceScore>::const_iterator previous_it=it;
+			previous_it--;
+			const ResidueID& prev_residue_id=previous_it->first;
+			if((residue_id.residue_number-prev_residue_id.residue_number>1) && residue_id.chain_id==prev_residue_id.chain_id)
+			{
+				for(int i=prev_residue_id.residue_number+1;i<residue_id.residue_number;i++)
+				{
+					local_scores[ResidueID(prev_residue_id.chain_id, i)]=-1;
+					insertion_it=local_scores.insert(insertion_it, std::make_pair(ResidueID(prev_residue_id.chain_id, i), -1));
+				}
+			}
+		}
+
+		contacto::ResidueContactAreaDifferenceScore::Ratio ratio=residue_score.ratio(category);
+		insertion_it=local_scores.insert(insertion_it, std::make_pair(residue_id, ((ratio.reference>0) ? (ratio.difference/ratio.reference) : -2)));
+	}
+	return local_scores;
+}
+
+template<typename ResidueID>
+std::map<ResidueID, double> blur_local_scores(const std::map<ResidueID, double>& local_scores, const int window_size)
+{
+	if(window_size==0)
+	{
+		return local_scores;
+	}
+
+	std::map<ResidueID, double> blured_scores;
+	typename std::map<ResidueID, double>::iterator insertion_it=blured_scores.begin();
+	for(typename std::map<ResidueID, double>::const_iterator it=local_scores.begin();it!=local_scores.end();++it)
+	{
+		const ResidueID& residue_id=it->first;
+		double sum=it->second;
+		int count=1;
+		if(sum>=0)
+		{
+			typename std::map<ResidueID, double>::const_iterator left_it=it;
+			double left_last=sum;
+
+			typename std::map<ResidueID, double>::const_iterator right_it=it;
+			double right_last=sum;
+
+			for(int i=1;i<=window_size;i++)
+			{
+				if(left_it!=local_scores.begin())
+				{
+					left_it--;
+					const ResidueID& left_residue_id=left_it->first;
+					if(residue_id.residue_number-left_residue_id.residue_number<=window_size
+							&& residue_id.chain_id==left_residue_id.chain_id
+							&& left_it->second>0)
+					{
+						left_last=left_it->second;
+					}
+				}
+				sum+=left_last;
+				count++;
+
+				if(right_it!=local_scores.end() && (++right_it)!=local_scores.end())
+				{
+					const ResidueID& right_residue_id=right_it->first;
+					if(right_residue_id.residue_number-residue_id.residue_number<=window_size
+							&& residue_id.chain_id==right_residue_id.chain_id
+							&& right_it->second>0)
+					{
+						right_last=right_it->second;
+					}
+				}
+				sum+=right_last;
+				count++;
+			}
+		}
+		insertion_it=blured_scores.insert(insertion_it, std::make_pair(residue_id, (sum/count)));
+	}
+	return blured_scores;
+}
+
+}
+
+#endif /* CONTACTO_RESIDUE_CONTACT_AREA_DIFFERENCE_LOCAL_SCORES_H_ */
--- src/main_calculate_contact_area_difference_local_scores.cpp
+++ src/main_calculate_contact_area_difference_local_scores.cpp
@@ -4,97 +4,11 @@
 #include "protein/residue_id.h"
 
 #include "contacto/residue_contact_area_difference_profile.h"
+#include "contacto/residue_contact_area_difference_local_scores.h"
 
 #include "auxiliaries/command_line_options.h"
 #include "auxiliaries/file_header.h"
 #include "auxiliaries/map_io.h"
-
-typedef std::map<protein::ResidueID, contacto::ResidueContactAreaDifferenceScore> Profile;
-typedef std::map<protein::ResidueID, double> LocalScores;
-
-const double GAP_VALUE=-1;
-const double NO_VALUE=-2;
-
-LocalScores construct_local_scores_from_profile(const Profile& profile, const std::string& category)
-{
-	LocalScores local_scores;
-	for(Profile::const_iterator it=profile.begin();it!=profile.end();++it)
-	{
-		const protein::ResidueID& residue_id=it->first;
-		const contacto::ResidueContactAreaDifferenceScore& residue_score=it->second;
-
-		if(it!=profile.begin())
-		{
-			Profile::const_iterator prev_it=it;
-			prev_it--;
-			const protein::ResidueID& prev_residue_id=prev_it->first;
-			if((residue_id.residue_number-prev_residue_id.residue_number>1) && residue_id.chain_id==prev_residue_id.chain_id)
-			{
-				for(int i=prev_residue_id.residue_number+1;i<residue_id.residue_number;i++)
-				{
-					local_scores[protein::ResidueID(prev_residue_id.chain_id, i)]=GAP_VALUE;
-				}
-			}
-		}
-
-		contacto::ResidueContactAreaDifferenceScore::Ratio ratio=residue_score.ratio(category);
-		local_scores[residue_id]=(ratio.reference>0) ? (ratio.difference/ratio.reference) : NO_VALUE;
-	}
-	return local_scores;
-}
-
-std::map<protein::ResidueID, double> blur_local_scores(const LocalScores& local_scores, const int window_size)
-{
-	if(window_size==0)
-	{
-		return local_scores;
-	}
-
-	LocalScores blured_scores;
-	LocalScores::iterator prev=blured_scores.begin();
-	for(LocalScores::const_iterator it=local_scores.begin();it!=local_scores.end();++it)
-	{
-		const protein::ResidueID& residue_id=it->first;
-		double sum=it->second;
-		int count=1;
-		if(it->second>0)
-		{
-			LocalScores::const_iterator left=it;
-			LocalScores::const_iterator right=it;
-			for(int i=1;i<=window_size;i++)
-			{
-				if(left!=local_scores.begin())
-				{
-					left--;
-					const protein::ResidueID& left_residue_id=left->first;
-					if(residue_id.residue_number-left_residue_id.residue_number<=window_size && residue_id.chain_id==left_residue_id.chain_id)
-					{
-						if(left->second>0)
-						{
-							sum+=left->second;
-							count++;
-						}
-					}
-				}
-				if(right!=local_scores.end())
-				{
-					right++;
-					const protein::ResidueID& right_residue_id=right->first;
-					if(right_residue_id.residue_number-residue_id.residue_number<=window_size && residue_id.chain_id==right_residue_id.chain_id)
-					{
-						if(right->second>0)
-						{
-							sum+=right->second;
-							count++;
-						}
-					}
-				}
-			}
-		}
-		prev=blured_scores.insert(prev, std::make_pair(residue_id, (sum/count)));
-	}
-	return blured_scores;
-}
 
 void main_calculate_contact_area_difference_local_scores(const auxiliaries::CommandLineOptions& clo)
 {
@@ -102,9 +16,9 @@ void main_calculate_contact_area_difference_local_scores(const auxiliaries::Comm
 	const int window_size=clo.arg<int>("--window", 0, 1000);
 
 	auxiliaries::assert_file_header("cad_profile");
-	const Profile profile=auxiliaries::read_map<protein::ResidueID, contacto::ResidueContactAreaDifferenceScore>();
+	const std::map<protein::ResidueID, contacto::ResidueContactAreaDifferenceScore> profile=auxiliaries::read_map<protein::ResidueID, contacto::ResidueContactAreaDifferenceScore>();
 
-	const LocalScores local_scores=blur_local_scores(construct_local_scores_from_profile(profile, category), window_size);
+	const std::map<protein::ResidueID, double> local_scores=contacto::blur_local_scores(contacto::construct_local_scores_from_profile(profile, category), window_size);
 
 	auxiliaries::print_file_header("local_scores");
 	auxiliaries::print_map(local_scores, false);
