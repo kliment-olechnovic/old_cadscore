67b6c105b97c3bee67377426928a4c17993e8264
kliment <kliment@localhost>
2011-11-02 11:39:40 +0200

    Improved cad script.

--- scripts/cad_score.bash
+++ scripts/cad_score.bash
@@ -11,12 +11,21 @@ BOTH_NAMES=$TARGET_NAME"_and_"$MODEL_NAME
 PARENT_OUTPUT_DIRECTORY=$3/$TARGET_NAME
 OUTPUT_DIRECTORY=$PARENT_OUTPUT_DIRECTORY/$MODEL_NAME
 
+SCORING_MODES=(0 1 2)
+CONTACT_CATEGORIES=(AA AS SA SS)
+
 mkdir -p $OUTPUT_DIRECTORY
 
 TARGET_ALL_ATOMS_FILE=$PARENT_OUTPUT_DIRECTORY/all_atoms_of_target_$TARGET_NAME
 if [ ! -f $TARGET_ALL_ATOMS_FILE ];
 then
   cat $TARGET_FILE | ./voroprot2 --mode collect-atoms --radius-classes ./resources/vdwr_classes.txt --radius-members ./resources/vdwr_members.txt --include-heteroatoms 0 --include-water 0 > $TARGET_ALL_ATOMS_FILE
+fi
+
+TARGET_RESIDUE_IDS_FILE=$PARENT_OUTPUT_DIRECTORY/residue_ids_of_target_$TARGET_NAME
+if [ ! -f $TARGET_RESIDUE_IDS_FILE ];
+then
+  cat $TARGET_ALL_ATOMS_FILE | ./voroprot2 --mode collect-residue-ids  > $TARGET_RESIDUE_IDS_FILE
 fi
 
 TARGET_INTER_ATOM_CONTACTS_FILE=$PARENT_OUTPUT_DIRECTORY/inter_atom_contacts_of_target_$TARGET_NAME
@@ -31,10 +40,12 @@ cat $MODEL_FILE | ./voroprot2 --mode collect-atoms --radius-classes ./resources/
 MODEL_SELECTED_ATOMS_FILE=$OUTPUT_DIRECTORY/selected_atoms_of_model_$MODEL_NAME
 cat $TARGET_ALL_ATOMS_FILE $MODEL_ALL_ATOMS_FILE | ./voroprot2 --mode filter-atoms-by-target > $MODEL_SELECTED_ATOMS_FILE
 
+MODEL_RESIDUE_IDS_FILE=$OUTPUT_DIRECTORY/residue_ids_of_model_$MODEL_NAME
+cat $MODEL_SELECTED_ATOMS_FILE | ./voroprot2 --mode collect-residue-ids  > $MODEL_RESIDUE_IDS_FILE
+
 MODEL_INTER_ATOM_CONTACTS_FILE=$OUTPUT_DIRECTORY/inter_atom_contacts_of_model_$MODEL_NAME
 cat $MODEL_SELECTED_ATOMS_FILE | ./voroprot2 --mode construct-inter-atom-contacts --depth 3 --probe 1.4 > $MODEL_INTER_ATOM_CONTACTS_FILE
 
-SCORING_MODES=(0 1 2)
 for SCORING_MODE in ${SCORING_MODES[*]}
 do
   CAD_PROFILE_FILE=$OUTPUT_DIRECTORY/cad_profile_$SCORING_MODE"_"of_$BOTH_NAMES
@@ -43,7 +54,6 @@ do
   GLOBAL_SCORES_FILE=$OUTPUT_DIRECTORY/global_scores_$SCORING_MODE"_"of_$BOTH_NAMES
   cat $CAD_PROFILE_FILE | ./voroprot2 --mode calculate-contact-area-difference-global-score --use-min 1 --prefix sm$SCORING_MODE > $GLOBAL_SCORES_FILE
 
-  CONTACT_CATEGORIES=(AA AS SA SS)
   for CONTACT_CATEGORY in ${CONTACT_CATEGORIES[*]}
   do
     LOCAL_SCORES_PLOT_FILE=$OUTPUT_DIRECTORY/local_scores_plot_$SCORING_MODE"_"$CONTACT_CATEGORY"_"of_$BOTH_NAMES.ppm
@@ -54,7 +64,6 @@ done
 COMBINED_INTER_RESIDUE_CONTACTS_FILE=$OUTPUT_DIRECTORY/combined_inter_residue_contacts_of_$BOTH_NAMES
 cat $TARGET_INTER_ATOM_CONTACTS_FILE $MODEL_INTER_ATOM_CONTACTS_FILE | ./voroprot2 --mode combine-inter-residue-contacts > $COMBINED_INTER_RESIDUE_CONTACTS_FILE
 
-CONTACT_CATEGORIES=(AA AS SA SS)
 for CONTACT_CATEGORY in ${CONTACT_CATEGORIES[*]}
 do
   COMBINED_INTER_RESIDUE_CONTACTS_PLOT_FILE_PPM=$OUTPUT_DIRECTORY/combined_inter_residue_contacts_plot_$CONTACT_CATEGORY"_"of_$BOTH_NAMES.ppm
