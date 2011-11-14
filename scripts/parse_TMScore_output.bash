c59904f9651d2000078c65a5b666fe5bec1935af
kliment <kliment@localhost>
2011-11-14 10:15:03 +0200

    Removed outdated scripts

--- scripts/parse_TMScore_output.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo TM_score `cat $1 | egrep "TM-score\s*=.*d0" | sed 's/TM-score\s*=\s*\(.*\)\s*(.*/\1/g'`
-echo GDT_TS `cat $1 | egrep GDT-TS-score | sed 's/GDT-TS-score\s*=\s*\(.*\)\s*%(d<1).*/\1/g'`
-echo GDT_HA `cat $1 | egrep GDT-HA-score | sed 's/GDT-HA-score\s*=\s*\(.*\)\s*%(d<0\.5).*/\1/g'`
--- scripts/run_TMScore.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-TEMP_FILE="/tmp/$(basename $0).$RANDOM.txt"
-
-TMscore $1 $2 > $TEMP_FILE
-
-./parse_TMScore_output.bash $TEMP_FILE
