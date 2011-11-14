9a8cd1fa2bdff7f2dec37e9ad31a5b8114bed8d2
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-11-14 10:15:03 +0200

    Removed outdated scripts

--- scripts/calc_contacts.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./voroprot --mode collect-atoms --radius-classes ./resources/vdwr_classes.txt --radius-members ./resources/vdwr_members.txt --include-heteroatoms 0 --include-water 0 \
-| \
-./voroprot --mode construct-inter-atom-contacts --depth 3 --probe 1.4
--- scripts/calc_score.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./voroprot --mode calculate-contact-area-difference-profile --intersect-sequences $1 --scoring-mode $2 \
-| \
-./voroprot --mode calculate-contact-area-difference-global-score --use-min $3 --prefix $4
