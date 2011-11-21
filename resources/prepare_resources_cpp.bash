ff0e0072621ddecdf1963deac3419b99fc018e4f
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-11-21 14:10:45 +0200

    Removed unused file.

--- resources/prepare_resources_cpp.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cat vdwr_classes.txt | ( echo "static unsigned char vdwr_classes[] = {"; xxd -i; echo "};" ) > resources.cpp
-echo >> resources.cpp
-cat vdwr_members.txt | ( echo "static unsigned char vdwr_members[] = {"; xxd -i; echo "};" ) >> resources.cpp
