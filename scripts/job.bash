425cbfd0e584c87232125bddcf55ebf4a716f5e8
kliment <kliment@localhost>
2011-11-21 12:03:46 +0200

    Added table tidying.

--- scripts/job.bash
+++ scripts/job.bash
@@ -5,3 +5,5 @@ JOB_DIRECTORY=$1
 ./score_many_many.bash ./cad_score.bash $JOB_DIRECTORY/targets "*" $JOB_DIRECTORY/models "*" $JOB_DIRECTORY/output
 
 ./assemble_scores_table.bash $JOB_DIRECTORY/output "*summary_table" > $JOB_DIRECTORY/scores
+
+R --silent --vanilla --args $JOB_DIRECTORY/scores $JOB_DIRECTORY/tidy_scores < tidy_cad_scores_table.R
