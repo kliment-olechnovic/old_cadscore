88c8643333674f8e19acf40459347710d1abb5b7
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-11-24 12:03:09 +0200

    Fixed directory search in jobs script.

--- scripts/jobs.bash
+++ scripts/jobs.bash
@@ -2,7 +2,7 @@
 
 JOBS_DIRECTORY=$1
 
-for JOB_DIRECTORY in `find $JOBS_DIRECTORY -type d -maxdepth 1`
+for JOB_DIRECTORY in `find $JOBS_DIRECTORY ./refinement -mindepth 1 -maxdepth 1 -type d`
 do
-  job.bash $JOB_DIRECTORY
+  ./job.bash $JOB_DIRECTORY
 done
