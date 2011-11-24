7a2b6c3d0fdbf0e2bf41378aa7fd27fb740b8c10
kliment <kliment@localhost>
2011-11-24 11:43:55 +0200

    Cosmetic changes.

--- scripts/job.bash
+++ scripts/job.bash
@@ -2,7 +2,7 @@
 
 JOB_DIRECTORY=$1
 
-./score_many_many.bash ./cad_score.bash $JOB_DIRECTORY/targets "*" $JOB_DIRECTORY/models "*" $JOB_DIRECTORY/output
+./score_many_many.bash ./cad_score.bash $JOB_DIRECTORY/target "*" $JOB_DIRECTORY/model "*" $JOB_DIRECTORY/output
 
 ./assemble_scores_table.bash $JOB_DIRECTORY/output "*summary_table" > $JOB_DIRECTORY/output/scores
 
