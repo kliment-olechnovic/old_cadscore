b39a80a958bb99aee73eb747320d0c88977615a8
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-11-25 09:11:56 +0200

    Improved job script.

--- scripts/job.bash
+++ scripts/job.bash
@@ -4,5 +4,5 @@ JOB_DIRECTORY=$1
 
 ./score_many_many.bash ./cad_score.bash $JOB_DIRECTORY/target "*" $JOB_DIRECTORY/model "*" $JOB_DIRECTORY/output
 
-./assemble_scores_table.bash $JOB_DIRECTORY/output "*summary_table" > $JOB_DIRECTORY/output/scores
+./assemble_scores_table.bash $JOB_DIRECTORY/output "*summary_table" "sm0_SS" > $JOB_DIRECTORY/output/scores
 
