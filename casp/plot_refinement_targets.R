3121e4928935a754b5747ebd1f47a7b348e74fc1
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-11-28 15:13:29 +0200

    Added refinement targets plotting.

---
+++ casp/plot_refinement_targets.R
@@ -0,0 +1,23 @@
+cmd_args=commandArgs(TRUE);
+
+t=read.table(cmd_args[1], header=TRUE);
+
+targets_set=union(t$target, t$target);
+
+score_types=c("sm0_SA");
+
+for(i in targets_set)
+{
+  tt=t[which(t$target==i), ];
+  png(paste(cmd_args[2], i, ".png", sep=""), width=5*length(score_types), height=5, units="in", res=200);
+  par(mfrow=c(1, length(score_types)));
+  for(score_type in score_types)
+  {
+    plot(x=c(0, 1), y=c(0,1), main=paste("Target ", i, sep=""), xlab="GDT_HA", ylab=score_type, type="n");
+    points(x=tt$TM_score_GDT_HA, y=tt[, score_type], col="blue", pch=21);
+    stt=t[which(tt$group==0), ];
+    points(x=stt$TM_score_GDT_HA, y=stt[, score_type], col="red", pch=16);
+    legend(0.1, 0.9, c("refined", "initial"), col=c("blue","red"), pch=c(21,16));
+  }
+  dev.off();
+}
