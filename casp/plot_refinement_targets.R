b1430feea62de9081404b86a2aec6c34af656a58
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-11-29 11:57:36 +0200

    Improved refinement plotting.

--- casp/plot_refinement_targets.R
+++ casp/plot_refinement_targets.R
@@ -4,7 +4,8 @@ t=read.table(cmd_args[1], header=TRUE);
 
 targets_set=union(t$target, t$target);
 
-score_types=c("sm0_AA", "sm0_SS");
+ref_score_type="TM_score_GDT_HA";
+score_types=c("TM_score_GDT_HA", "sm0_AA", "sm0_SS");
 
 for(i in targets_set)
 {
@@ -16,12 +17,13 @@ for(i in targets_set)
     scores=tt[, score_type];
     max_score=max(scores);
     best=tt[which(scores==max_score),];
-    plot(x=c(0, 1), y=c(0,1), main=paste("Refinement target ", i, " GDT_HA vs ", score_type, sep=""), xlab="GDT_HA", ylab=score_type, type="n");
-    points(x=tt$TM_score_GDT_HA, y=tt[, score_type], col="blue", pch=21);
+    plot(x=c(0, 1), y=c(0,1), main=paste("Refinement target", i, "-", ref_score_type, "vs", score_type, sep=" "), xlab=ref_score_type, ylab=score_type, type="n");
+    points(x=tt[, ref_score_type], y=tt[, score_type], col="blue", pch=21);
     stt=tt[which(tt$group==0), ];
-    points(x=stt$TM_score_GDT_HA, y=stt[, score_type], col="red", pch=16);
+    points(x=stt[, ref_score_type], y=stt[, score_type], col="red", pch=16);
     legend(0.05, 0.95, c("Initial", "Refined"), col=c("red","blue"), pch=c(21,16));
-    text(x=0.7, y=0.9, labels=paste("Best is TR", best$target[1], "TS", best$group[1], "_", best$model[1], sep=""));
+    text(x=0.7, y=0.95, labels=paste("The best is TR", best$target[1], "TS", best$group[1], "_", best$model[1], sep=""));
+    text(x=0.7, y=0.9, labels=paste("Best score improved ", sprintf("%.3f", (max_score/stt[, score_type][1])), " times", sep=""));
   }
   dev.off();
 }
