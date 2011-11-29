1e5cc8278db1124b2ae923c89d2e117b8906c054
kliment <kliment@localhost>
2011-11-29 10:35:29 +0200

    Improved full and domain plotting.

--- casp/plot_full_and_four_domains.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p ../results/plot_full_and_four_domains
-
-R --vanilla --args ../results/enhanced_table.txt ../results/plot_full_and_four_domains/ < plot_full_and_four_domains.R
