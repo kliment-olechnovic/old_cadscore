be14bf117136e749baa55b4992441fd15acc9e28
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-12-02 13:41:45 +0200

    Redesigned profile input.

--- src/main_calculate_contact_area_difference_profile.cpp
+++ src/main_calculate_contact_area_difference_profile.cpp
@@ -1,39 +1,26 @@
 #include "protein/atom.h"
 #include "protein/residue_id.h"
-#include "protein/residue_ids_collection.h"
 
-#include "contacto/inter_residue_contacts_construction.h"
-#include "contacto/inter_residue_contacts_combination.h"
+#include "contacto/inter_residue_contact_dual_areas.h"
 #include "contacto/residue_contact_area_difference_profile.h"
 #include "contacto/residue_contact_area_difference_basic_scoring_functors.h"
 #include "contacto/utilities.h"
 
 #include "auxiliaries/command_line_options.h"
 #include "auxiliaries/file_header.h"
-#include "auxiliaries/vector_io.h"
 #include "auxiliaries/map_io.h"
+#include "auxiliaries/set_io.h"
 
 void main_calculate_contact_area_difference_profile(const auxiliaries::CommandLineOptions& clo)
 {
-	const int scoring_mode=clo.arg<int>("--scoring-mode", 0, 3);
+	const int scoring_mode=clo.arg<int>("--scoring-mode", 0, 2);
 
-	auxiliaries::assert_file_header("atoms");
-	const std::vector<protein::Atom> atoms_1=auxiliaries::read_vector<protein::Atom>();
+	auxiliaries::assert_file_header("combined_residue_contacts");
+	const std::map< contacto::InterResidueContactID<protein::ResidueID>, contacto::InterResidueContactDualAreas > combined_inter_residue_contacts=
+			auxiliaries::read_map< contacto::InterResidueContactID<protein::ResidueID>, contacto::InterResidueContactDualAreas >();
 
-	auxiliaries::assert_file_header("contacts");
-	const std::vector<contacto::InterAtomContact> inter_atom_contacts_1=auxiliaries::read_vector<contacto::InterAtomContact>();
-
-	auxiliaries::assert_file_header("atoms");
-	const std::vector<protein::Atom> atoms_2=auxiliaries::read_vector<protein::Atom>();
-
-	auxiliaries::assert_file_header("contacts");
-	const std::vector<contacto::InterAtomContact> inter_atom_contacts_2=auxiliaries::read_vector<contacto::InterAtomContact>();
-
-	const std::set<protein::ResidueID> residue_ids_1=protein::collect_residue_ids_from_atoms(atoms_1);
-
-	const std::map< contacto::InterResidueContactID<protein::ResidueID>, contacto::InterResidueContactDualAreas > combined_inter_residue_contacts=contacto::combine_two_inter_residue_contact_maps<protein::ResidueID>(
-			contacto::construct_inter_residue_contacts<protein::Atom, protein::ResidueID>(atoms_1, inter_atom_contacts_1),
-			contacto::construct_inter_residue_contacts<protein::Atom, protein::ResidueID>(atoms_2, inter_atom_contacts_2));
+	auxiliaries::assert_file_header("residue_ids");
+	const std::set<protein::ResidueID> residue_ids_1=auxiliaries::read_set<protein::ResidueID>();
 
 	std::map<protein::ResidueID, contacto::ResidueContactAreaDifferenceScore> residue_contact_area_difference_profile;
 	if(scoring_mode==0)
@@ -50,11 +37,6 @@ void main_calculate_contact_area_difference_profile(const auxiliaries::CommandLi
 	{
 		residue_contact_area_difference_profile=
 				contacto::construct_residue_contact_area_difference_profile<protein::ResidueID, contacto::SimpleDifferenceProducer, contacto::SummingReferenceProducer>(combined_inter_residue_contacts, residue_ids_1);
-	}
-	else if(scoring_mode==3)
-	{
-		residue_contact_area_difference_profile=
-				contacto::construct_residue_contact_area_difference_profile<protein::ResidueID, contacto::SimpleDifferenceProducer, contacto::SummingReferenceProducer>(combined_inter_residue_contacts, contacto::intersect_two_sets(residue_ids_1, protein::collect_residue_ids_from_atoms(atoms_2)));
 	}
 	else
 	{
