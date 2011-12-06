8712f077b31fb1ca8b3a6dea94281aee4012153d
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2011-12-06 11:30:32 +0200

    Incorporated filtering inter residue contacts and refactored cad score scripts.

--- scripts/job.bash
+++ scripts/job.bash
@@ -2,7 +2,6 @@
 
 JOB_DIRECTORY=$1
 
-./score_many_many.bash ./cad_score.bash $JOB_DIRECTORY/target "*" $JOB_DIRECTORY/model "*" $JOB_DIRECTORY/output
+./score_many_many.bash $JOB_DIRECTORY/output $JOB_DIRECTORY/target "*" $JOB_DIRECTORY/model "*" $JOB_DIRECTORY/filter "*"
 
 ./assemble_scores_table.bash $JOB_DIRECTORY/output "*summary_table" "sm0_SS" > $JOB_DIRECTORY/output/scores
-
