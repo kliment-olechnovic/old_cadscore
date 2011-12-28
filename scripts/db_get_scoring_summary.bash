83cbf2d3620b7b9e270ed91f1eb3e7eecef4f77f
kliment <kliment@localhost>
2011-12-28 18:55:44 +0200

    Added scoring summary getting script.

---
+++ scripts/db_get_scoring_summary.bash
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+###########################################
+
+print_help()
+{
+cat << EOF 1>&2
+
+$0 options:
+
+  -h    show this message and exit
+  -D    path to existing database directory
+  
+EOF
+}
+
+DATABASE=""
+
+while getopts “hD:” OPTION
+do
+  case $OPTION in
+    h)
+      print_help
+      exit 0
+      ;;
+    D)
+      DATABASE=$OPTARG
+      ;;
+    ?)
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "$DATABASE" ]
+then
+  print_help
+  exit 1
+fi
+
+if [ ! -d "$DATABASE" ]
+then
+  echo "Database directory \"$DATABASE\" does not exist" 1>&2
+  exit 1
+fi
+
+###########################################
+
+cat $DATABASE/scoring_summary/table
