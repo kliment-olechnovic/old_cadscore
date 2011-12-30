c26a250617c3377afb273d25934d7cdf7c79132d
kliment <kliment@localhost>
2011-12-30 16:05:19 +0200

    Added tangent plane drawing.

--- src/main_draw_spheres_tangency_demo.cpp
+++ src/main_draw_spheres_tangency_demo.cpp
@@ -3,6 +3,7 @@
 #include "protein/atom.h"
 
 #include "apollo/spheres_tangent_sphere.h"
+#include "apollo/spheres_tangent_plane.h"
 
 #include "auxiliaries/command_line_options.h"
 
@@ -12,12 +13,30 @@ void draw_sphere(const T& s, const int quality, const int wired)
 	std::cout << "sphere " << s.x << " " << s.y << " " << s.z << " " << s.r << " " << quality << " " << wired << "\n";
 }
 
+template<typename T>
+void print_point(const T& p)
+{
+	std::cout << p.x << " " << p.y << " " << p.z << " ";
+}
+
+template<typename T>
+void draw_triangle(const T& a, const T& b, const T& c, const T& n)
+{
+	std::cout << "triangle ";
+	print_point(a);
+	print_point(b);
+	print_point(c);
+	std::cout << "1 ";
+	print_point(n);
+	std::cout << "\n";
+}
+
 void main_draw_spheres_tangency_demo(const auxiliaries::CommandLineOptions& clo)
 {
 	typedef apollo::SimpleSphere Sphere;
+	typedef apollo::SimplePoint Point;
 
 	std::cout << "color 1 1 1\n";
-
 	const Sphere a( 0.0, 0.6, 0.0, 0.5);
 	const Sphere b( 0.5,-0.3, 0.0, 0.4);
 	const Sphere c(-0.5,-0.3, 0.0, 0.1);
@@ -25,14 +44,44 @@ void main_draw_spheres_tangency_demo(const auxiliaries::CommandLineOptions& clo)
 	draw_sphere(b, 3, 0);
 	draw_sphere(c, 3, 0);
 
-	std::cout << "color 1 1 0\n";
-	Sphere d( 0.0, 0.0, 0.0, 0.15);
-	for(d.z=-0.7;d.z<=0.7;d.z+=0.05)
+	if(clo.isopt("--ts"))
 	{
+		Sphere d(0.0, 0.0, 0.7, 0.15);
+		draw_sphere(d, 3, 0);
+		std::cout << "color 1 1 0\n";
 		const std::vector<Sphere> ts=apollo::construct_spheres_tangent<Sphere>(a, b, c, d);
 		for(std::size_t i=0;i<ts.size();i++)
 		{
 			draw_sphere(ts[0], 3, 0);
+		}
+	}
+
+	if(clo.isopt("--dc"))
+	{
+		std::cout << "color 1 1 0\n";
+		Sphere d(0.0, 0.0, 0.0, 0.15);
+		for(d.z=-0.7;d.z<=0.7;d.z+=0.05)
+		{
+			const std::vector<Sphere> ts=apollo::construct_spheres_tangent<Sphere>(a, b, c, d);
+			for(std::size_t i=0;i<ts.size();i++)
+			{
+				draw_sphere(ts[0], 3, 0);
+			}
+		}
+	}
+
+	if(clo.isopt("--tp"))
+	{
+		std::cout << "color 0 1 0\n";
+		const std::vector<Point> ps=apollo::construct_spheres_tangent_planes_normals<Point>(a, b, c);
+		for(std::size_t i=0;i<ps.size();i++)
+		{
+			const Point& p=ps[i];
+			draw_triangle(
+					apollo::custom_point_from_object<Point>(a)+(p*a.r),
+					apollo::custom_point_from_object<Point>(b)+(p*b.r),
+					apollo::custom_point_from_object<Point>(c)+(p*c.r),
+					p);
 		}
 	}
 
