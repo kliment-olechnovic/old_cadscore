ee97c458f6fddc8c815b8ec011d9c9ac257b57e5
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2012-01-05 11:06:04 +0200

    Improved hierarchy drawing.

--- src/main_draw_spheres_hierarchy.cpp
+++ src/main_draw_spheres_hierarchy.cpp
@@ -34,23 +34,25 @@ void main_draw_spheres_hierarchy(const auxiliaries::CommandLineOptions& clo)
 		std::cout << "scale " << s.x << " " << s.y << " " << s.z << " " << s.r << "\n";
 	}
 
-	std::cout << "color 1 0 0\n";
+	std::cout << "background 1 1 1\n";
+
+	std::cout << "color 0.4 0.6 1.0\n";
 	for(std::size_t i=0;i<atoms.size();i++)
 	{
-		draw_sphere(atoms[i], 2, 0);
+		draw_sphere(atoms[i], 3, 0);
 	}
 	std::cout << "flush\n";
 	std::cout << "sleep 1000\n";
 	std::cout.flush();
 
-	std::cout << "color 1 1 1\n";
+	std::cout << "color 0.4 1 0.5\n";
 	for(std::size_t i=0;i<spheres_layers.size();i++)
 	{
 		std::cout << "$layer" << i << "\n";
 		const std::vector<apollo::SimpleSphere>& spheres_layer=spheres_layers[i];
 		for(std::size_t j=0;j<spheres_layer.size();j++)
 		{
-			draw_sphere(spheres_layer[j], 2, 1);
+			draw_sphere(spheres_layer[j], 3, 0);
 		}
 		std::cout << "flush\n";
 		std::cout << "sleep 1000\n";
