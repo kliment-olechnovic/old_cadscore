bb736ab64e869f3e7c74a80ed236d3d2dd432919
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2012-01-15 23:58:25 +0200

    Added Voronoi vertices count difference plotting.

--- speedtest/plot_summary.R
+++ speedtest/plot_summary.R
@@ -8,6 +8,8 @@ vq00=vq00[order(vq00$atoms),];
 vq10=vq10[order(vq10$atoms),];
 qtf=qtf[order(qtf$atoms),];
 
+####################
+
 png("summary/atoms_vs_time.png", width=7, height=7, units="in", res=300);
 
 plot(x=vq01$atoms, y=vq01$real, col="purple", type="l", main="Calculation time", xlab="Atoms", ylab="Time");
@@ -21,5 +23,23 @@ points(x=vq10$atoms, y=vq10$real, col="green", type="p", pch=16);
 
 points(x=qtf$atoms, y=qtf$real, col="red", type="l");
 points(x=qtf$atoms, y=qtf$real, col="red", type="p", pch=16);
+
+dev.off();
+
+####################
+
+png("summary/atoms_vs_vertices.png", width=7, height=7, units="in", res=300);
+
+plot(ylim=c(min(vq10$vertices-qtf$vertices), max(vq01$vertices-qtf$vertices)), x=vq01$atoms, y=vq01$vertices-qtf$vertices, col="purple", type="l", main="Voronoi vertices count difference", xlab="Atoms", ylab="Difference");
+points(x=vq01$atoms, y=vq01$vertices-qtf$vertices, col="purple", type="p", pch=16);
+
+points(x=vq00$atoms, y=vq00$vertices-qtf$vertices, col="blue", type="l");
+points(x=vq00$atoms, y=vq00$vertices-qtf$vertices, col="blue", type="p", pch=16);
+
+points(x=vq10$atoms, y=vq10$vertices-qtf$vertices, col="green", type="l");
+points(x=vq10$atoms, y=vq10$vertices-qtf$vertices, col="green", type="p", pch=16);
+
+points(x=qtf$atoms, y=qtf$vertices-qtf$vertices, col="red", type="l");
+points(x=qtf$atoms, y=qtf$vertices-qtf$vertices, col="red", type="p", pch=16);
 
 dev.off();
