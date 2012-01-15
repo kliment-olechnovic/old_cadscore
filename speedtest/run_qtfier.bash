f62a8d070af416a0036830476006ae99fc92c1c4
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2012-01-15 22:25:06 +0200

    Improved speedtest.

--- speedtest/run_qtfier.bash
+++ speedtest/run_qtfier.bash
@@ -17,6 +17,7 @@ OUTPUTFILEBASE=$(basename $INPUTFILE .pdb)
 OUTPUTFILE="$OUTPUTDIR/$OUTPUTFILEBASE.a.qtf"
 OUTPUTLOG="$OUTPUTFILE.log"
 
-(echo "input $INPUTFILE" >&2 ; time -p $QTFIER $INPUTFILE $OUTPUTDIR) > /dev/null 2> $OUTPUTLOG
+(echo "input $INPUTFILE" | sed 's/\.*\S*\/\(\S*\).pdb/\1/' >&2 ; time -p $QTFIER $INPUTFILE $OUTPUTDIR) > /dev/null 2> $OUTPUTLOG
 cat $OUTPUTFILE | egrep '^MODEL' | awk '{print "atoms",$3}' >> $OUTPUTLOG
 cat $OUTPUTFILE | egrep '^MODEL' | awk '{print "vertices",$4}' >> $OUTPUTLOG
+for i in 1 2 ; do cat $OUTPUTLOG | cut --delimiter " " --fields $i | paste -s ; done > $OUTPUTLOG.table
--- speedtest/run_voroprot2.bash
+++ speedtest/run_voroprot2.bash
@@ -29,4 +29,5 @@ done
 OUTPUTFILE="$INPUTFILE.vq.p$ASPOINTS.s$SD3.out"
 OUTPUTFILELOG="$OUTPUTFILE.log"
 
-(echo "input $INPUTFILE" >&2 ; time -p cat $INPUTFILE | $VOROPROT2 --mode collect-atoms --radius-classes ../resources/vdwr_classes_simple.txt --radius-members ../resources/vdwr_members_simple.txt --include-heteroatoms 1 --include-water 0 | $VOROPROT2 --mode construct-apollonius-quadrupalization --radius $RADIUS --low-count $LOWCOUNT --as-points $ASPOINTS --search-for-d3 $SD3 --check-for-orphans) > $OUTPUTFILE 2> $OUTPUTFILELOG
+(echo "input $INPUTFILE" | sed 's/\.*\S*\/\(\S*\).pdb/\1/' >&2 ; time -p cat $INPUTFILE | $VOROPROT2 --mode collect-atoms --radius-classes ../resources/vdwr_classes_simple.txt --radius-members ../resources/vdwr_members_simple.txt --include-heteroatoms 1 --include-water 0 | $VOROPROT2 --mode construct-apollonius-quadrupalization --radius $RADIUS --low-count $LOWCOUNT --as-points $ASPOINTS --search-for-d3 $SD3 --check-for-orphans) > $OUTPUTFILE 2> $OUTPUTFILELOG
+for i in 1 2 ; do cat $OUTPUTFILELOG | cut --delimiter " " --fields $i | paste -s ; done > $OUTPUTFILELOG.table
