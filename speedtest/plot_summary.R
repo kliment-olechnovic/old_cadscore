128a737b3324bff72fb6c66f07c50fa82323162b
kliment <kliment@localhost>
2012-01-15 23:50:33 +0200

    Fixed tables generation and added plotting.

---
+++ speedtest/plot_summary.R
@@ -0,0 +1,25 @@
+vq01=read.table("summary/vq.p0.s1.table", header=TRUE, stringsAsFactors=FALSE);
+vq00=read.table("summary/vq.p0.s0.table", header=TRUE, stringsAsFactors=FALSE);
+vq10=read.table("summary/vq.p1.s0.table", header=TRUE, stringsAsFactors=FALSE);
+qtf=read.table("summary/qtf.table", header=TRUE, stringsAsFactors=FALSE);
+
+vq01=vq01[order(vq01$atoms),];
+vq00=vq00[order(vq00$atoms),];
+vq10=vq10[order(vq10$atoms),];
+qtf=qtf[order(qtf$atoms),];
+
+png("summary/atoms_vs_time.png", width=7, height=7, units="in", res=300);
+
+plot(x=vq01$atoms, y=vq01$real, col="purple", type="l", main="Calculation time", xlab="Atoms", ylab="Time");
+points(x=vq01$atoms, y=vq01$real, col="purple", type="p", pch=16);
+
+points(x=vq00$atoms, y=vq00$real, col="blue", type="l");
+points(x=vq00$atoms, y=vq00$real, col="blue", type="p", pch=16);
+
+points(x=vq10$atoms, y=vq10$real, col="green", type="l");
+points(x=vq10$atoms, y=vq10$real, col="green", type="p", pch=16);
+
+points(x=qtf$atoms, y=qtf$real, col="red", type="l");
+points(x=qtf$atoms, y=qtf$real, col="red", type="p", pch=16);
+
+dev.off();
