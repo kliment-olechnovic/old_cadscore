0e916df970c5bc59bb81cdd459079bb0d0e4df1a
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2012-01-15 23:50:33 +0200

    Fixed tables generation and added plotting.

--- speedtest/summarize.bash
+++ speedtest/summarize.bash
@@ -2,17 +2,26 @@
 
 DATADIR="./data/"
 SUMMARYDIR="./summary/"
+TEMPFILE="$SUMMARYDIR/tempfile"
 
 mkdir -p $SUMMARYDIR
 
 ./multirun.bash "./run_voroprot2.bash -p 0 -s 1 -f"
-cat $DATADIR/*.vq.p0.s1.out.log.table > $SUMMARYDIR/vq.p0.s1.table
+cat $DATADIR/*.vq.p0.s1.out.log.table > $TEMPFILE
+(cat $TEMPFILE | head -1 ; cat $TEMPFILE | grep -v atoms) > $SUMMARYDIR/vq.p0.s1.table
+rm $TEMPFILE
 
 ./multirun.bash "./run_qtfier.bash"
-cat $DATADIR/*.qtf.log.table > $SUMMARYDIR/qtf.table
+cat $DATADIR/*.qtf.log.table > $TEMPFILE
+(cat $TEMPFILE | head -1 ; cat $TEMPFILE | grep -v atoms) > $SUMMARYDIR/qtf.table
+rm $TEMPFILE
 
 ./multirun.bash "./run_voroprot2.bash -p 0 -s 0 -f"
-cat $DATADIR/*.vq.p0.s0.out.log.table > $SUMMARYDIR/vq.p0.s0.table
+cat $DATADIR/*.vq.p0.s0.out.log.table > $TEMPFILE
+(cat $TEMPFILE | head -1 ; cat $TEMPFILE | grep -v atoms) > $SUMMARYDIR/vq.p0.s0.table
+rm $TEMPFILE
 
 ./multirun.bash "./run_voroprot2.bash -p 1 -s 0 -f"
-cat $DATADIR/*.vq.p1.s0.out.log.table > $SUMMARYDIR/vq.p1.s0.table
+cat $DATADIR/*.vq.p1.s0.out.log.table > $TEMPFILE
+(cat $TEMPFILE | head -1 ; cat $TEMPFILE | grep -v atoms) > $SUMMARYDIR/vq.p1.s0.table
+rm $TEMPFILE
