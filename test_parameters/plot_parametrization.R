92e32593a3a97a20f2c7513162de29b5e82f66b6
kliment <kliment@localhost>
2012-01-20 23:15:31 +0200

    Improved parametrization scripts.

--- test_parameters/plot_parametrization.R
+++ test_parameters/plot_parametrization.R
@@ -1,13 +1,8 @@
-lct=read.table("parametrization/lowcounts.list", header=FALSE, stringsAsFactors=FALSE);
-lc=lct[[2]][which(lct[[1]]=="lowcount")];
-time=lct[[2]][which(lct[[1]]=="real")];
-png("parametrization/lowcount_vs_time.png", width=7, height=7, units="in", res=300);
-plot(x=lc, y=time, col="black", type="l", main="LowCount vs Time", xlab="LowCount", ylab="Time");
-dev.off();
+cmdargs=commandArgs(TRUE);
 
-rt=read.table("parametrization/radii.list", header=FALSE, stringsAsFactors=FALSE);
-r=rt[[2]][which(rt[[1]]=="radius")];
-time=rt[[2]][which(rt[[1]]=="real")];
-png("parametrization/radius_vs_time.png", width=7, height=7, units="in", res=300);
-plot(x=r, y=time, col="black", type="l", main="Radius vs Time", xlab="Radius", ylab="Time");
+t=read.table(cmdargs[1], header=FALSE, stringsAsFactors=FALSE);
+p=t[[2]][which(t[[1]]=="param")];
+v=t[[2]][which(t[[1]]=="user")];
+png(paste(cmdargs[1], ".png", sep=""), width=7, height=7, units="in", res=300);
+plot(x=p, y=v, col="black", type="l", main="Parameter vs Time", xlab="Parameter", ylab="Time");
 dev.off();
