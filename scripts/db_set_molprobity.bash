88ba4b3a13edd92b7d8b987140db6e743a4fe64e
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2012-01-20 10:53:38 +0200

    Incorporated MolProbity calculation.

---
+++ scripts/db_set_molprobity.bash
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+###########################################
+
+print_help()
+{
+cat << EOF 1>&2
+
+$0 options:
+
+  -h    show this message and exit
+  -D    path to writable database directory
+  -m    model file
+
+EOF
+}
+
+DATABASE=""
+MODEL_FILE=""
+
+while getopts "hD:t:m:" OPTION
+do
+  case $OPTION in
+    h)
+      print_help
+      exit 0
+      ;;
+    D)
+      DATABASE=$OPTARG
+      ;;
+    m)
+      MODEL_FILE=$OPTARG
+      ;;
+    ?)
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "$DATABASE" ] || [ -z "$MODEL_FILE" ]
+then
+  print_help
+  exit 1
+fi
+
+if [ -f "$MODEL_FILE" ]
+then
+  MODEL_NAME=$(basename $MODEL_FILE)
+else
+  echo "Model file \"$MODEL_FILE\" does not exist" 1>&2
+  exit 1
+fi
+
+###########################################
+
+OUTPUT_DIRECTORY="$DATABASE/molprobity/$MODEL_NAME"
+mkdir -p $OUTPUT_DIRECTORY
+
+MOLPROBITY_PROFILE_FILE="$OUTPUT_DIRECTORY/molprobity_profile"
+test -f $MOLPROBITY_PROFILE_FILE || molprobity.bash $MODEL_FILE > $MOLPROBITY_PROFILE_FILE 2> "$MOLPROBITY_PROFILE_FILE.log"
+
+MOLPROBITY_SCORE=$(cat $MOLPROBITY_PROFILE_FILE | cut -d":" -f28 | tail -n 1)
+if [ -z "$MOLPROBITY_SCORE" ] ; then MOLPROBITY_SCORE="-1" ; fi
+
+MOLPROBITY_SUMMARY_FILE="$OUTPUT_DIRECTORY/molprobity_summary"
+echo "MolProbityScore $MOLPROBITY_SCORE" > $MOLPROBITY_SUMMARY_FILE
