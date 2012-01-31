dded5e6bd1f35f33a5563b7eb5a817f588ab9340
kliment <kliment@localhost>
2012-01-31 20:55:10 +0200

    Improved molprobity plotting.

--- casp9/plot_molprobity_analysis.R
+++ casp9/plot_molprobity_analysis.R
@@ -32,3 +32,24 @@ for(treshold in tresholds_GDT_TS)
     dev.off();
   }
 }
+
+###########################################
+
+t=read.table("enhanced_table", header=TRUE, stringsAsFactors=FALSE);
+t=t[which(t$MolProbityScore>(0-1)),];
+t=t[which(t$domain>0),];
+
+score_names=c("CASP_GDT_TS", "AA", "SA", "SS");
+for(score_name in score_names)
+{
+  x=t[, score_name];
+  y=t$MolProbityScore;
+  blue_density_colors=densCols(x, y);
+  png(paste(output_directory, "/", "correlation_", score_name, "_with_MolProbity", ".png", sep=""), height=5, width=5, units="in", res=200);
+  plot(x=x, y=y, xlim=c(0, 1), col=blue_density_colors, pch=16, cex=0.5, xlab=score_name, ylab="MolProbityScore", main=paste(score_name, " correlation with MolProbity", sep=""));
+  dev.off();
+}
+
+png(paste(output_directory, "/histogram_of_MolProbity.png", sep=""), height=5, width=5, units="in", res=200);
+hist(t$MolProbityScore);
+dev.off();
