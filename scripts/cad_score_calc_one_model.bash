edd10104729fb8b234d0ec79d0a1af64f61b12ad
kliment <kliment@localhost>
2012-02-09 19:11:52 +0200

    Working new main script.

--- scripts/cad_score_calc_one_model.bash
+++ scripts/cad_score_calc_one_model.bash
@@ -10,26 +10,37 @@ $0 options:
   -D    path to writable database directory
   -t    path to target file in PBD format
   -m    path to model file in PBD format
+  -l    flag to include heteroatoms (optional)
+  -i    flag to consider only interface contacts (optional)
+  -o    max timeout (optional)
 
 EOF
 }
 
-###########################################
-
 SCRIPT_DIRECTORY=$(dirname $0)
-VOROPROT="$SCRIPT_DIRECTORY/voroprot2"
+VOROPROT="$SCRIPT_DIRECTORY/voroprot"
+USE_TMSCORE="$SCRIPT_DIRECTORY/use_tmscore.bash"
 
-if [ ! -f "$TARGET_FILE" ]
+if [ ! -f "$VOROPROT" ]
 then
-  echo "Voroprot executable not found in the script deirectory" 1>&2
+  echo "$VOROPROT does not exist" 1>&2
+  exit 1
+fi
+
+if [ ! -f "$USE_TMSCORE" ]
+then
+  echo "$USE_TMSCORE does not exist" 1>&2
   exit 1
 fi
 
 DATABASE=""
 TARGET_FILE=""
 MODEL_FILE=""
+HETATM_FLAG=""
+INTERFACE_ONLY_FLAG=""
+TIMEOUT="300s"
 
-while getopts "hD:t:m:" OPTION
+while getopts "hD:t:m:lio:" OPTION
 do
   case $OPTION in
     h)
@@ -44,6 +55,15 @@ do
       ;;
     m)
       MODEL_FILE=$OPTARG
+      ;;
+    l)
+      HETATM_FLAG="--HETATM"
+      ;;
+    i)
+      INTERFACE_ONLY_FLAG="--interface-only"
+      ;;
+    o)
+      TIMEOUT=$OPTARG
       ;;
     ?)
       exit 1
@@ -73,8 +93,6 @@ else
   exit 1
 fi
 
-###########################################
-
 TARGET_DIR="$DATABASE/$TARGET_NAME"
 TARGET_ATOMS_FILE="$TARGET_DIR/atoms"
 TARGET_RESIDUE_IDS_FILE="$TARGET_DIR/residue_ids"
@@ -89,22 +107,52 @@ COMBINED_INTER_RESIDUE_CONTACTS_FILE="$MODEL_DIR/combined_inter_residue_contacts
 CAD_PROFILE_FILE="$MODEL_DIR/cad_profile"
 CAD_GLOBAL_SCORES_FILE="$MODEL_DIR/cad_global_scores"
 
+TMSCORE_PROFILE_FILE="$MODEL_DIR/tmscore_profile"
+TMSCORE_GLOBAL_SCORES_FILE="$MODEL_DIR/tmscore_global_scores"
+
+SUMMARY_FILE="$MODEL_DIR/summary"
+LOGS_FILE="$MODEL_DIR/logs"
+
 mkdir -p "$TARGET_DIR"
 mkdir -p "$MODEL_DIR"
 
-test -f $TARGET_ATOMS_FILE || cat $TARGET_FILE | $VOROPROT --mode collect-atoms --force-one-chain > "$TARGET_ATOMS_FILE" 2> "$TARGET_ATOMS_FILE.log"
-test -f $TARGET_RESIDUE_IDS_FILE || cat $TARGET_ATOMS_FILE | $VOROPROT --mode collect-residue-ids  > "$TARGET_RESIDUE_IDS_FILE" 2> "$TARGET_RESIDUE_IDS_FILE.log"
-test -f $TARGET_INTER_ATOM_CONTACTS_FILE || cat $TARGET_ATOMS_FILE | timeout 300s $VOROPROT --mode calc-inter-atom-contacts > "$TARGET_INTER_ATOM_CONTACTS_FILE" 2> "$TARGET_INTER_ATOM_CONTACTS_FILE.log"
+test -f $TARGET_ATOMS_FILE || cat $TARGET_FILE | $VOROPROT --mode collect-atoms $HETATM_FLAG > $TARGET_ATOMS_FILE 2> "$TARGET_ATOMS_FILE.log"
+test -f $TARGET_RESIDUE_IDS_FILE || cat $TARGET_ATOMS_FILE | $VOROPROT --mode collect-residue-ids  > $TARGET_RESIDUE_IDS_FILE 2> "$TARGET_RESIDUE_IDS_FILE.log"
+test -f $TARGET_INTER_ATOM_CONTACTS_FILE || cat $TARGET_ATOMS_FILE | timeout 300s $VOROPROT --mode calc-inter-atom-contacts > $TARGET_INTER_ATOM_CONTACTS_FILE 2> "$TARGET_INTER_ATOM_CONTACTS_FILE.log"
 
-if [ ! -f $MODEL_ATOMS_FILE ]
+if [ ! -f "$MODEL_ATOMS_FILE" ]
 then
-  cat $MODEL_FILE | $VOROPROT --mode collect-atoms --force-one-chain > "$MODEL_ATOMS_FILE" 2> "$MODEL_ATOMS_FILE.log"
-  cat $TARGET_ATOMS_FILE $MODEL_ATOMS_FILE | $VOROPROT --mode filter-atoms-by-target > "$MODEL_ATOMS_FILE.filtered" 2>> "$MODEL_ATOMS_FILE.log"
+  cat $MODEL_FILE | $VOROPROT --mode collect-atoms $HETATM_FLAG > $MODEL_ATOMS_FILE 2> "$MODEL_ATOMS_FILE.log"
+  cat $MODEL_ATOMS_FILE $TARGET_INTER_ATOM_CONTACTS_FILE | $VOROPROT --mode filter-atoms-by-target > "$MODEL_ATOMS_FILE.filtered" 2>> "$MODEL_ATOMS_FILE.log"
   mv "$MODEL_ATOMS_FILE.filtered" $MODEL_ATOMS_FILE
 fi
-test -f $MODEL_RESIDUE_IDS_FILE || cat $MODEL_ATOMS_FILE | $VOROPROT --mode collect-residue-ids  > "$MODEL_RESIDUE_IDS_FILE" 2> "$MODEL_RESIDUE_IDS_FILE.log"
-test -f $MODEL_INTER_ATOM_CONTACTS_FILE || cat $MODEL_ATOMS_FILE | timeout 300s $VOROPROT --mode calc-inter-atom-contacts > "$MODEL_INTER_ATOM_CONTACTS_FILE" 2> "$MODEL_INTER_ATOM_CONTACTS_FILE.log"
+test -f $MODEL_RESIDUE_IDS_FILE || cat $MODEL_ATOMS_FILE | $VOROPROT --mode collect-residue-ids  > $MODEL_RESIDUE_IDS_FILE 2> "$MODEL_RESIDUE_IDS_FILE.log"
+test -f $MODEL_INTER_ATOM_CONTACTS_FILE || cat $MODEL_ATOMS_FILE | timeout 300s $VOROPROT --mode calc-inter-atom-contacts > $MODEL_INTER_ATOM_CONTACTS_FILE 2> "$MODEL_INTER_ATOM_CONTACTS_FILE.log"
 
-test -f $COMBINED_INTER_RESIDUE_CONTACTS_FILE || cat $TARGET_INTER_ATOM_CONTACTS_FILE $MODEL_INTER_ATOM_CONTACTS_FILE | $VOROPROT --mode calc-combined-inter-residue-contacts > $COMBINED_INTER_RESIDUE_CONTACTS_FILE 2> "$COMBINED_INTER_RESIDUE_CONTACTS_FILE.log"
+test -f $COMBINED_INTER_RESIDUE_CONTACTS_FILE || cat $TARGET_INTER_ATOM_CONTACTS_FILE $MODEL_INTER_ATOM_CONTACTS_FILE | $VOROPROT --mode calc-combined-inter-residue-contacts $INTERFACE_ONLY_FLAG > $COMBINED_INTER_RESIDUE_CONTACTS_FILE 2> "$COMBINED_INTER_RESIDUE_CONTACTS_FILE.log"
 test -f $CAD_PROFILE_FILE || cat $COMBINED_INTER_RESIDUE_CONTACTS_FILE $TARGET_RESIDUE_IDS_FILE | $VOROPROT --mode calc-CAD-profile > $CAD_PROFILE_FILE 2> "$CAD_PROFILE_FILE.log"
 test -f $CAD_GLOBAL_SCORES_FILE || cat $CAD_PROFILE_FILE | $VOROPROT --mode calc-CAD-global-scores > $CAD_GLOBAL_SCORES_FILE 2> "$CAD_GLOBAL_SCORES_FILE.log"
+
+test -f $TMSCORE_GLOBAL_SCORES_FILE || $USE_TMSCORE -m $MODEL_FILE -t $TARGET_FILE -p $TMSCORE_PROFILE_FILE -s $TMSCORE_GLOBAL_SCORES_FILE
+
+if [ -s "$CAD_GLOBAL_SCORES_FILE" ] && [ -s "$TMSCORE_GLOBAL_SCORES_FILE" ] && [ -s "$TARGET_ATOMS_FILE" ] && [ -s "$MODEL_ATOMS_FILE" ] && [ -s "$TARGET_RESIDUE_IDS_FILE" ] && [ -s "$MODEL_RESIDUE_IDS_FILE" ]
+then
+  echo target $TARGET_NAME > $SUMMARY_FILE
+  echo model $MODEL_NAME >> $SUMMARY_FILE
+  echo target_atoms_count `sed -n '2p' $TARGET_ATOMS_FILE` >> $SUMMARY_FILE
+  echo model_atoms_count `sed -n '2p' $MODEL_ATOMS_FILE` >> $SUMMARY_FILE
+  echo target_residues_count `sed -n '2p' $TARGET_RESIDUE_IDS_FILE` >> $SUMMARY_FILE
+  echo model_residues_count `sed -n '2p' $MODEL_RESIDUE_IDS_FILE` >> $SUMMARY_FILE
+  cat $CAD_GLOBAL_SCORES_FILE >> $SUMMARY_FILE
+  cat $TMSCORE_GLOBAL_SCORES_FILE >> $SUMMARY_FILE
+else
+  if [ -f "$SUMMARY_FILE" ]
+  then
+    rm $SUMMARY_FILE
+  fi
+fi
+
+cat "$TARGET_ATOMS_FILE.log" "$TARGET_RESIDUE_IDS_FILE.log" "$TARGET_INTER_ATOM_CONTACTS_FILE.log" > $LOGS_FILE
+cat "$MODEL_ATOMS_FILE.log" "$MODEL_RESIDUE_IDS_FILE.log" "$MODEL_INTER_ATOM_CONTACTS_FILE.log" >> $LOGS_FILE
+cat "$COMBINED_INTER_RESIDUE_CONTACTS_FILE.log" "$CAD_PROFILE_FILE.log" "$CAD_GLOBAL_SCORES_FILE.log" >> $LOGS_FILE
+cat "$TMSCORE_PROFILE_FILE.log" >> $LOGS_FILE
---
+++ scripts/use_tmscore.bash
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+print_help()
+{
+cat << EOF 1>&2
+
+$0 options:
+
+  -h    show this message and exit
+  -t    path to target file in PBD format
+  -m    path to model file in PBD format
+  -p    profile output file
+  -s    summary output file
+
+EOF
+}
+
+TARGET_FILE=""
+MODEL_FILE=""
+TMSCORE_PROFILE_FILE=""
+TMSCORE_SUMMARY_FILE=""
+
+while getopts "ht:m:p:s:" OPTION
+do
+  case $OPTION in
+    h)
+      print_help
+      exit 0
+      ;;
+    t)
+      TARGET_FILE=$OPTARG
+      ;;
+    m)
+      MODEL_FILE=$OPTARG
+      ;;
+    p)
+      TMSCORE_PROFILE_FILE=$OPTARG
+      ;;
+    s)
+      TMSCORE_SUMMARY_FILE=$OPTARG
+      ;;
+    ?)
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "$TARGET_FILE" ] || [ -z "$MODEL_FILE" ] || [ -z "$TMSCORE_PROFILE_FILE" ] || [ -z "$TMSCORE_SUMMARY_FILE" ]
+then
+  print_help
+  exit 1
+fi
+
+if [ ! -f "$TARGET_FILE" ]
+then
+  echo "Target file \"$TARGET_FILE\" does not exist" 1>&2
+  exit 1
+fi
+
+if [ ! -f "$MODEL_FILE" ]
+then
+  echo "Model file \"$MODEL_FILE\" does not exist" 1>&2
+  exit 1
+fi
+
+TEMP_SHORT_TARGET_FILE=$(mktemp)
+TEMP_SHORT_MODEL_FILE=$(mktemp)
+
+cp $TARGET_FILE $TEMP_SHORT_TARGET_FILE
+cp $MODEL_FILE $TEMP_SHORT_MODEL_FILE
+
+TMscore $TEMP_SHORT_MODEL_FILE $TEMP_SHORT_TARGET_FILE > $TMSCORE_PROFILE_FILE 2> "$TMSCORE_PROFILE_FILE.log"
+
+rm $TEMP_SHORT_MODEL_FILE $TEMP_SHORT_TARGET_FILE
+
+TM_SCORE=$(cat $TMSCORE_PROFILE_FILE | egrep "TM-score\s*=.*d0" | sed 's/TM-score\s*=\s*\(.*\)\s*(.*/\1/g')
+if [ -z "$TM_SCORE" ] ; then TM_SCORE=0 ; fi
+
+TM_SCORE_GDT_TS=$(cat $TMSCORE_PROFILE_FILE | egrep "GDT-TS-score" | sed 's/GDT-TS-score\s*=\s*\(.*\)\s*%(d<1).*/\1/g')
+if [ -z "$TM_SCORE_GDT_TS" ] ; then TM_SCORE_GDT_TS=0 ; fi
+
+TM_SCORE_GDT_HA=$(cat $TMSCORE_PROFILE_FILE | egrep "GDT-HA-score" | sed 's/GDT-HA-score\s*=\s*\(.*\)\s*%(d<0\.5).*/\1/g')
+if [ -z "$TM_SCORE_GDT_HA" ] ; then TM_SCORE_GDT_HA=0 ; fi
+
+echo "TM_score $TM_SCORE" > $TMSCORE_SUMMARY_FILE
+echo "TM_score_GDT_TS $TM_SCORE_GDT_TS" >> $TMSCORE_SUMMARY_FILE
+echo "TM_score_GDT_HA $TM_SCORE_GDT_HA" >> $TMSCORE_SUMMARY_FILE
