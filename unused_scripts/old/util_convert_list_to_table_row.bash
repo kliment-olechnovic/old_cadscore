43a0878ca24c3c585aabeaf141cb7f12c0327be9
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2012-02-10 09:13:12 +0200

    Refactored directory structure.

--- unused_scripts/old/util_convert_list_to_table_row.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-###########################################
-
-LIST_FILE=$1
-
-###########################################
-
-for i in 1 2 ; do cat $LIST_FILE | cut --delimiter " " --fields $i | paste -s ; done
