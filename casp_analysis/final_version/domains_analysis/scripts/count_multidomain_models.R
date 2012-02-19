2537e54ce599a58def779efa1e53370599adcde4
kliment <kliment@localhost>
2012-02-19 22:22:53 +0200

    Corrected multidomain models count.

--- casp_analysis/final_version/domains_analysis/scripts/count_multidomain_models.R
+++ casp_analysis/final_version/domains_analysis/scripts/count_multidomain_models.R
@@ -1,17 +1,30 @@
 t=read.table("../prepared_data/enhanced_table", header=TRUE, stringsAsFactors=FALSE);
 
-names=c();
-numbers=c();
+multidomain_targets=0;
+multidomain_models=0;
 
 second_domains_set=t$target[which(t$domain==2)];
 targets_set=union(second_domains_set, second_domains_set);
 
-names=c(names, "multidomain_targets");
-numbers=c(numbers, length(targets_set));
+for(target in targets_set)
+{
+  st=t[which(t$target==target),];
+  domains_set=sort(union(st$domain, st$domain));
+  if(length(domains_set)>2)
+  {
+    multidomain_targets=multidomain_targets+1;
+    groups_set=sort(union(st$group, st$group));
+    for(g in groups_set)
+    {
+      stg=st[which(st$group==g),];
+      if(length(union(stg$domain, stg$domain))==length(domains_set))
+      {
+        multidomain_models=multidomain_models+1;
+      }
+    }
+  }
+}
 
-names=c(names, "multidomain_models");
-numbers=c(numbers, length(second_domains_set));
-
-result=data.frame(names=names, numbers=numbers);
+result=data.frame(names=c("multidomain_targets", "multidomain_models"), numbers=c(multidomain_targets, multidomain_models));
 
 write.table(result, "multidomain_models_counts", quote=FALSE, row.names=FALSE, col.names=FALSE);
