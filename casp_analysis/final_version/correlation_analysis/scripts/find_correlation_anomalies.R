ca175684cfbd5226a7be14bf967623ff26d6bfec
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2012-02-29 21:15:44 +0200

    Improved correlation anomalies finding.

--- casp_analysis/final_version/correlation_analysis/scripts/find_correlation_anomalies.R
+++ casp_analysis/final_version/correlation_analysis/scripts/find_correlation_anomalies.R
@@ -1,6 +1,9 @@
 t=read.table("../prepared_data/enhanced_table", header=TRUE, stringsAsFactors=FALSE);
 t=t[which(t$domain>0),];
 
+output_directory="correlation_anomalies";
+dir.create(output_directory);
+
 ###################
 
 x=c(0.425, 1.0);
@@ -11,11 +14,12 @@ b=y[1]-a*x[1];
 
 st=t[which((t$SS-(t$CASP_GDT_TS*a+b))<0.0),]
 
-write.table(st, "correlation_anomalies_for_low_SS_high_GDT_TS", quote=FALSE, row.names=FALSE);
+write.table(st, paste(output_directory, "/correlation_anomalies_for_low_SS_high_GDT_TS", sep=""), quote=FALSE, row.names=FALSE);
 
-png("correlation_anomalies_for_low_SS_high_GDT_TS.png", height=7, width=7, units="in", res=200);
-plot(x=st$CASP_GDT_TS, y=st$SS, xlim=c(0, 1), ylim=c(0, 1), col="black", pch=16, cex=0.5, xlab="GDT_TS", ylab="SS");
-points(x=x, y=y, col="black", type="l");
+png(paste(output_directory, "/correlation_anomalies_for_low_SS_high_GDT_TS.png", sep=""), height=7, width=7, units="in", res=200);
+plot(x=t$CASP_GDT_TS, y=t$SS, xlim=c(0, 1), ylim=c(0, 1), col="black", pch=16, cex=0.5, xlab="GDT_TS", ylab="SS");
+points(x=st$CASP_GDT_TS, y=st$SS, col="red", pch=16, cex=0.5);
+points(x=c(-1, 2), y=c(-1, 2)*a+b, col="blue", type="l");
 dev.off();
 
 ###################
@@ -28,9 +32,10 @@ b=y[1]-a*x[1];
 
 st=t[which((t$AA-(t$CASP_GDT_TS*a+b))>0.0),]
 
-write.table(st, "correlation_anomalies_for_high_AA_low_GDT_TS", quote=FALSE, row.names=FALSE);
+write.table(st, paste(output_directory, "/correlation_anomalies_for_high_AA_low_GDT_TS", sep=""), quote=FALSE, row.names=FALSE);
 
-png("correlation_anomalies_for_high_AA_low_GDT_TS.png", height=7, width=7, units="in", res=200);
-plot(x=st$CASP_GDT_TS, y=st$AA, xlim=c(0, 1), ylim=c(0, 1), col="black", pch=16, cex=0.5, xlab="GDT_TS", ylab="SS");
-points(x=x, y=y, col="black", type="l");
+png(paste(output_directory, "/correlation_anomalies_for_high_AA_low_GDT_TS.png", sep=""), height=7, width=7, units="in", res=200);
+plot(x=t$CASP_GDT_TS, y=t$AA, xlim=c(0, 1), ylim=c(0, 1), col="black", pch=16, cex=0.5, xlab="GDT_TS", ylab="AA");
+points(x=st$CASP_GDT_TS, y=st$AA, col="red", pch=16, cex=0.5);
+points(x=c(-1, 2), y=c(-1, 2)*a+b, col="blue", type="l");
 dev.off();
