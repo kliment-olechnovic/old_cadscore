d464756695cc83b42c715f10e412e55db0ed1245
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2012-02-29 16:25:50 +0200

    Improved correlation anomalies finding.

--- casp_analysis/final_version/correlation_analysis/scripts/find_correlation_anomalies.R
+++ casp_analysis/final_version/correlation_analysis/scripts/find_correlation_anomalies.R
@@ -1,9 +1,36 @@
 t=read.table("../prepared_data/enhanced_table", header=TRUE, stringsAsFactors=FALSE);
 t=t[which(t$domain>0),];
 
-sel1=which(t$AA>0.5);
-sel2=sel1[which(t$CASP_GDT_TS[sel1]<0.4)];
+###################
 
-t=t[sel2,];
+x=c(0.425, 1.0);
+y=c(0.0, 0.4);
 
-write.table(t, "correlation_anomalies", quote=FALSE, row.names=FALSE);
+a=(y[1]-y[2])/(x[1]-x[2]);
+b=y[1]-a*x[1];
+
+st=t[which((t$SS-(t$CASP_GDT_TS*a+b))<0.0),]
+
+write.table(st, "correlation_anomalies_for_low_SS_high_GDT_TS", quote=FALSE, row.names=FALSE);
+
+png("correlation_anomalies_for_low_SS_high_GDT_TS.png", height=7, width=7, units="in", res=200);
+plot(x=st$CASP_GDT_TS, y=st$SS, xlim=c(0, 1), ylim=c(0, 1), col="black", pch=16, cex=0.5, xlab="GDT_TS", ylab="SS");
+points(x=x, y=y, col="black", type="l");
+dev.off();
+
+###################
+
+x=c(0.2, 0.4);
+y=c(0.5, 0.6);
+
+a=(y[1]-y[2])/(x[1]-x[2]);
+b=y[1]-a*x[1];
+
+st=t[which((t$AA-(t$CASP_GDT_TS*a+b))>0.0),]
+
+write.table(st, "correlation_anomalies_for_high_AA_low_GDT_TS", quote=FALSE, row.names=FALSE);
+
+png("correlation_anomalies_for_high_AA_low_GDT_TS.png", height=7, width=7, units="in", res=200);
+plot(x=st$CASP_GDT_TS, y=st$AA, xlim=c(0, 1), ylim=c(0, 1), col="black", pch=16, cex=0.5, xlab="GDT_TS", ylab="SS");
+points(x=x, y=y, col="black", type="l");
+dev.off();
