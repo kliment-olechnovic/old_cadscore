01d67f83b4cbd633255a3d20aa670b191f1fb925
kliment <kliment@localhost>
2012-07-02 13:28:30 +0300

    Improved MolProbity analysis scripts.

--- analysis/casp9/molprobity_analysis/scripts/plot_molprobity_distribution_analysis.R
+++ analysis/casp9/molprobity_analysis/scripts/plot_molprobity_distribution_analysis.R
@@ -6,12 +6,17 @@ t=t[which(t$SS!=0),];
 output_directory="distribution";
 dir.create(output_directory);
 
-png(paste(output_directory, "/histogram_of_MolProbity_differences.png", sep=""), height=5, width=5, units="in", res=200);
-hist(t$MolProbity, breaks=100, col="red", main="", xlab="", ylab="");
+scores_names=c("MolProbity", "GDT_TS", "AA", "AS", "SS");
+
+for(score_name in scores_names)
+{
+png(paste(output_directory, "/histogram_of_", score_name, "_differences.png", sep=""), height=5, width=5, units="in", res=200);
+hist(t[, score_name], breaks=100, col="red", main="", xlab="", ylab="");
 usr=par("usr");
-clip(usr[1], (0-sd(t$MolProbity)), usr[3], usr[4]);
-hist(t$MolProbity, breaks=100, col="green", add=TRUE);
-clip(sd(t$MolProbity), usr[2], usr[3], usr[4]);
-hist(t$MolProbity, breaks=100, col="green", add=TRUE);
+clip(usr[1], (0-sd(t[, score_name])), usr[3], usr[4]);
+hist(t[, score_name], breaks=100, col="green", add=TRUE);
+clip(sd(t[, score_name]), usr[2], usr[3], usr[4]);
+hist(t[, score_name], breaks=100, col="green", add=TRUE);
 do.call("clip", as.list(usr))
 dev.off();
+}
