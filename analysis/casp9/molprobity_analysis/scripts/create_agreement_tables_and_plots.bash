ca2fda190eb4e833d1c9a02c4f4472430d53977c
kliment <kliment@localhost>
2012-07-02 14:30:22 +0300

    Improved MolProbity analysis scripts.

--- analysis/casp9/molprobity_analysis/scripts/create_agreement_tables_and_plots.bash
+++ analysis/casp9/molprobity_analysis/scripts/create_agreement_tables_and_plots.bash
@@ -9,11 +9,14 @@ R --vanilla --args "judging_from_1" < scripts/plot_molprobity_judging_analysis.R
 R --vanilla --args "sd" < scripts/create_agreement_table.R
 R --vanilla --args "judging_from_sd" < scripts/plot_molprobity_judging_analysis.R
 
-R --vanilla --args "sdall" < scripts/create_agreement_table.R
-R --vanilla --args "judging_from_sdall" < scripts/plot_molprobity_judging_analysis.R
+R --vanilla --args "sd_or" < scripts/create_agreement_table.R
+R --vanilla --args "judging_from_sd_or" < scripts/plot_molprobity_judging_analysis.R
 
-R --vanilla --args "sdgdt" < scripts/create_agreement_table.R
-R --vanilla --args "judging_from_sdgdt" < scripts/plot_molprobity_judging_analysis.R
+R --vanilla --args "sd_and" < scripts/create_agreement_table.R
+R --vanilla --args "judging_from_sd_and" < scripts/plot_molprobity_judging_analysis.R
 
-R --vanilla --args "sdcad" < scripts/create_agreement_table.R
-R --vanilla --args "judging_from_sdcad" < scripts/plot_molprobity_judging_analysis.R
+R --vanilla --args "sd_gdt" < scripts/create_agreement_table.R
+R --vanilla --args "judging_from_sd_gdt" < scripts/plot_molprobity_judging_analysis.R
+
+R --vanilla --args "sd_cad" < scripts/create_agreement_table.R
+R --vanilla --args "judging_from_sd_cad" < scripts/plot_molprobity_judging_analysis.R
