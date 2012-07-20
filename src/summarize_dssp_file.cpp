ea993393974ee40b04b98ff6a6bbd5e026f5db45
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2012-07-20 14:24:56 +0300

    Added convenience feature - coloring sequence by secondary structure when printing to Linux console.

--- src/summarize_dssp_file.cpp
+++ src/summarize_dssp_file.cpp
@@ -6,18 +6,47 @@
 
 void summarize_dssp_file(const auxiliaries::CommandLineOptions& clo)
 {
-	clo.check_allowed_options("--mode:");
+	clo.check_allowed_options("--mode: --use-linux-console-colors");
 
 	std::vector<protein::DSSPResidueRecord> dssp_records=protein::read_DSSP_atom_records_from_DSSP_file_stream(std::cin);
 
-	for(std::size_t i=0;i<dssp_records.size();i++)
+	if(clo.isopt("--use-linux-console-colors"))
 	{
-		std::cout << dssp_records[i].short_residue_name;
+		for(std::size_t i=0;i<dssp_records.size();i++)
+		{
+			const protein::DSSPResidueRecord& dr=dssp_records[i];
+			std::cout << "\033[30;";
+			if(dr.secondary_structure_summary=="H"
+					|| dr.secondary_structure_summary=="G"
+					|| dr.secondary_structure_summary=="I")
+			{
+				std::cout << "41m";
+			}
+			else if(dr.secondary_structure_summary=="B"
+					|| dr.secondary_structure_summary=="E")
+			{
+				std::cout << "42m";
+			}
+			else
+			{
+				std::cout << "47m";
+			}
+			std::cout << dr.short_residue_name;
+			std::cout << "\033[0m";
+		}
+		std::cout << "\n";
 	}
-	std::cout << "\n";
-	for(std::size_t i=0;i<dssp_records.size();i++)
+	else
 	{
-		std::cout << dssp_records[i].secondary_structure_summary;
+		for(std::size_t i=0;i<dssp_records.size();i++)
+		{
+			std::cout << dssp_records[i].short_residue_name;
+		}
+		std::cout << "\n";
+		for(std::size_t i=0;i<dssp_records.size();i++)
+		{
+			std::cout << dssp_records[i].secondary_structure_summary;
+		}
+		std::cout << "\n";
 	}
-	std::cout << "\n";
 }
