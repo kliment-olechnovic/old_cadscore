7c68cd68b4dfc2d64dfd2c0656f4d1c3a8ef5ae7
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2012-07-25 11:05:11 +0300

    Improved fast checking for any inter-chain contact.

---
+++ scripts/Voroprot2_fast_find_interacting_structures.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#This script accepts PDB filenames from the standard input
+
+SCRIPT_DIRECTORY=$(dirname $0)
+VOROPROT_NAME="voroprot2"
+VOROPROT="$SCRIPT_DIRECTORY/$VOROPROT_NAME"
+if [ ! -f "$VOROPROT" ]
+then
+  if which $VOROPROT_NAME &> /dev/null
+  then
+    VOROPROT=$VOROPROT_NAME
+  else
+    echo "Fatal error: '$VOROPROT_NAME' executable not found" 1>&2
+    exit 1
+  fi
+fi
+
+$VOROPROT --mode check-for-any-inter-chain-contact --direct-reading
