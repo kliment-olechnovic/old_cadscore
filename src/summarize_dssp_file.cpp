fd0633a570b112c807432cbde4682adb38791884
kliment <kliment@localhost>
2012-08-06 14:47:07 +0300

    Improved DSSP summary output.

--- src/summarize_dssp_file.cpp
+++ src/summarize_dssp_file.cpp
@@ -4,26 +4,46 @@
 
 #include "auxiliaries/command_line_options.h"
 
+inline bool is_in_helix(const protein::DSSPResidueRecord& dr)
+{
+	return (dr.secondary_structure_summary=="H" || dr.secondary_structure_summary=="G" || dr.secondary_structure_summary=="I");
+}
+
+inline bool is_in_strand(const protein::DSSPResidueRecord& dr)
+{
+	return (dr.secondary_structure_summary=="B" || dr.secondary_structure_summary=="E");
+}
+
 void summarize_dssp_file(const auxiliaries::CommandLineOptions& clo)
 {
-	clo.check_allowed_options("--use-linux-console-colors");
+	clo.check_allowed_options("--print-sequence --print-sequence-colored");
 
 	std::vector<protein::DSSPResidueRecord> dssp_records=protein::read_DSSP_atom_records_from_DSSP_file_stream(std::cin);
 
-	if(clo.isopt("--use-linux-console-colors"))
+	if(clo.isopt("--print-sequence"))
+	{
+		for(std::size_t i=0;i<dssp_records.size();i++)
+		{
+			std::cout << dssp_records[i].short_residue_name;
+		}
+		std::cout << "\n";
+		for(std::size_t i=0;i<dssp_records.size();i++)
+		{
+			std::cout << dssp_records[i].secondary_structure_summary;
+		}
+		std::cout << "\n";
+	}
+	else if(clo.isopt("--print-sequence-colored"))
 	{
 		for(std::size_t i=0;i<dssp_records.size();i++)
 		{
 			const protein::DSSPResidueRecord& dr=dssp_records[i];
 			std::cout << "\033[30;";
-			if(dr.secondary_structure_summary=="H"
-					|| dr.secondary_structure_summary=="G"
-					|| dr.secondary_structure_summary=="I")
+			if(is_in_helix(dr))
 			{
 				std::cout << "41m";
 			}
-			else if(dr.secondary_structure_summary=="B"
-					|| dr.secondary_structure_summary=="E")
+			else if(is_in_strand(dr))
 			{
 				std::cout << "42m";
 			}
@@ -38,15 +58,22 @@ void summarize_dssp_file(const auxiliaries::CommandLineOptions& clo)
 	}
 	else
 	{
+		std::size_t count_of_residues_in_helix=0;
+		std::size_t count_of_residues_in_strand=0;
 		for(std::size_t i=0;i<dssp_records.size();i++)
 		{
-			std::cout << dssp_records[i].short_residue_name;
+			const protein::DSSPResidueRecord& dr=dssp_records[i];
+			if(is_in_helix(dr))
+			{
+				count_of_residues_in_helix++;
+			}
+			else if(is_in_strand(dr))
+			{
+				count_of_residues_in_strand++;
+			}
 		}
-		std::cout << "\n";
-		for(std::size_t i=0;i<dssp_records.size();i++)
-		{
-			std::cout << dssp_records[i].secondary_structure_summary;
-		}
-		std::cout << "\n";
+		std::cout << "all_residues_count " << dssp_records.size() << "\n";
+		std::cout << "helix_residues_count " << count_of_residues_in_helix << "\n";
+		std::cout << "strand_residues_count " << count_of_residues_in_strand << "\n";
 	}
 }
