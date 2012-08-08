d109445759118b9b2407ba755b13c9a666bb082b
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2012-08-08 13:53:09 +0300

    Added regression testing routine.

---
+++ tests/run_tests.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+rm -r ./output
+mkdir ./output
+
+for MODEL in ./input/*
+do
+  ../scripts/Voroprot2_calc.bash -f $MODEL -q > ./output/quadruples_$(basename $MODEL)
+done
+
+
+DBNAME="db_all_contacts"
+for MODEL in ./input/model*
+do
+  ../scripts/CADscore_calc.bash -D ./output/$DBNAME -t ./input/target -m $MODEL
+  ../scripts/CADscore_read_local_scores.bash -D ./output/$DBNAME -t ./input/target -m $MODEL -c AA -w 1 > ./output/$DBNAME/local_scores_$(basename $MODEL)
+done
+../scripts/CADscore_read_global_scores.bash -D ./output/$DBNAME | sort -r | column -t > ./output/$DBNAME/global_scores
+
+
+DBNAME="db_inter_chain_contacts"
+for MODEL in ./input/model*
+do
+  ../scripts/CADscore_calc.bash -D ./output/$DBNAME -t ./input/target -m $MODEL -c
+  ../scripts/CADscore_read_local_scores.bash -D ./output/$DBNAME -t ./input/target -m $MODEL -c AA -w 1 > ./output/$DBNAME/local_scores_$(basename $MODEL)
+done
+../scripts/CADscore_read_global_scores.bash -D ./output/$DBNAME | sort -r | column -t > ./output/$DBNAME/global_scores
