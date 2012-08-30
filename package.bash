07789a70eb6343ed1f2c6ea397614b2defda5e4d
kliment <kliment@localhost>
2012-08-30 13:36:01 +0300

    Updated packaging script.

--- package.bash
+++ package.bash
@@ -14,6 +14,7 @@ cp -r src $PACKAGE_NAME/src
 mkdir -p $PACKAGE_NAME/bin
 cp Release/voroprot2 $PACKAGE_NAME/bin/voroprot2
 cp scripts/Voroprot2_calc.bash $PACKAGE_NAME/bin
+cp scripts/Voroprot2_print_interfaces_as_PyMol_graphics.bash $PACKAGE_NAME/bin
 cp scripts/CADscore_* $PACKAGE_NAME/bin
 
 mkdir -p $PACKAGE_NAME/resources
