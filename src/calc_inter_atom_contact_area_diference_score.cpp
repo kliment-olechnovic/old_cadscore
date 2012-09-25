cc31be7cd75838e8cb5a1d0701d895b82667a44c
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2012-09-25 16:12:55 +0300

    Added detailed inter atom CAD local scores calculation.

--- src/calc_inter_atom_contact_area_diference_score.cpp
+++ src/calc_inter_atom_contact_area_diference_score.cpp
@@ -29,7 +29,10 @@ std::map<contacto::ContactID<protein::AtomID>, double> construct_inter_atom_cont
 
 void calc_inter_atom_contact_area_difference_score(const auxiliaries::CommandLineOptions& clo)
 {
-	clo.check_allowed_options("");
+	clo.check_allowed_options("--local --global");
+
+	const bool print_local=clo.isopt("--local");
+	const bool print_global=clo.isopt("--global") || !print_local;
 
 	auxiliaries::assert_file_header(std::cin, "atoms");
 	const std::vector<protein::Atom> atoms_1=auxiliaries::read_vector<protein::Atom>(std::cin);
@@ -49,15 +52,37 @@ void calc_inter_atom_contact_area_difference_score(const auxiliaries::CommandLin
 			construct_inter_atom_contacts_map(atoms_1, inter_atom_contacts_1),
 			construct_inter_atom_contacts_map(atoms_2, inter_atom_contacts_2));
 
-	double difference=0.0;
-	double reference=0.0;
-	for(CombinedContactsMap::const_iterator it=combined_contacts.begin();it!=combined_contacts.end();++it)
+	if(print_global)
 	{
-		const double t=it->second.first;
-		const double m=it->second.second;
-		difference+=std::min(fabs(t-m), t);
-		reference+=t;
+		std::pair<double, double> global_ratio;
+		for(CombinedContactsMap::const_iterator it=combined_contacts.begin();it!=combined_contacts.end();++it)
+		{
+			const double t=it->second.first;
+			const double m=it->second.second;
+			global_ratio.first+=std::min(fabs(t-m), t);
+			global_ratio.second+=t;
+		}
+		std::cout << "inter_atom_cad_global_score " << (global_ratio.second>0.0 ? (1-(global_ratio.first/global_ratio.second)) : 0.0) << "\n";
 	}
 
-	std::cout << "inter_atom_cad_score " << (reference>0.0 ? (1-(difference/reference)) : 0.0) << "\n";
+	if(print_local)
+	{
+		typedef std::map< protein::AtomID, std::pair<double, double> > LocalRatiosMap;
+		LocalRatiosMap local_ratios;
+		for(CombinedContactsMap::const_iterator it=combined_contacts.begin();it!=combined_contacts.end();++it)
+		{
+			const double t=it->second.first;
+			const double m=it->second.second;
+			std::pair<double, double>& local_ratio=local_ratios[it->first.a];
+			local_ratio.first+=std::min(fabs(t-m), t);
+			local_ratio.second+=t;
+		}
+		auxiliaries::print_file_header(std::cout, "inter_atom_cad_local_scores");
+		std::cout << local_ratios.size() << "\n";
+		for(LocalRatiosMap::const_iterator it=local_ratios.begin();it!=local_ratios.end();++it)
+		{
+			const std::pair<double, double>& local_ratio=it->second;
+			std::cout << it->first << " " << (local_ratio.second>0.0 ? (1-(local_ratio.first/local_ratio.second)) : 0.0) << "\n";
+		}
+	}
 }
