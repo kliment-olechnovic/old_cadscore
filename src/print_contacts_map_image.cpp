e5eb4f4166d0ae47ed028e006bb17db520d0eeea
kliment <kliment@localhost>
2012-10-19 10:09:34 +0300

    Added distances map image printing.

--- src/print_contacts_map_image.cpp
+++ src/print_contacts_map_image.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <sstream>
+#include <limits>
 
+#include "protein/atom.h"
 #include "protein/residue_id.h"
+#include "protein/residue_ids_collection.h"
+
+#include "apollo/spheres_basic_operations.h"
 
 #include "contacto/contact_id.h"
 #include "contacto/inter_residue_contact_areas.h"
 
 #include "auxiliaries/command_line_options.h"
 #include "auxiliaries/map_io.h"
+#include "auxiliaries/vector_io.h"
 #include "auxiliaries/xml_writer.h"
 #include "auxiliaries/color.h"
+#include "auxiliaries/ppm_image_writer.h"
 
 void print_inter_residue_contacts_map_image(const auxiliaries::CommandLineOptions& clo)
 {
@@ -59,4 +66,68 @@ void print_inter_residue_contacts_map_image(const auxiliaries::CommandLineOption
 		}
 	}
 	svg.write(std::cout);
+}
+
+void print_inter_residue_distance_map_image(const auxiliaries::CommandLineOptions& clo)
+{
+	clo.check_allowed_options("--max-distance:");
+
+	const double max_distance=clo.isopt("--max-distance") ? clo.arg_with_min_value<double>("--max-distance", 0.1) : -1.0;
+
+	const std::vector<protein::Atom> atoms=auxiliaries::read_vector<protein::Atom>(std::cin, "atoms", "atoms", false);
+
+	const std::map<protein::ResidueID, std::vector<std::size_t> > residue_ids_atoms=protein::group_atoms_indices_by_residue_ids(atoms);
+
+	if(residue_ids_atoms.size()<2)
+	{
+		throw std::runtime_error("Less than 2 residues provided");
+	}
+
+	std::vector< std::vector<double> > distance_map(residue_ids_atoms.size(), std::vector<double>(residue_ids_atoms.size(), 0.0));
+
+	double observed_max_distance=std::numeric_limits<double>::min();
+
+	{
+		std::size_t i=0;
+		for(std::map<protein::ResidueID, std::vector<std::size_t> >::const_iterator it=residue_ids_atoms.begin();it!=residue_ids_atoms.end();++it)
+		{
+			std::size_t j=i+1;
+			std::map<protein::ResidueID, std::vector<std::size_t> >::const_iterator jt=it;
+			++jt;
+			for(;jt!=residue_ids_atoms.end();++jt)
+			{
+				const std::vector<std::size_t>& atoms_i=it->second;
+				const std::vector<std::size_t>& atoms_j=jt->second;
+				double min_distance=std::numeric_limits<double>::max();
+				for(std::size_t m=0;m<atoms_i.size();m++)
+				{
+					for(std::size_t n=0;n<atoms_j.size();n++)
+					{
+						min_distance=std::min(min_distance, apollo::minimal_distance_from_sphere_to_sphere(atoms[atoms_i[m]], atoms[atoms_j[n]]));
+					}
+				}
+				distance_map[i][j]=min_distance;
+				distance_map[j][i]=min_distance;
+				observed_max_distance=std::max(observed_max_distance, min_distance);
+				j++;
+			}
+			i++;
+		}
+	}
+
+	const double usable_max_distance=(max_distance>0.0 ? max_distance : observed_max_distance);
+
+	auxiliaries::PPMImageWriter image_writer(distance_map.size(), distance_map.size());
+	for(std::size_t i=0;i<distance_map.size();i++)
+	{
+		for(std::size_t j=0;j<distance_map.size();j++)
+		{
+			const double dist=distance_map[i][j];
+			if(dist<=usable_max_distance)
+			{
+				image_writer.color(i, j)=auxiliaries::Color::from_temperature_to_blue_white_red(1.0-dist/usable_max_distance);
+			}
+		}
+	}
+	image_writer.write(std::cout);
 }
