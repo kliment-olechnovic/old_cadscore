a23351610f00c18bda8947075122563c6327b7fd
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2012-10-25 11:30:31 +0300

    Added atomic scores output testing.

--- tests/run_tests.bash
+++ tests/run_tests.bash
@@ -17,7 +17,7 @@ done
 DBNAME="db_all_contacts"
 for MODEL in ./input/model*
 do
-  ../scripts/CADscore_calc.bash -D ./output/$DBNAME -t ./input/target -m $MODEL
+  ../scripts/CADscore_calc.bash -D ./output/$DBNAME -t ./input/target -m $MODEL -a
   ../scripts/CADscore_read_local_scores.bash -D ./output/$DBNAME -t ./input/target -m $MODEL -c AA -w 1 > ./output/$DBNAME/local_scores_$(basename $MODEL)
 done
 ../scripts/CADscore_read_global_scores.bash -D ./output/$DBNAME | sort -r | column -t > ./output/$DBNAME/global_scores
@@ -26,7 +26,7 @@ done
 DBNAME="db_inter_chain_contacts"
 for MODEL in ./input/model*
 do
-  ../scripts/CADscore_calc.bash -D ./output/$DBNAME -t ./input/target -m $MODEL -c
+  ../scripts/CADscore_calc.bash -D ./output/$DBNAME -t ./input/target -m $MODEL -c -a
   ../scripts/CADscore_read_local_scores.bash -D ./output/$DBNAME -t ./input/target -m $MODEL -c AA -w 1 > ./output/$DBNAME/local_scores_$(basename $MODEL)
 done
 ../scripts/CADscore_read_global_scores.bash -D ./output/$DBNAME | sort -r | column -t > ./output/$DBNAME/global_scores
