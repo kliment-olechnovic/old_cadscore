2fbda32bbf2e3a990eed0f2bb1420b9d2e930cac
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2012-10-30 17:00:15 +0200

    Improved RNA stacking and siding interactions analysis.

--- analysis/rna/contacts_typing/scripts/compare_typings.bash
+++ analysis/rna/contacts_typing/scripts/compare_typings.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cat ./input/typed_contacts | sed 's/.pdb//' | awk '{print $1 "_" $2 "_" $3 $4 "_" $5 $6 " " $2 " " $7}' | sort > typed_contacts_ids_and_areas
+cat ./input/typed_contacts | sed 's/.pdb//' | awk '{print $1 "_" $2 "_" $3 $4 "_" $5 $6 "\t" $2 "\t" $7}' | sort > typed_contacts_ids_and_areas
 
 ( \
-cat ./input/mc_annotate_stacking_interactions | sed 's/.pdb//' | sed 's/-\([[:upper:]]\)/ \1/g' | awk '{print $1 "_stack_" $2 "_" $3 " stack"}' ; \
-cat ./input/mc_annotate_base_pairing_interactions | sed 's/.pdb//' | sed 's/-\([[:upper:]]\)/ \1/g' | awk '{print $1 "_side_" $2 "_" $3 " side"}' \
-) | sort > mc_annotate_interactions_ids
+cat ./input/mc_annotate_stacking_interactions | sed 's/.pdb//' | sed 's/-\([[:upper:]]\)/ \1/g' | sed 's/\(.*\)\s\(.*\)\s\(.*\)\s:\s\(.*\)/\1_stack_\2_\3\tstack\t\4/' | tr ' ' '_' ; \
+cat ./input/mc_annotate_base_pairing_interactions | sed 's/.pdb//' | sed 's/-\([[:upper:]]\)/ \1/g' | sed 's/\(.*\)\s\(.*\)\s\(.*\)\s:\s\(.*\)/\1_side_\2_\3\tside\t\4/' | tr ' ' '_' \
+) | egrep '_stack_|_side_' | sort > mc_annotate_interactions_ids
 
 
 R --vanilla < ./scripts/compare_typings.R
