670715070c61a9e13f429072131ccfadf90bbabe
kliment <kliment@localhost>
2012-11-02 14:46:32 +0200

    Improved resources preparation, added version printing option.

--- resources/create_vdwr_header.bash
+++ resources/create_vdwr_header.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-(echo -e '#ifndef VDWR_H_\n#define VDWR_H_\n' ; xxd -i vdwr_classes ; echo ; xxd -i vdwr_members ; echo -e '\n#endif /* VDWR_H_ */') > ../src/resources/vdwr.h
+cd $(dirname "$0")
+
+(echo -e '#ifndef VDWR_H_\n#define VDWR_H_\n\nnamespace resources\n{\n' ; xxd -i vdwr_classes ; echo ; xxd -i vdwr_members ; echo -e '\n}\n\n#endif /* VDWR_H_ */') > ../src/resources/vdwr.h
