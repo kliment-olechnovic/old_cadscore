c551a137b7190f5ed2bcd563be72b1d2751f8204
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2012-11-04 15:08:05 +0200

    Improved and enhanced calculation of optimal rotation for structure display.

--- src/print_optimal_rotation.cpp
+++ src/print_optimal_rotation.cpp
@@ -26,7 +26,7 @@ void print_optimal_rotation(const auxiliaries::CommandLineOptions& clo)
 	}
 	center=center/static_cast<double>(atoms.size());
 
-	apollo::SimplePoint max_v=sih.vertices().at(0);
+	apollo::SimplePoint max_projection_vector=sih.vertices().at(0);
 	{
 		double max_sum=0.0;
 		for(std::size_t j=1;j<sih.vertices().size();j++)
@@ -40,12 +40,12 @@ void print_optimal_rotation(const auxiliaries::CommandLineOptions& clo)
 			if(sum>max_sum)
 			{
 				max_sum=sum;
-				max_v=v;
+				max_projection_vector=v;
 			}
 		}
 	}
 
-	double side=1.0;
+	double max_projection_side=1.0;
 	{
 		double vertical_sum=0;
 		{
@@ -53,23 +53,56 @@ void print_optimal_rotation(const auxiliaries::CommandLineOptions& clo)
 			double high_val=0.0;
 			for(std::size_t i=0;i<atoms.size();i++)
 			{
-				const double val=apollo::project_point_on_vector(center, center+max_v, atoms[i]);
+				const double val=apollo::project_point_on_vector(center, center+max_projection_vector, atoms[i]);
 				low_val=std::min(low_val, val);
 				high_val=std::max(high_val, val);
 			}
-			const apollo::SimplePoint median_center=center+(max_v*((low_val+high_val)/2));
+			const apollo::SimplePoint median_center=center+(max_projection_vector*((low_val+high_val)/2));
 			for(std::size_t i=0;i<atoms.size();i++)
 			{
-				vertical_sum+=apollo::project_point_on_vector(median_center, median_center+max_v, atoms[i]);
+				vertical_sum+=apollo::project_point_on_vector(median_center, median_center+max_projection_vector, atoms[i]);
 			}
 		}
 		if(vertical_sum<0.0)
 		{
-			side=-1.0;
+			max_projection_side=-1.0;
 		}
 	}
 
-	const apollo::Rotation rotation=apollo::Rotation::from_two_points(max_v, apollo::SimplePoint(0, 0, side));
+	double max_vertical_angle=0.0;
+	{
+		double max_vertical_sum=0.0;
+		const apollo::SimplePoint initial_vertical_vector=apollo::any_normal_of_vector<apollo::SimplePoint>(max_projection_vector);
+		const double angle_step=1.0;
+		for(double angle=angle_step;angle<360.0;angle+=angle_step)
+		{
+			const apollo::SimplePoint v=apollo::Rotation(max_projection_vector, angle).rotate<apollo::SimplePoint>(initial_vertical_vector);
+			double vertical_sum=0;
+			{
+				double low_val=0.0;
+				double high_val=0.0;
+				for(std::size_t i=0;i<atoms.size();i++)
+				{
+					const double val=apollo::project_point_on_vector(center, center+v, atoms[i]);
+					low_val=std::min(low_val, val);
+					high_val=std::max(high_val, val);
+				}
+				const apollo::SimplePoint median_center=center+(v*((low_val+high_val)/2));
+				for(std::size_t i=0;i<atoms.size();i++)
+				{
+					vertical_sum+=apollo::project_point_on_vector(median_center, median_center+v, atoms[i]);
+				}
+			}
+			if(vertical_sum>max_vertical_sum)
+			{
+				max_vertical_sum=vertical_sum;
+				max_vertical_angle=angle;
+			}
+		}
+	}
 
-	std::cout << "rotate [" << rotation.axis.x << "," << rotation.axis.y << "," << rotation.axis.z << "], " << rotation.angle << "\n";
+	const apollo::Rotation max_projection_rotation=apollo::Rotation::from_two_points(max_projection_vector, apollo::SimplePoint(0.0, 0.0, max_projection_side));
+
+	std::cout << "rotate [" << max_projection_vector.x << "," << max_projection_vector.y << "," << max_projection_vector.z << "], " << max_vertical_angle << "\n";
+	std::cout << "rotate [" << max_projection_rotation.axis.unit().x << "," << max_projection_rotation.axis.unit().y << "," << max_projection_rotation.axis.unit().z << "], " << max_projection_rotation.angle << "\n";
 }
