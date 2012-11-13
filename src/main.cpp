e5480bee7d13a6c7a16cca9147fc981bd5518033
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2012-11-13 09:41:11 +0200

    Removed unused stuff.

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,6 @@ void calc_inter_atom_contact_area_difference_score(const auxiliaries::CommandLin
 void summarize_inter_atom_contacts(const auxiliaries::CommandLineOptions& clo);
 void filter_atoms_by_name(const auxiliaries::CommandLineOptions& clo);
 
-void rotate_coordinates(const auxiliaries::CommandLineOptions& clo);
 void print_quadruples_image(const auxiliaries::CommandLineOptions& clo);
 void print_inter_residue_contacts_map_image(const auxiliaries::CommandLineOptions& clo);
 void print_inter_residue_distance_map_image(const auxiliaries::CommandLineOptions& clo);
@@ -96,7 +95,6 @@ int main(const int argc, const char** argv)
 		modes_map["summarize-inter-atom-contacts"]=ModeFunctionPointer(summarize_inter_atom_contacts);
 		modes_map["filter-atoms-by-name"]=ModeFunctionPointer(filter_atoms_by_name);
 
-		modes_map["rotate-coordinates"]=ModeFunctionPointer(rotate_coordinates);
 		modes_map["print-quadruples-image"]=ModeFunctionPointer(print_quadruples_image);
 		modes_map["print-inter-residue-contacts-map-image"]=ModeFunctionPointer(print_inter_residue_contacts_map_image);
 		modes_map["print-inter-residue-distance-map-image"]=ModeFunctionPointer(print_inter_residue_distance_map_image);
