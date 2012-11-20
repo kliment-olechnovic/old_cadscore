117f9e6d7fa5e11f43239b000cb93699f470a48c
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2012-11-20 23:58:29 +0200

    Improved sequence alignment printing.

--- src/print_sequence_alignment.cpp
+++ src/print_sequence_alignment.cpp
@@ -101,24 +101,87 @@ std::vector< std::pair<int, int> > create_sequence_alignment(const T& seq1, cons
 template<typename T>
 void print_sequence_alignment(const T& seq1, const T& seq2, const std::vector< std::pair<int, int> >& alignment)
 {
-	for(std::size_t i=0;i<alignment.size();i++)
+	std::pair<int, int> start(-1, -1);
+	std::pair<int, int> end(-1, -1);
+	for(std::size_t i=0;i<alignment.size() && (start.first<0 || start.second<0 || end.first<0 || end.second<0);i++)
 	{
-		const int j=alignment[i].first;
-		if(j<static_cast<int>(seq1.size()))
+		if(start.first<0)
 		{
-			std::cout << (j>=0 ? seq1[j] : '-') << " ";
+			start.first=alignment[i].first;
+		}
+		if(start.second<0)
+		{
+			start.second=alignment[i].second;
+		}
+		if(end.first<0)
+		{
+			end.first=alignment[alignment.size()-1-i].first;
+		}
+		if(end.second<0)
+		{
+			end.second=alignment[alignment.size()-1-i].second;
 		}
 	}
-	std::cout << "\n";
-	for(std::size_t i=0;i<alignment.size();i++)
+
+	if(start.first>=0 && start.second>=0)
 	{
-		const int j=alignment[i].second;
-		if(j<static_cast<int>(seq2.size()))
+		const int offset=std::max(start.first, start.second);
+
+		for(int i=0;i<(offset-start.first);i++)
 		{
-			std::cout << (j>=0 ? seq2[j] : '-') << " ";
+			std::cout << " ";
 		}
+		for(int i=0;i<start.first;i++)
+		{
+			std::cout << seq1[i];
+		}
+		for(std::size_t i=0;i<alignment.size();i++)
+		{
+			const int j=alignment[i].first;
+			std::cout << (j>=0 ? seq1[j] : '-');
+		}
+		for(int i=end.first+1;i<static_cast<int>(seq1.size());i++)
+		{
+			std::cout << seq1[i];
+		}
+		std::cout << "\n";
+
+		for(int i=0;i<offset;i++)
+		{
+			std::cout << " ";
+		}
+		for(std::size_t i=0;i<alignment.size();i++)
+		{
+			if(alignment[i].first>=0 && alignment[i].second>=0 && (seq1[alignment[i].first]==seq2[alignment[i].second]))
+			{
+				std::cout << "+";
+			}
+			else
+			{
+				std::cout << "|";
+			}
+		}
+		std::cout << "\n";
+
+		for(int i=0;i<(offset-start.second);i++)
+		{
+			std::cout << " ";
+		}
+		for(int i=0;i<start.second;i++)
+		{
+			std::cout << seq2[i];
+		}
+		for(std::size_t i=0;i<alignment.size();i++)
+		{
+			const int j=alignment[i].second;
+			std::cout << (j>=0 ? seq2[j] : '-');
+		}
+		for(int i=end.second+1;i<static_cast<int>(seq2.size());i++)
+		{
+			std::cout << seq2[i];
+		}
+		std::cout << "\n";
 	}
-	std::cout << "\n";
 }
 
 void print_sequence_alignment(const auxiliaries::CommandLineOptions& clo)
