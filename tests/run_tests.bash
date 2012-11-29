d0f3477e499badd1ca11d79de8128c33c442ea09
kliment <kliment@localhost>
2012-11-29 10:39:53 +0200

    Improved testing script.

--- tests/run_tests.bash
+++ tests/run_tests.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+RECOMPILE=false
+if [ "$1" == "recompile" ]
+then
+  RECOMPILE=true
+fi
+
 set -e
 
 cd $(dirname "$0")
@@ -9,7 +15,14 @@ mv ../cadscore_package.tar.gz ./cadscore_package.tar.gz
 tar -xf ./cadscore_package.tar.gz
 rm ./cadscore_package.tar.gz
 
-CADSCORE_BIN_DIR_ABSOLUTE_PATH="$(pwd)/cadscore_package/bin/"
+CADSCORE_DIR_ABSOLUTE_PATH="$(pwd)/cadscore_package/"
+CADSCORE_BIN_DIR_ABSOLUTE_PATH="$CADSCORE_DIR_ABSOLUTE_PATH/bin/"
+CADSCORE_SRC_DIR_ABSOLUTE_PATH="$CADSCORE_DIR_ABSOLUTE_PATH/src/"
+
+if $RECOMPILE
+then
+  g++ -O3 -o $CADSCORE_BIN_DIR_ABSOLUTE_PATH/voroprot2 $CADSCORE_SRC_DIR_ABSOLUTE_PATH/*.cpp
+fi
 
 ./basic/run_tests.bash $CADSCORE_BIN_DIR_ABSOLUTE_PATH &
 ./rna/run_tests.bash $CADSCORE_BIN_DIR_ABSOLUTE_PATH &
