1ee0ef2fb577a67326400d70344a6823298889bc
kliment <kliment@localhost>
2012-12-27 12:34:07 +0200

    Added simple sequence printing.

--- src/print_sequence_alignment.cpp
+++ src/print_sequence_alignment.cpp
@@ -5,45 +5,19 @@
 #include "protein/pairwise_sequence_alignment.h"
 #include "protein/residue_id.h"
 #include "protein/residue_summary.h"
-#include "protein/letters_coding.h"
 
 #include "auxiliaries/command_line_options.h"
 #include "auxiliaries/map_io.h"
 
-std::string read_sequence_from_residue_ids()
-{
-	const std::map<protein::ResidueID, protein::ResidueSummary> residue_ids=auxiliaries::read_map<protein::ResidueID, protein::ResidueSummary>(std::cin, "residue identifiers", "residue_ids", false);
-	std::ostringstream output;
-	for(std::map<protein::ResidueID, protein::ResidueSummary>::const_iterator it=residue_ids.begin();it!=residue_ids.end();++it)
-	{
-		output << protein::convert_residue_codes_big_to_small(it->second.name);
-		std::map<protein::ResidueID, protein::ResidueSummary>::const_iterator next_it=it;
-		++next_it;
-		if(next_it!=residue_ids.end() && next_it->first.chain_id!=it->first.chain_id)
-		{
-			output << "=";
-		}
-	}
-	return output.str();
-}
-
 void print_sequence_alignment(const auxiliaries::CommandLineOptions& clo)
 {
-	clo.check_allowed_options("--read-residue-ids");
+	clo.check_allowed_options("");
 
 	std::string seq1;
 	std::string seq2;
 
-	if(clo.isopt("--read-residue-ids"))
-	{
-		seq1=read_sequence_from_residue_ids();
-		seq2=read_sequence_from_residue_ids();
-	}
-	else
-	{
-		std::getline(std::cin, seq1);
-		std::getline(std::cin, seq2);
-	}
+	std::getline(std::cin, seq1);
+	std::getline(std::cin, seq2);
 
 	if(seq1.empty() || seq2.empty())
 	{
