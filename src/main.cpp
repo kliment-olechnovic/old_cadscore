a674cfe54ea42b7991d99c426c732797a2d9b375
kliment <kliment@localhost>
2013-03-18 16:19:07 +0200

    Added some specialized sequence handling.

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,9 @@ void merge_atoms(const auxiliaries::CommandLineOptions& clo);
 void print_inter_chain_interface_graphics(const auxiliaries::CommandLineOptions& clo);
 void summarize_inter_atom_contacts(const auxiliaries::CommandLineOptions& clo);
 
+void x_collect_residue_sequence(const auxiliaries::CommandLineOptions& clo);
+void x_combine_three_global_sequence_alignments(const auxiliaries::CommandLineOptions& clo);
+void x_construct_global_sequence_alignment(const auxiliaries::CommandLineOptions& clo);
 void x_print_inter_residue_contacts_graph(const auxiliaries::CommandLineOptions& clo);
 void x_print_inter_residue_contacts_map_image(const auxiliaries::CommandLineOptions& clo);
 void x_print_inter_residue_distance_map_image(const auxiliaries::CommandLineOptions& clo);
@@ -90,6 +93,9 @@ int main(const int argc, const char** argv)
 		modes_map["print-inter-chain-interface-graphics"]=ModeFunctionPointer(print_inter_chain_interface_graphics);
 		modes_map["summarize-inter-atom-contacts"]=ModeFunctionPointer(summarize_inter_atom_contacts);
 
+		modes_map["x-collect-residue-sequence"]=ModeFunctionPointer(x_collect_residue_sequence);
+		modes_map["x-combine-three-global-sequence-alignments"]=ModeFunctionPointer(x_combine_three_global_sequence_alignments);
+		modes_map["x-construct-global-sequence-alignment"]=ModeFunctionPointer(x_construct_global_sequence_alignment);
 		modes_map["x-print-inter-residue-contacts-graph"]=ModeFunctionPointer(x_print_inter_residue_contacts_graph);
 		modes_map["x-print-inter-residue-contacts-map-image"]=ModeFunctionPointer(x_print_inter_residue_contacts_map_image);
 		modes_map["x-print-inter-residue-distance-map-image"]=ModeFunctionPointer(x_print_inter_residue_distance_map_image);
