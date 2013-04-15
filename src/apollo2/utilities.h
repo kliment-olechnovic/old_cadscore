af0ce073ea4fd9b54e16efe66c301e82c7ca73bf
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2013-04-15 12:12:07 +0300

    Refactored spheres hierarchy.

--- src/apollo2/utilities.h
+++ src/apollo2/utilities.h
@@ -26,37 +26,6 @@ std::vector<std::size_t> sort_objects_by_functor_result(const ListType& list, co
 	return result;
 }
 
-template<typename A, typename B>
-std::vector< std::pair<typename A::value_type, typename B::value_type> > make_pairs(const A& a, const B& b)
-{
-	std::vector< std::pair<typename A::value_type, typename B::value_type> > pairs;
-	pairs.reserve(std::min(a.size(), b.size()));
-	typename A::const_iterator ait=a.begin();
-	typename B::const_iterator bit=b.begin();
-	while(ait!=a.end() && bit!=b.end())
-	{
-		pairs.push_back(std::make_pair(*ait, *bit));
-		++ait;
-		++bit;
-	}
-	return pairs;
-}
-
-template<typename T>
-std::pair< std::vector<typename T::value_type::first_type>, std::vector<typename T::value_type::second_type> > split_pairs(const T& pairs)
-{
-	std::vector<typename T::value_type::first_type> a;
-	a.reserve(pairs.size());
-	std::vector<typename T::value_type::second_type> b;
-	b.reserve(pairs.size());
-	for(typename T::const_iterator it=pairs.begin();it!=pairs.end();++it)
-	{
-		a.push_back(it->first);
-		b.push_back(it->second);
-	}
-	return std::make_pair(a, b);
-}
-
 }
 
 #endif /* APOLLO2_UTILITIES_H_ */
