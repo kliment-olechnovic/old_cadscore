999ea4c73b336e2c527678a33584e606a2ede8db
kliment <kliment@localhost>
2013-04-18 14:44:55 +0300

    Added table-based printing of triangulation.

--- src/apollo2/apollonius_triangulation.h
+++ src/apollo2/apollonius_triangulation.h
@@ -1,6 +1,9 @@
 #ifndef APOLLO2_APOLLONIUS_TRIANGULATION_2_H_
 #define APOLLO2_APOLLONIUS_TRIANGULATION_2_H_
 
+#include <iostream>
+#include <sstream>
+
 #include "apollonius_triangulation/search_for_valid_quadruples.h"
 
 namespace apollo2
@@ -13,14 +16,79 @@ public:
 	typedef apollonius_triangulation::Log Log;
 
 	template<typename SphereType>
-	static QuadruplesMap construct(const BoundingSpheresHierarchy<SphereType>& bsh, const bool enable_searching_for_e)
+	static QuadruplesMap construct_map_of_quadruples(const BoundingSpheresHierarchy<SphereType>& bsh, const bool enable_searching_for_e)
 	{
 		return apollonius_triangulation::find_valid_quadruples(bsh, enable_searching_for_e);
+	}
+
+	static void print_map_of_quadruples(const QuadruplesMap& quadruples_map, std::ostream& output)
+	{
+		const QuadruplesOrderedMap quadruples_ordered_map=collect_ordered_map_of_quadruples(quadruples_map);
+		for(QuadruplesOrderedMap::const_iterator it=quadruples_ordered_map.begin();it!=quadruples_ordered_map.end();++it)
+		{
+			const Quadruple& quadruple=it->first;
+			const std::vector<SimpleSphere>& tangent_spheres=it->second;
+			for(std::size_t i=0;i<tangent_spheres.size();i++)
+			{
+				const SimpleSphere& tangent_sphere=tangent_spheres[i];
+				output << quadruple.get(0) << " " << quadruple.get(1) << " " << quadruple.get(2) << " " << quadruple.get(3) << " ";
+				output << tangent_sphere.x << " " << tangent_sphere.y << " " << tangent_sphere.z << " " << tangent_sphere.r << "\n";
+			}
+		}
+	}
+
+	static QuadruplesMap read_map_of_quadruples(std::istream& input)
+	{
+		QuadruplesMap quadruples_map;
+		bool valid=true;
+		while(input.good() && valid)
+		{
+			std::string line;
+			std::getline(input, line);
+			if(line.empty())
+			{
+				valid=false;
+			}
+			else
+			{
+				std::istringstream line_stream(line);
+				std::size_t q[4]={0, 0, 0, 0};
+				double s[4]={0, 0, 0, 0};
+				for(int i=0;i<4 && valid;i++)
+				{
+					line_stream >> q[i];
+					valid=!line_stream.fail();
+				}
+				for(int i=0;i<4 && valid;i++)
+				{
+					line_stream >> s[i];
+					valid=!line_stream.fail();
+				}
+				if(valid)
+				{
+					quadruples_map[Quadruple(q[0], q[1], q[2], q[3])].push_back(SimpleSphere(s[0], s[1], s[2], s[3]));
+				}
+			}
+		}
+		return quadruples_map;
 	}
 
 	static const Log& log()
 	{
 		return apollonius_triangulation::log_ref();
+	}
+
+private:
+	typedef std::map<QuadruplesMap::key_type, QuadruplesMap::mapped_type> QuadruplesOrderedMap;
+
+	static QuadruplesOrderedMap collect_ordered_map_of_quadruples(const QuadruplesMap& quadruples_map)
+	{
+		QuadruplesOrderedMap quadruples_ordered_map;
+		for(QuadruplesMap::const_iterator it=quadruples_map.begin();it!=quadruples_map.end();++it)
+		{
+			quadruples_ordered_map.insert(*it);
+		}
+		return quadruples_ordered_map;
 	}
 };
 
