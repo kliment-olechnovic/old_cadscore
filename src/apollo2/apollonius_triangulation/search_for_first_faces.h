1f1e2238bcb04c99f9f3c3972310c5264df08ca3
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2013-04-28 13:09:48 +0300

    Refactored search fo first faces.

--- src/apollo2/apollonius_triangulation/search_for_first_faces.h
+++ src/apollo2/apollonius_triangulation/search_for_first_faces.h
@@ -16,15 +16,21 @@ namespace apollo2
 namespace apollonius_triangulation
 {
 
-template<typename SphereType>
-std::vector< Face<SphereType> > find_first_faces(const BoundingSpheresHierarchy<SphereType>& bsh, const std::size_t starting_sphere_id, const bool fix_starting_sphere_id, std::size_t& iterations_count)
+template<typename SphereType, typename TriplesSetType>
+std::vector< Face<SphereType> > find_first_faces(
+		const BoundingSpheresHierarchy<SphereType>& bsh,
+		const std::size_t starting_sphere_id,
+		std::size_t& iterations_count,
+		const TriplesSetType& forbidden_triples_set,
+		const bool fix_starting_sphere_id=false,
+		const double max_distance=std::numeric_limits<double>::max())
 {
 	const std::vector<SphereType>& spheres=bsh.leaves_spheres();
 	iterations_count=0;
 	std::vector< Face<SphereType> > result;
 	if(spheres.size()>=4 && starting_sphere_id<spheres.size())
 	{
-		const std::vector<std::size_t> traversal=BoundingSpheresHierarchy<SphereType>::sort_objects_by_distance_to_one_of_them(spheres, starting_sphere_id, minimal_distance_from_sphere_to_sphere<SphereType, SphereType>);
+		const std::vector<std::size_t> traversal=BoundingSpheresHierarchy<SphereType>::sort_objects_by_distance_to_one_of_them(spheres, starting_sphere_id, minimal_distance_from_sphere_to_sphere<SphereType, SphereType>, max_distance);
 		for(std::size_t u=4;u<traversal.size();u++)
 		{
 			for(std::size_t a=0;a<(fix_starting_sphere_id ? 1 : u);a++)
@@ -34,15 +40,16 @@ std::vector< Face<SphereType> > find_first_faces(const BoundingSpheresHierarchy<
 					for(std::size_t c=b+1;c+1<u;c++)
 					{
 						iterations_count++;
-						Quadruple quadruple(traversal[a], traversal[b], traversal[c], traversal[u-1]);
-						const std::vector<SimpleSphere> tangents=TangentSphereOfFourSpheres::calculate<SimpleSphere>(spheres[quadruple.get(0)], spheres[quadruple.get(1)], spheres[quadruple.get(2)], spheres[quadruple.get(3)]);
-						if(tangents.size()==1 && find_any_collision(bsh, tangents.front()).empty())
+						const Triple triple(traversal[a], traversal[b], traversal[c]);
+						if(forbidden_triples_set.count(triple)==0)
 						{
-							for(int i=0;i<4;i++)
+							Quadruple quadruple(triple, traversal[u-1]);
+							const std::vector<SimpleSphere> tangents=TangentSphereOfFourSpheres::calculate<SimpleSphere>(spheres[quadruple.get(0)], spheres[quadruple.get(1)], spheres[quadruple.get(2)], spheres[quadruple.get(3)]);
+							if(tangents.size()==1 && find_any_collision(bsh, tangents.front()).empty())
 							{
-								result.push_back(Face<SphereType>(bsh.leaves_spheres(), quadruple.exclude(i), bsh.min_input_radius()));
+								result.push_back(Face<SphereType>(bsh.leaves_spheres(), triple, bsh.min_input_radius()));
+								return result;
 							}
-							return result;
 						}
 					}
 				}
