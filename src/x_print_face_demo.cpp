bf251833f4a929ff63b0b0ff0bc62445ad3dfe04
kliment <kliment@localhost>
2013-05-14 14:20:03 +0300

    Improved face demo.

--- src/x_print_face_demo.cpp
+++ src/x_print_face_demo.cpp
@@ -1,3 +1,5 @@
+#include <deque>
+
 #include "apollo2/apollonius_triangulation/tangent_sphere_of_three_spheres.h"
 
 #include "auxiliaries/command_line_options.h"
@@ -26,18 +28,32 @@ void x_print_face_demo(const auxiliaries::CommandLineOptions& clo)
 	if(min_tangents.size()==1)
 	{
 		const apollo2::SimpleSphere min_tangent=min_tangents.front();
-		opengl_printer.print_sphere(apollo2::SimpleSphere(min_tangent), auxiliaries::Color::from_code(0x37DE6A));
+		opengl_printer.print_sphere(apollo2::SimpleSphere(min_tangent, 0.1), auxiliaries::Color::from_code(0xA61700));
 
-		const double r_mult=1.01;
-		const double r_max=5;
-		for(double r=min_tangent.r;r<r_max;r=r*r_mult)
+		std::deque<apollo2::SimplePoint> curve;
 		{
-			const std::vector<apollo2::SimpleSphere> tangent_spheres=apollo2::apollonius_triangulation::TangentSphereOfThreeSpheres::calculate(generators[0], generators[1], generators[2], r);
-			if(tangent_spheres.size()==2)
+			const double r_mult=1.01;
+			const double r_max=5;
+			for(double r=min_tangent.r;r<r_max;r=r*r_mult)
 			{
-				opengl_printer.print_sphere(apollo2::SimpleSphere(tangent_spheres[0]), auxiliaries::Color::from_code(0x37DE6A));
-				opengl_printer.print_sphere(apollo2::SimpleSphere(tangent_spheres[1]), auxiliaries::Color::from_code(0x37DE6A));
+				const std::vector<apollo2::SimpleSphere> tangent_spheres=apollo2::apollonius_triangulation::TangentSphereOfThreeSpheres::calculate(generators[0], generators[1], generators[2], r);
+				if(tangent_spheres.size()==2)
+				{
+					for(std::size_t i=0;i<tangent_spheres.size();i++)
+					{
+						const double h=apollo2::halfspace_of_point(generators[0], apollo2::plane_normal_from_three_points<apollo2::SimplePoint>(generators[0], generators[1], generators[2]), tangent_spheres[i]);
+						if(h<0)
+						{
+							curve.push_front(apollo2::SimplePoint(tangent_spheres[i]));
+						}
+						else
+						{
+							curve.push_back(apollo2::SimplePoint(tangent_spheres[i]));
+						}
+					}
+				}
 			}
 		}
+		opengl_printer.print_line_strip(std::vector<apollo2::SimplePoint>(curve.begin(), curve.end()));
 	}
 }
