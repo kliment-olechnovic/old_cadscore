d5c0bf5ee09eec9e13a6fda44df0aa12257cbf0b
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2013-05-14 10:01:32 +0300

    Simplified BSH demo.

--- src/x_print_bsh_demo.cpp
+++ src/x_print_bsh_demo.cpp
@@ -23,46 +23,29 @@ void x_print_bsh_demo(const auxiliaries::CommandLineOptions& clo)
 		std::cout << "from pymol.cgo import *\n";
 		std::cout << "from pymol import cmd\n\n";
 
+		{
+			std::ostringstream obj_name;
+			obj_name << "obj_as";
+			std::ostringstream cgo_name;
+			cgo_name << "cgo_as";
+			auxiliaries::OpenGLPrinter opengl_printer(obj_name.str(), cgo_name.str());
+			for(std::size_t i=0;i<atoms.size();i++)
+			{
+				opengl_printer.print_sphere(atoms[i], auxiliaries::Color::from_code(0x36BBCE));
+			}
+		}
+
 		for(std::size_t l=0;l<bsh.levels() && l<=max_level;l++)
 		{
 			const std::vector<apollo2::SimpleSphere> bs=bsh.collect_bounding_spheres(l);
-
-			std::vector<unsigned int> bs_color_codes(bs.size(), 0xFFFFFF);
-			std::vector<unsigned int> atoms_color_codes(atoms.size(), 0xFFFFFF);
+			std::ostringstream obj_name;
+			obj_name << "obj_bs" << l;
+			std::ostringstream cgo_name;
+			cgo_name << "cgo_bs" << l;
+			auxiliaries::OpenGLPrinter opengl_printer(obj_name.str(), cgo_name.str());
 			for(std::size_t i=0;i<bs.size();i++)
 			{
-				bs_color_codes[i]=rand()%(0xFFFFFF);
-				for(std::size_t j=0;j<atoms.size();j++)
-				{
-					if(apollo2::sphere_contains_sphere(bs[i], atoms[j]))
-					{
-						atoms_color_codes[j]=bs_color_codes[i];
-					}
-				}
-			}
-
-			{
-				std::ostringstream obj_name;
-				obj_name << "obj_as" << l;
-				std::ostringstream cgo_name;
-				cgo_name << "cgo_as" << l;
-				auxiliaries::OpenGLPrinter opengl_printer(obj_name.str(), cgo_name.str());
-				for(std::size_t i=0;i<atoms.size();i++)
-				{
-					opengl_printer.print_sphere(atoms[i], auxiliaries::Color::from_code(atoms_color_codes[i]));
-				}
-			}
-
-			{
-				std::ostringstream obj_name;
-				obj_name << "obj_bs" << l;
-				std::ostringstream cgo_name;
-				cgo_name << "cgo_bs" << l;
-				auxiliaries::OpenGLPrinter opengl_printer(obj_name.str(), cgo_name.str());
-				for(std::size_t i=0;i<bs.size();i++)
-				{
-					opengl_printer.print_sphere(bs[i], auxiliaries::Color::from_code(bs_color_codes[i]));
-				}
+				opengl_printer.print_sphere(bs[i], auxiliaries::Color::from_code(0x37DE6A));
 			}
 		}
 	}
