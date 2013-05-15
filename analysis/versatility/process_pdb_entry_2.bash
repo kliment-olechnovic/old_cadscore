93459019f95fc8b343c2e8a9c29e9e0a5dadc7a7
kliment <kliment@localhost>
2013-05-15 16:09:49 +0300

    Improved versatility testing scripts.

--- analysis/versatility/process_pdb_entry_2.bash
+++ analysis/versatility/process_pdb_entry_2.bash
@@ -5,7 +5,7 @@ PDB_FILE_ZIPPED=$2
 
 cd $(dirname "$0")
 
-QTFIER="QTFier64"
+QTFIER="./QTFier64"
 
 if [ -z "$PDB_FILE_ZIPPED" ] || [ -z "$OUTPUT_DIRECTORY" ]
 then
@@ -27,7 +27,7 @@ OUTPUT_ERR="$OUTPUT_DIRECTORY/$PDB_FILE_BASENAME.qtfier.err"
 
 zcat "$PDB_FILE_ZIPPED" | awk '/^END/{exit}1' | egrep '^ATOM' > $ATOMS_FILE
 
-( time -p ($QTFIER $ATOMS_FILE $TMP_DIR/ &> $CALC_ERR) ) 2> $TIME_LOG
+( time -p (timeout 1200 $QTFIER $ATOMS_FILE $TMP_DIR/ &> $CALC_ERR) ) 2> $TIME_LOG
 
 cat $CALC_RESULTS | egrep '^MODEL' | awk '{print "spheres " $3}' > $CALC_LOG
 cat $CALC_RESULTS | egrep '^MODEL' | awk '{print "vertices " $4}' >> $CALC_LOG
