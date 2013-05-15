e2575a0521688402829207b00037406b8e7aee2b
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2013-05-15 16:09:49 +0300

    Improved versatility testing scripts.

--- analysis/versatility/process_pdb_entry_1.bash
+++ analysis/versatility/process_pdb_entry_1.bash
@@ -5,7 +5,7 @@ PDB_FILE_ZIPPED=$2
 
 cd $(dirname "$0")
 
-VOROPROT="../../Release/voroprot2"
+VOROPROT="./voroprot2"
 
 if [ -z "$PDB_FILE_ZIPPED" ] || [ -z "$OUTPUT_DIRECTORY" ]
 then
@@ -21,19 +21,21 @@ ATOMS_FILE="$TMP_DIR/input.atoms"
 CALC_LOG="$TMP_DIR/calc.log"
 CALC_ERR="$TMP_DIR/calc.err"
 TIME_LOG="$TMP_DIR/time.log"
-OUTPUT_LOG="$OUTPUT_DIRECTORY/$PDB_FILE_BASENAME.voroprot2.log"
+OUTPUT_GOOD_LOG="$OUTPUT_DIRECTORY/$PDB_FILE_BASENAME.voroprot2.good.log"
+OUTPUT_BAD_LOG="$OUTPUT_DIRECTORY/$PDB_FILE_BASENAME.voroprot2.bad.log"
 OUTPUT_ERR="$OUTPUT_DIRECTORY/$PDB_FILE_BASENAME.voroprot2.err"
 
-zcat "$PDB_FILE_ZIPPED" | awk '/^END/{exit}1' | egrep '^ATOM' | $VOROPROT --mode collect-atoms --radius-classes "resources/vdwr_classes" --radius-members "resources/vdwr_members" > $ATOMS_FILE
+zcat "$PDB_FILE_ZIPPED" | awk '/^END/{exit}1' | egrep '^ATOM' | $VOROPROT --mode collect-atoms --radius-classes "resources/vdwr_classes" --radius-members "resources/vdwr_members" --include-insertions > $ATOMS_FILE
 
-( time -p (cat $ATOMS_FILE | $VOROPROT --mode calc-quadruples --monitoring-level 9 --clog-file $CALC_LOG > /dev/null 2> $CALC_ERR) ) 2> $TIME_LOG
+( time -p (cat $ATOMS_FILE | $VOROPROT --mode x-calc-quadruples-2 --print-log --clog-file $CALC_LOG > /dev/null 2> $CALC_ERR) ) 2> $TIME_LOG
 
-echo "input $PDB_FILE_BASENAME" > $OUTPUT_LOG
-cat $CALC_LOG $TIME_LOG | sed 's/^/voroprot2_/' >> $OUTPUT_LOG
+echo "input $PDB_FILE_BASENAME" > $OUTPUT_GOOD_LOG
+cat $CALC_LOG $TIME_LOG | sed 's/^/voroprot2_/' >> $OUTPUT_GOOD_LOG
 
 if [ -s $CALC_ERR ]
 then
   mv $CALC_ERR $OUTPUT_ERR
+  mv $OUTPUT_GOOD_LOG $OUTPUT_BAD_LOG
 fi
 
 rm -r "$TMP_DIR"
