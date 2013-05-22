f549d0019531e89d6d1dc9411f19b10c520dc466
kliment <kliment@localhost>
2013-05-22 17:03:59 +0300

    Improved versatility testing.

--- analysis/versatility/compare.bash
+++ analysis/versatility/compare.bash
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-PDB_FILE_ZIPPED=$1
+OUTPUT_DIRECTORY=$1
+PDB_FILE_UNZIPPED=$2
 
 cd $(dirname "$0")
 
 VOROPROT="./voroprot2"
 QTFIER="./QTFier32"
 
-if [ -z "$PDB_FILE_ZIPPED" ]
+if [ -z "$PDB_FILE_UNZIPPED" ] || [ -z "$OUTPUT_DIRECTORY" ]
 then
   echo "Fatal error: parameters not provided" 1>&2
   exit 1
 fi
 
-PDB_FILE_BASENAME=$(basename $PDB_FILE_ZIPPED .ent.gz)
+PDB_FILE_BASENAME=$(basename $PDB_FILE_UNZIPPED .ent)
 
 TMP_DIR=$(mktemp -d)
 
@@ -25,8 +26,9 @@ VOROPROT_INPUT_FILE="$TMP_DIR/input.balls"
 VOROPROT_CALC_RESULTS="$TMP_DIR/vertices"
 VOROPROT_CALC_QUADRUPLES="$VOROPROT_CALC_RESULTS.quadruples"
 VOROPROT_CALC_LOG="$VOROPROT_CALC_RESULTS.log"
+OUTPUT_RESULTS="$OUTPUT_DIRECTORY/$PDB_FILE_BASENAME.results"
 
-zcat "$PDB_FILE_ZIPPED" | awk '/^END/{exit}1' | egrep '^ATOM' > $QTFIER_INPUT_FILE
+cat "$PDB_FILE_UNZIPPED" | awk '/^END/{exit}1' | egrep '^ATOM' > $QTFIER_INPUT_FILE
 
 timeout 1200 $QTFIER $QTFIER_INPUT_FILE $TMP_DIR/
 cat $QTFIER_CALC_RESULTS | egrep '^QTCELL' | awk '{print ($3-1) " " ($4-1) " " ($5-1) " " ($6-1)}' | awk '{split($0,array," "); asort(array); printf array[1] " " array[2] " " array[3] " " array[4] "\n"}' | sort > $QTFIER_CALC_QUADRUPLES
@@ -39,9 +41,9 @@ then
   cat $VOROPROT_INPUT_FILE | $VOROPROT --mode x-calc-quadruples-2 --print-log --clog-file $VOROPROT_CALC_LOG > $VOROPROT_CALC_RESULTS
   cat $VOROPROT_CALC_RESULTS | awk '{print $1 " " $2 " " $3 " " $4}' | awk '{split($0,array," "); asort(array); printf array[1] " " array[2] " " array[3] " " array[4] "\n"}' | sort > $VOROPROT_CALC_QUADRUPLES
   
-  echo "input $PDB_FILE_BASENAME"
-  cat $QTFIER_CALC_LOG $VOROPROT_CALC_LOG
-  cat $VOROPROT_INPUT_FILE | $VOROPROT --mode x-compare-two-sets-of-quadruples-2 --file1 $QTFIER_CALC_QUADRUPLES --file2 $VOROPROT_CALC_QUADRUPLES
+  echo "input $PDB_FILE_BASENAME" > $OUTPUT_RESULTS
+  cat $QTFIER_CALC_LOG $VOROPROT_CALC_LOG >> $OUTPUT_RESULTS
+  cat $VOROPROT_INPUT_FILE | $VOROPROT --mode x-compare-two-sets-of-quadruples-2 --file1 $QTFIER_CALC_QUADRUPLES --file2 $VOROPROT_CALC_QUADRUPLES >> $OUTPUT_RESULTS
 fi
 
 rm -r "$TMP_DIR"
