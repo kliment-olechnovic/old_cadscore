ad1b29b4a09f0970354a6df0d8d456356a0fb5ee
kliment <kliment@localhost>
2013-05-26 19:13:54 +0300

    Refactored versatility testing.

--- src/apollo2/apollonius_triangulation.h
+++ src/apollo2/apollonius_triangulation.h
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <limits>
 #include <map>
+#include <algorithm>
 
 #include "apollonius_triangulation/search_for_valid_quadruples.h"
 #include "bounding_spheres_hierarchy.h"
@@ -211,6 +212,91 @@ private:
 			quadruples_ordered_map.insert(*it);
 		}
 		return quadruples_ordered_map;
+	}
+};
+
+class ComparisonOfSetsOfQuadruples
+{
+public:
+	struct DifferencesBetweenSetsOfQuadruples
+	{
+		std::vector<apollonius_triangulation::Quadruple> all_differences;
+		std::vector<apollonius_triangulation::Quadruple> confirmed_differences;
+	};
+
+	template<typename SphereType, typename InputQuadruples>
+	static DifferencesBetweenSetsOfQuadruples calculate_directional_difference_between_two_sets_of_quadruples(const std::vector<SphereType>& spheres, const double initial_radius_for_spheres_bucketing, const InputQuadruples& input_quadruples1, const InputQuadruples& input_quadruples2)
+	{
+		DifferencesBetweenSetsOfQuadruples result;
+		if(!input_quadruples1.empty())
+		{
+			const std::vector<apollonius_triangulation::Quadruple> quadruples1=get_sorted_unique_quadruples(input_quadruples1);
+			const std::vector<apollonius_triangulation::Quadruple> quadruples2=get_sorted_unique_quadruples(input_quadruples2);
+
+			result.all_differences.resize(quadruples1.size());
+			std::vector<apollonius_triangulation::Quadruple>::iterator it=std::set_difference(quadruples1.begin(), quadruples1.end(), quadruples2.begin(), quadruples2.end(), result.all_differences.begin());
+			result.all_differences.resize(it-result.all_differences.begin());
+
+			if(!result.all_differences.empty())
+			{
+				const BoundingSpheresHierarchy<SphereType> bsh(spheres, initial_radius_for_spheres_bucketing, 1);
+				const std::vector<SimpleSphere> empty_tangent_spheres2=get_empty_tangent_spheres_from_quadruples(bsh, quadruples2);
+				for(std::size_t i=0;i<result.all_differences.size();i++)
+				{
+					const apollonius_triangulation::Quadruple q=result.all_differences[i];
+					if(q.get(0)<bsh.leaves_spheres().size() && q.get(1)<bsh.leaves_spheres().size() && q.get(2)<bsh.leaves_spheres().size() && q.get(3)<bsh.leaves_spheres().size())
+					{
+						const std::vector<SimpleSphere> tangents=apollonius_triangulation::TangentSphereOfFourSpheres::calculate<SimpleSphere>(bsh.leaves_spheres().at(q.get(0)), bsh.leaves_spheres().at(q.get(1)), bsh.leaves_spheres().at(q.get(2)), bsh.leaves_spheres().at(q.get(3)));
+						if(
+								(tangents.size()==1 && apollonius_triangulation::find_any_collision(bsh, tangents.front()).empty())
+								|| (tangents.size()==2 && (apollonius_triangulation::find_any_collision(bsh, tangents.front()).empty() || apollonius_triangulation::find_any_collision(bsh, tangents.back()).empty()))
+							)
+						{
+							result.confirmed_differences.push_back(q);
+						}
+					}
+				}
+			}
+		}
+		return result;
+	}
+
+private:
+	template<typename InputQuadruples>
+	static std::vector<apollonius_triangulation::Quadruple> get_sorted_unique_quadruples(const InputQuadruples& input_quadruples)
+	{
+		std::vector<apollonius_triangulation::Quadruple> result(input_quadruples.begin(), input_quadruples.end());
+		if(!result.empty())
+		{
+			std::sort(result.begin(), result.end());
+			std::vector<apollonius_triangulation::Quadruple>::iterator it=std::unique(result.begin(), result.end());
+			result.resize(it-result.begin());
+		}
+		return result;
+	}
+
+	template<typename SphereType, typename InputQuadruples>
+	static std::vector<SimpleSphere> get_empty_tangent_spheres_from_quadruples(const BoundingSpheresHierarchy<SphereType>& bsh, const InputQuadruples& quadruples)
+	{
+		std::vector<SimpleSphere> result;
+		result.reserve(quadruples.size()+(quadruples.size()/10));
+		for(typename InputQuadruples::const_iterator it=quadruples.begin();it!=quadruples.end();++it)
+		{
+			const apollonius_triangulation::Quadruple& q=(*it);
+			if(q.get(0)<bsh.leaves_spheres().size() && q.get(1)<bsh.leaves_spheres().size() && q.get(2)<bsh.leaves_spheres().size() && q.get(3)<bsh.leaves_spheres().size())
+			{
+				const std::vector<SimpleSphere> tangent_spheres=apollonius_triangulation::TangentSphereOfFourSpheres::calculate<SimpleSphere>(bsh.leaves_spheres().at(q.get(0)), bsh.leaves_spheres().at(q.get(1)), bsh.leaves_spheres().at(q.get(2)), bsh.leaves_spheres().at(q.get(3)));
+				for(std::size_t j=0;j<tangent_spheres.size();j++)
+				{
+					const SimpleSphere& tangent_sphere=tangent_spheres[j];
+					if(apollonius_triangulation::find_any_collision(bsh, tangent_sphere).empty())
+					{
+						result.push_back(tangent_sphere);
+					}
+				}
+			}
+		}
+		return result;
 	}
 };
 
--- src/x_compare_two_sets_of_quadruples_2.cpp
+++ src/x_compare_two_sets_of_quadruples_2.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <algorithm>
 #include <limits>
 
 #include "apollo2/apollonius_triangulation.h"
@@ -40,44 +39,6 @@ std::vector<apollo2::apollonius_triangulation::Quadruple> read_quadruples_from_s
 	return result;
 }
 
-std::vector<apollo2::SimpleSphere> get_empty_tangent_spheres_from_quadruples(const apollo2::BoundingSpheresHierarchy<apollo2::SimpleSphere>& bsh, const std::vector<apollo2::apollonius_triangulation::Quadruple>& quadruples)
-{
-	std::vector<apollo2::SimpleSphere> result;
-	result.reserve(quadruples.size()+(quadruples.size()/10));
-	for(std::size_t i=0;i<quadruples.size();i++)
-	{
-		const apollo2::apollonius_triangulation::Quadruple q=quadruples[i];
-		if(q.get(0)<bsh.leaves_spheres().size() && q.get(1)<bsh.leaves_spheres().size() && q.get(2)<bsh.leaves_spheres().size() && q.get(3)<bsh.leaves_spheres().size())
-		{
-			const std::vector<apollo2::SimpleSphere> tangents=apollo2::apollonius_triangulation::TangentSphereOfFourSpheres::calculate<apollo2::SimpleSphere>(bsh.leaves_spheres().at(q.get(0)), bsh.leaves_spheres().at(q.get(1)), bsh.leaves_spheres().at(q.get(2)), bsh.leaves_spheres().at(q.get(3)));
-			for(std::size_t j=0;j<tangents.size();j++)
-			{
-				const apollo2::SimpleSphere& tangent=tangents[j];
-				if(apollo2::apollonius_triangulation::find_any_collision(bsh, tangent).empty())
-				{
-					result.push_back(tangent);
-				}
-			}
-		}
-	}
-	return result;
-}
-
-std::tr1::unordered_set<apollo2::apollonius_triangulation::Triple, apollo2::apollonius_triangulation::Triple::HashFunctor> collect_triples_from_quadruples(const std::vector<apollo2::apollonius_triangulation::Quadruple>& quadruples)
-{
-	std::tr1::unordered_set<apollo2::apollonius_triangulation::Triple, apollo2::apollonius_triangulation::Triple::HashFunctor> result;
-	for(std::size_t i=0;i<quadruples.size();i++)
-	{
-		const apollo2::apollonius_triangulation::Quadruple q=quadruples[i];
-		for(int j=0;j<4;j++)
-		{
-			result.insert(q.exclude(j));
-		}
-
-	}
-	return result;
-}
-
 void x_compare_two_sets_of_quadruples_2(const auxiliaries::CommandLineOptions& clo)
 {
 	clo.check_allowed_options("--file1: --file2:");
@@ -93,73 +54,8 @@ void x_compare_two_sets_of_quadruples_2(const auxiliaries::CommandLineOptions& c
 	std::vector<apollo2::apollonius_triangulation::Quadruple> quadruples1=read_quadruples_from_stream(stream1);
 	std::vector<apollo2::apollonius_triangulation::Quadruple> quadruples2=read_quadruples_from_stream(stream2);
 
-	std::vector<apollo2::apollonius_triangulation::Quadruple> difference(quadruples1.size());
-	std::vector<apollo2::apollonius_triangulation::Quadruple>::iterator it=std::set_difference(quadruples1.begin(), quadruples1.end(), quadruples2.begin(), quadruples2.end(), difference.begin());
-	difference.resize(it-difference.begin());
+	const apollo2::ComparisonOfSetsOfQuadruples::DifferencesBetweenSetsOfQuadruples differences=apollo2::ComparisonOfSetsOfQuadruples::calculate_directional_difference_between_two_sets_of_quadruples(atoms, 3.5, quadruples1, quadruples2);
 
-	int confirmed_differences=0;
-	int confirmed_differences_with_ball_between_planes=0;
-	double max_min_perturbation=0.0;
-	int confirmed_differences_with_found_triples=0;
-	if(!difference.empty())
-	{
-		apollo2::BoundingSpheresHierarchy<apollo2::SimpleSphere> bsh(atoms, 3.5, 1);
-		const std::vector<apollo2::SimpleSphere> empty_tangent_spheres2=get_empty_tangent_spheres_from_quadruples(bsh, quadruples2);
-		const std::tr1::unordered_set<apollo2::apollonius_triangulation::Triple, apollo2::apollonius_triangulation::Triple::HashFunctor> triples2=collect_triples_from_quadruples(quadruples2);
-		for(std::size_t i=0;i<difference.size();i++)
-		{
-			const apollo2::apollonius_triangulation::Quadruple q=difference[i];
-			if(q.get(0)<atoms.size() && q.get(1)<atoms.size() && q.get(2)<atoms.size() && q.get(3)<atoms.size())
-			{
-				const std::vector<apollo2::SimpleSphere> tangents=apollo2::apollonius_triangulation::TangentSphereOfFourSpheres::calculate<apollo2::SimpleSphere>(atoms.at(q.get(0)), atoms.at(q.get(1)), atoms.at(q.get(2)), atoms.at(q.get(3)));
-				bool some_tangents_are_empty=false;
-				for(std::size_t j=0;j<tangents.size();j++)
-				{
-					const apollo2::SimpleSphere& tangent=tangents[j];
-					if(apollo2::apollonius_triangulation::find_any_collision(bsh, tangent).empty())
-					{
-						some_tangents_are_empty=true;
-						double min_perturbation=std::numeric_limits<double>::max();
-						for(std::size_t e=0;e<empty_tangent_spheres2.size();e++)
-						{
-							const double candidate_perturbation=apollo2::distance_from_point_to_point(tangent, empty_tangent_spheres2[e]);
-							if(candidate_perturbation<min_perturbation)
-							{
-								min_perturbation=candidate_perturbation;
-							}
-						}
-						if(min_perturbation>max_min_perturbation)
-						{
-							max_min_perturbation=min_perturbation;
-						}
-					}
-				}
-				if(some_tangents_are_empty)
-				{
-					confirmed_differences++;
-					if(tangents.size()==2)
-					{
-						confirmed_differences_with_ball_between_planes++;
-					}
-					bool found_triple=false;
-					for(int j=0;j<4 && !found_triple;j++)
-					{
-						if(triples2.count(q.exclude(j))>0)
-						{
-							confirmed_differences_with_found_triples++;
-							found_triple=true;
-						}
-					}
-				}
-			}
-		}
-	}
-
-	std::cout.precision(std::numeric_limits<double>::digits10);
-	std::cout << std::fixed;
-	std::cout << "all_differences " << difference.size() << "\n";
-	std::cout << "confirmed_differences " << confirmed_differences << "\n";
-	std::cout << "confirmed_differences_with_ball_between_planes " << confirmed_differences_with_ball_between_planes << "\n";
-	std::cout << "confirmed_differences_with_found_triples " << confirmed_differences_with_found_triples << "\n";
-	std::cout << "max_min_perturbation " << max_min_perturbation << "\n";
+	std::cout << "all_differences " << differences.all_differences.size() << "\n";
+	std::cout << "confirmed_differences " << differences.confirmed_differences.size() << "\n";
 }
