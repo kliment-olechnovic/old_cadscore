cc45b664aeabcd1c84ab88e11d6d31235737c548
kliment <kliment@localhost>
2013-05-30 16:48:58 +0300

    Refactored nucleotide contacts typing.

--- src/categorize_inter_nucleotide_side_chain_contacts.cpp
+++ src/categorize_inter_nucleotide_side_chain_contacts.cpp
@@ -1,5 +1,5 @@
 #include "protein/atom.h"
-#include "protein/nucleotide_plane.h"
+#include "protein/residue_ids_collection.h"
 
 #include "apollo2/basic_operations_on_spheres.h"
 
@@ -10,9 +10,75 @@
 #include "auxiliaries/vector_io.h"
 #include "auxiliaries/map_io.h"
 
+namespace
+{
+
+template<typename PointType>
+struct NucleotidePlane
+{
+	PointType point;
+	PointType normal;
+
+	static std::map<protein::ResidueID, NucleotidePlane> calc_nucleotides_planes(const std::vector<protein::Atom>& atoms)
+	{
+		std::map<protein::ResidueID, NucleotidePlane> planes;
+		const std::map<protein::ResidueID, std::vector<std::size_t> > residue_ids_atoms=protein::group_atoms_indices_by_residue_ids(atoms);
+		for(std::map<protein::ResidueID, std::vector<std::size_t> >::const_iterator it=residue_ids_atoms.begin();it!=residue_ids_atoms.end();++it)
+		{
+			const protein::ResidueID& residue_id=it->first;
+			const std::vector<std::size_t> atoms_ids=it->second;
+			if(!atoms_ids.empty())
+			{
+				if(atoms.at(atoms_ids.front()).molecule_class==protein::Atom::nucleotide)
+				{
+					std::vector<std::size_t> abc;
+					for(std::size_t i=0;i<atoms_ids.size() && abc.size()<3;i++)
+					{
+						const std::size_t a_id=atoms_ids[i];
+						const protein::Atom& a=atoms.at(a_id);
+						if(a.atom_name=="C2" || a.atom_name=="C4" || a.atom_name=="C6")
+						{
+							abc.push_back(a_id);
+						}
+					}
+					if(abc.size()==3)
+					{
+						const protein::Atom& a=atoms[abc[0]];
+						const protein::Atom& b=atoms[abc[1]];
+						const protein::Atom& c=atoms[abc[2]];
+						const PointType pa(a.x, a.y, a.z);
+						const PointType pb(b.x, b.y, b.z);
+						const PointType pc(c.x, c.y, c.z);
+						NucleotidePlane& plane=planes[residue_id];
+						plane.point=((pa+pb)+pc)*(1.0/3.0);
+						plane.normal=((pb-pa)&(pc-pa)).unit();
+					}
+				}
+			}
+		}
+		return planes;
+	}
+
+	friend std::ostream& operator<<(std::ostream &output, const NucleotidePlane& plane)
+	{
+		output << plane.point.x << " " << plane.point.y << " " << plane.point.z << " ";
+		output << plane.normal.x << " " << plane.normal.y << " " << plane.normal.z;
+		return output;
+	}
+
+	friend std::istream& operator>>(std::istream& input, NucleotidePlane& plane)
+	{
+		input >> plane.point.x >> plane.point.y >> plane.point.z;
+		input >> plane.normal.x >> plane.normal.y >> plane.normal.z;
+		return input;
+	}
+};
+
+}
+
 void categorize_inter_nucleotide_side_chain_contacts(const auxiliaries::CommandLineOptions& clo)
 {
-	typedef protein::NucleotidePlane<apollo2::SimplePoint> Plane;
+	typedef NucleotidePlane<apollo2::SimplePoint> Plane;
 
 	clo.check_allowed_options("");
 
