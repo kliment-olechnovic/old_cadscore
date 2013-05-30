c4bdf3ceaa9189229289a678a9482d8970c879af
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2013-05-30 11:14:39 +0300

    Removed unused scripts.

--- scripts/customized/customized_print_nucleotides_typed_contacts.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-VOROPROT=$1
-INPUT=$2
-AREA_TYPE=$3
-
-(cat $INPUT | $VOROPROT --mode collect-atoms ; cat $INPUT | $VOROPROT --mode collect-atoms | $VOROPROT --mode "calc-inter-atom-$AREA_TYPE" | $VOROPROT --mode calc-inter-residue-contacts) | $VOROPROT --mode x-print-stacking-nucleotides-contacts --prefix $(basename $INPUT)
