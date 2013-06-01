4c1bf27a3f8b56fe7d116248b3e86be85a85ab41
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2013-06-01 18:24:28 +0300

    Started nucleotide contacts annotations collection testing.

--- analysis/rna_speciality/reformatting_annotations/scripts/reformat_output_of_mc_annotate.bash
+++ analysis/rna_speciality/reformatting_annotations/scripts/reformat_output_of_mc_annotate.bash
@@ -2,5 +2,5 @@
 
 INPUT=$1
 
-echo "contact_id prop1 prop2 prop3 prop4 prop5 prop6"
-cat $INPUT | egrep -v "'" | sed 's/.pdb//' | awk '{print $1 "_" $2 " P_" $4 " P_" $5 " P_" $6 " P_" $7 " P_" $8 " P_" $9}'
+echo "pdb_id contact_id prop1 prop2 prop3 prop4 prop5 prop6"
+cat $INPUT | egrep -v "'" | sed 's/.pdb//' | awk '{print $1 " " $1 "_" $2 " P_" $4 " P_" $5 " P_" $6 " P_" $7 " P_" $8 " P_" $9}'
