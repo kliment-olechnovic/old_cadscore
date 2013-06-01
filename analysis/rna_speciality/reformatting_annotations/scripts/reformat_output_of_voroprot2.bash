f1bd9f65d945b0154f9a25c7c54fee4e516a4844
kliment <kliment@localhost>
2013-06-01 16:54:43 +0300

    Started nucleotide contacts annotations collection testing.

--- analysis/rna_speciality/reformatting_annotations/scripts/reformat_output_of_voroprot2.bash
+++ analysis/rna_speciality/reformatting_annotations/scripts/reformat_output_of_voroprot2.bash
@@ -3,4 +3,4 @@
 INPUT=$1
 
 echo "contact_id first_base_name second_base_name contact_type contact_area"
-cat $INPUT | egrep -v 'left_to_right' | sed 's/.pdb//' | sed 's/na_stacking/stack/' | sed 's/na_siding/side/' | awk '{print $1 "_" $2 $3 "-" $5 $6 " " $4 " " $7 " " $8 " " $9}'
+cat $INPUT | egrep 'left_to_right' | sed 's/.pdb//' | sed 's/na_stacking/stack/' | sed 's/na_siding/side/' | awk '{print $1 "_" $2 $3 "-" $5 $6 " " $4 " " $7 " " $8 " " $9}'
