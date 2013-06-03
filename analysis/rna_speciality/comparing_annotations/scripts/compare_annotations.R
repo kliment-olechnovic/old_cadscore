224d0ca5b3207326f7292de6c3b4370dbbdfedf2
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2013-06-03 11:31:36 +0300

    Improved some nucleobases analysis.

--- analysis/rna_speciality/comparing_annotations/scripts/compare_annotations.R
+++ analysis/rna_speciality/comparing_annotations/scripts/compare_annotations.R
@@ -13,6 +13,9 @@ pdb_ids_mcannotate=union(t_stack_mcannotate$pdb_id, t_side_mcannotate$pdb_id);
 t_any=t_contacts;
 t_any=t_any[which(is.element(t_any$pdb_id, pdb_ids_mcannotate)),];
 
+max_contact_area=300;
+t_any=t_any[which(t_any$contact_area<=max_contact_area),];
+
 allowed_basenames=c("A", "U", "G", "C");
 t_any=t_any[which(is.element(t_any$first_base_name, allowed_basenames)),];
 t_any=t_any[which(is.element(t_any$second_base_name, allowed_basenames)),];
@@ -46,53 +49,57 @@ t_side_sel=t_side[which(is.element(t_side$contact_id, ids_side_mcannotate)),];
 
 #################
 
-hist(t_any$contact_area, breaks=100, xlab="Area", main="Histogram of all areas", xlim=c(0, 250));
-hist(t_any_sel$contact_area, breaks=100, add=TRUE, col="red");
+hist_bins=50;
+hist_breaks=(0:hist_bins)*(max_contact_area/hist_bins);
+
+#################
+
+hist(t_any$contact_area, breaks=hist_breaks, xlab="Area", main="Histogram of all areas", xlim=c(0, max_contact_area));
+hist(t_any_sel$contact_area, breaks=hist_breaks, add=TRUE, col="red");
 
 ds_any=density(t_any$contact_area);
 ds_any_sel=density(t_any_sel$contact_area);
 
-plot(ds_any$x, ds_any$y*length(t_any[[1]]), type="l", col="blue", xlim=c(0, 250), main="Densities for all areas", xlab="Area", ylab="Frequency");
+plot(ds_any$x, ds_any$y*length(t_any[[1]]), type="l", col="blue", xlim=c(0, max_contact_area), main="Densities for all areas", xlab="Area", ylab="Frequency");
 points(ds_any_sel$x, ds_any_sel$y*length(t_any_sel[[1]]), type="l", col="red");
 
-plot(ds_any$x, ds_any$y*length(t_any[[1]])*ds_any$x, type="l", col="blue", xlim=c(0, 250), main="Densities for all contributions of areas", xlab="Area", ylab="Area * Frequency");
+plot(ds_any$x, ds_any$y*length(t_any[[1]])*ds_any$x, type="l", col="blue", xlim=c(0, max_contact_area), main="Densities for all contributions of areas", xlab="Area", ylab="Area * Frequency");
 points(ds_any_sel$x, ds_any_sel$y*length(t_any_sel[[1]])*ds_any_sel$x, type="l", col="red");
 
 #####
 
-hist(t_stack$contact_area, breaks=100, xlab="Area", main="Histogram of stacking areas", xlim=c(0, 250));
-hist(t_stack_sel$contact_area, breaks=100, add=TRUE, col="red");
+hist(t_stack$contact_area, breaks=hist_breaks, xlab="Area", main="Histogram of stacking areas", xlim=c(0, max_contact_area));
+hist(t_stack_sel$contact_area, breaks=hist_breaks, add=TRUE, col="red");
 
 ds_stack=density(t_stack$contact_area);
 ds_stack_sel=density(t_stack_sel$contact_area);
 
-plot(ds_stack$x, ds_stack$y*length(t_stack[[1]]), type="l", col="blue", xlim=c(0, 250), main="Densities for stacking areas", xlab="Area", ylab="Frequency");
+plot(ds_stack$x, ds_stack$y*length(t_stack[[1]]), type="l", col="blue", xlim=c(0, max_contact_area), main="Densities for stacking areas", xlab="Area", ylab="Frequency");
 points(ds_stack_sel$x, ds_stack_sel$y*length(t_stack_sel[[1]]), type="l", col="red");
 
-plot(ds_stack$x, ds_stack$y*length(t_stack[[1]])*ds_stack$x, type="l", col="blue", xlim=c(0, 250), main="Densities for contributions of stacking areas", xlab="Area", ylab="Area * Frequency");
+plot(ds_stack$x, ds_stack$y*length(t_stack[[1]])*ds_stack$x, type="l", col="blue", xlim=c(0, max_contact_area), main="Densities for contributions of stacking areas", xlab="Area", ylab="Area * Frequency");
 points(ds_stack_sel$x, ds_stack_sel$y*length(t_stack_sel[[1]])*ds_stack_sel$x, type="l", col="red");
 
 #####
 
-hist(t_side$contact_area, breaks=100, xlab="Area", main="Histogram of siding areas", xlim=c(0, 250));
-hist(t_side_sel$contact_area, breaks=100, add=TRUE, col="red");
+hist(t_side$contact_area, breaks=hist_breaks, xlab="Area", main="Histogram of siding areas", xlim=c(0, max_contact_area));
+hist(t_side_sel$contact_area, breaks=hist_breaks, add=TRUE, col="red");
 
 ds_side=density(t_side$contact_area);
 ds_side_sel=density(t_side_sel$contact_area);
 
-plot(ds_side$x, ds_side$y*length(t_side[[1]]), type="l", col="blue", xlim=c(0, 250), main="Densities for siding areas", xlab="Area", ylab="Frequency");
+plot(ds_side$x, ds_side$y*length(t_side[[1]]), type="l", col="blue", xlim=c(0, max_contact_area), main="Densities for siding areas", xlab="Area", ylab="Frequency");
 points(ds_side_sel$x, ds_side_sel$y*length(t_side_sel[[1]]), type="l", col="red");
 
-plot(ds_side$x, ds_side$y*length(t_side[[1]])*ds_side$x, type="l", col="blue", xlim=c(0, 250), main="Densities for contributions of siding areas", xlab="Area", ylab="Area * Frequency");
+plot(ds_side$x, ds_side$y*length(t_side[[1]])*ds_side$x, type="l", col="blue", xlim=c(0, max_contact_area), main="Densities for contributions of siding areas", xlab="Area", ylab="Area * Frequency");
 points(ds_side_sel$x, ds_side_sel$y*length(t_side_sel[[1]])*ds_side_sel$x, type="l", col="red");
 
 #################
 
-h_breaks=0:30*(300/30);
-h_xx=hist(t_stack_sel[, "contact_area"], breaks=h_breaks);
-h_bb=hist(t_stack_sel[which(t_stack_sel$basepairs=="bb"), "contact_area"], breaks=h_breaks);
-h_bB=hist(t_stack_sel[which(t_stack_sel$basepairs=="bB"), "contact_area"], breaks=h_breaks);
-h_BB=hist(t_stack_sel[which(t_stack_sel$basepairs=="BB"), "contact_area"], breaks=h_breaks);
+h_xx=hist(t_stack_sel[, "contact_area"], breaks=hist_breaks);
+h_bb=hist(t_stack_sel[which(t_stack_sel$basepairs=="bb"), "contact_area"], breaks=hist_breaks);
+h_bB=hist(t_stack_sel[which(t_stack_sel$basepairs=="bB"), "contact_area"], breaks=hist_breaks);
+h_BB=hist(t_stack_sel[which(t_stack_sel$basepairs=="BB"), "contact_area"], breaks=hist_breaks);
 
 barplot(rbind(h_bb$counts, h_bB$counts, h_BB$counts), names.arg=h_xx$mids, col=c("red", "green", "blue"));
 
@@ -121,8 +128,7 @@ nonroman_ids_side_mcannotate=t_side_mcannotate$contact_id[nonroman_sel];
 nonroman_ids_side_mcannotate=union(nonroman_ids_side_mcannotate, nonroman_ids_side_mcannotate);
 nonroman_t_side_sel=t_side[which(is.element(t_side$contact_id, nonroman_ids_side_mcannotate)),];
 
-h_breaks=0:20*(200/20);
-roman_h=hist(roman_t_side_sel$contact_area[which(roman_t_side_sel$contact_area<max(h_breaks))], breaks=h_breaks);
-nonroman_h=hist(nonroman_t_side_sel$contact_area[which(nonroman_t_side_sel$contact_area<max(h_breaks))], breaks=h_breaks);
+roman_h=hist(roman_t_side_sel$contact_area, breaks=hist_breaks);
+nonroman_h=hist(nonroman_t_side_sel$contact_area, breaks=hist_breaks);
 
 barplot(rbind(roman_h$counts, nonroman_h$counts), names.arg=roman_h$mids, col=c("red", "blue"));
