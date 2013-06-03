eea906c5b04b9a4864803c77d52fa7c865339472
kliment <kliment@localhost>
2013-06-03 16:03:17 +0300

    Improved some nucleobases analysis.

--- analysis/rna_speciality/collecting_annotations/scripts/run_mc_annotate_for_base_pairing.bash
+++ analysis/rna_speciality/collecting_annotations/scripts/run_mc_annotate_for_base_pairing.bash
@@ -4,4 +4,11 @@ MCANNOTATE="./MC-Annotate"
 
 INPUT=$1
 
-$MCANNOTATE $INPUT | sed -n -e '/Base-pairs/,/-------/ p' | egrep " : " | sed "s/^/$(basename $INPUT) /"
+TMP_DIR=$(mktemp -d)
+
+ONLY_ATOMS_INPUT="$TMP_DIR/atoms.pdb"
+cat $INPUT | awk '/^END/{exit}1' | egrep '^ATOM' > $ONLY_ATOMS_INPUT
+
+$MCANNOTATE $ONLY_ATOMS_INPUT | sed -n -e '/Base-pairs/,/-------/ p' | egrep " : " | sed "s/^/$(basename $INPUT) /"
+
+rm -r $TMP_DIR
--- analysis/rna_speciality/collecting_annotations/scripts/run_mc_annotate_for_stacking.bash
+++ analysis/rna_speciality/collecting_annotations/scripts/run_mc_annotate_for_stacking.bash
@@ -6,7 +6,10 @@ INPUT=$1
 
 TMP_DIR=$(mktemp -d)
 
-$MCANNOTATE $INPUT > $TMP_DIR/all
+ONLY_ATOMS_INPUT="$TMP_DIR/atoms.pdb"
+cat $INPUT | awk '/^END/{exit}1' | egrep '^ATOM' > $ONLY_ATOMS_INPUT
+
+$MCANNOTATE $ONLY_ATOMS_INPUT > $TMP_DIR/all
 
 cat $TMP_DIR/all | sed -n -e '/Adjacent stackings/,/Non-Adjacent stackings/ p' > $TMP_DIR/as
 cat $TMP_DIR/all | sed -n -e '/Non-Adjacent stackings/,/Number of stackings/ p' > $TMP_DIR/nas
