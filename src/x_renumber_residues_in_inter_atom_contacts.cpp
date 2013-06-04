19f84945f60fed94bb913be1547bf2e93aa3c444
kliment <kliment@localhost>
2013-06-04 13:08:05 +0300

    Refactored some sequence tools usage.

--- src/x_renumber_residues_in_inter_atom_contacts.cpp
+++ src/x_renumber_residues_in_inter_atom_contacts.cpp
@@ -1,244 +1,16 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <map>
-#include <algorithm>
 
 #include "protein/atom.h"
 #include "protein/residue_ids_collection.h"
+#include "protein/sequence_tools.h"
 
 #include "contacto/inter_atom_contact.h"
 
 #include "auxiliaries/command_line_options.h"
 #include "auxiliaries/std_containers_io.h"
+
 namespace
 {
-
-class LettersCoding
-{
-public:
-	inline static std::string convert_residue_codes_big_to_small(const std::string& aaa)
-	{
-		static const std::map<std::string, std::string> m=create_map_of_residue_codes_big_to_small();
-		const std::map<std::string, std::string>::const_iterator it=m.find(aaa);
-		return (it==m.end() ? std::string("?") : it->second);
-	}
-
-	inline static std::string convert_residue_codes_small_to_big(const std::string& a)
-	{
-		static const std::map<std::string, std::string> m=create_map_of_residue_codes_small_to_big();
-		const std::map<std::string, std::string>::const_iterator it=m.find(a);
-		return (it==m.end() ? std::string("???") : it->second);
-	}
-private:
-	static std::map<std::string, std::string> create_map_of_residue_codes_big_to_small()
-	{
-		std::map<std::string, std::string> m;
-		m["LEU"]="L";
-		m["VAL"]="V";
-		m["ILE"]="I";
-		m["ALA"]="A";
-		m["PHE"]="F";
-		m["TRP"]="W";
-		m["MET"]="M";
-		m["PRO"]="P";
-		m["ASP"]="D";
-		m["GLU"]="E";
-		m["LYS"]="K";
-		m["ARG"]="R";
-		m["HIS"]="H";
-		m["CYS"]="C";
-		m["SER"]="S";
-		m["THR"]="T";
-		m["TYR"]="Y";
-		m["ASN"]="N";
-		m["GLN"]="Q";
-		m["GLY"]="G";
-		m["ASX"]="B";
-		m["GLX"]="Z";
-		m["A"]="a";
-		m["C"]="c";
-		m["G"]="g";
-		m["T"]="t";
-		m["U"]="u";
-		m["???"]="?";
-		return m;
-	}
-
-	static std::map<std::string, std::string> create_map_of_residue_codes_small_to_big()
-	{
-		const std::map<std::string, std::string> m_big_to_small;
-		std::map<std::string, std::string> m;
-		for(std::map<std::string, std::string>::const_iterator it=m_big_to_small.begin();it!=m_big_to_small.end();++it)
-		{
-			m[it->second]=it->first;
-		}
-		return m;
-	}
-};
-
-class PairwiseSequenceAlignment
-{
-public:
-	struct SimpleScorer
-	{
-		int match_score;
-		int mismatch_score;
-		int gap_start_score;
-		int gap_extension_score;
-
-		SimpleScorer(int match_score, int mismatch_score, int gap_start_score, int gap_extension_score) :
-			match_score(match_score),
-			mismatch_score(mismatch_score),
-			gap_start_score(gap_start_score),
-			gap_extension_score(gap_extension_score)
-		{
-		}
-
-		template<typename T>
-		int match(const T& v1, const T& v2) const
-		{
-			return (v1==v2 ? match_score : mismatch_score);
-		}
-
-		int gap_start() const
-		{
-			return gap_start_score;
-		}
-
-		int gap_extension() const
-		{
-			return gap_extension_score;
-		}
-	};
-
-	template<typename T, typename Scorer>
-	static std::vector< std::pair<int, int> > construct_global_sequence_alignment(const T& seq1, const T& seq2, const Scorer& scorer)
-	{
-		std::vector< std::pair<int, int> > alignment;
-		if(!seq1.empty() && !seq2.empty())
-		{
-			std::vector< std::vector<int> > scores_matrix(seq1.size()+1, std::vector<int>(seq2.size()+1, 0));
-			std::vector< std::vector<int> > directions_matrix(seq1.size()+1, std::vector<int>(seq2.size()+1, 0));
-			int overall_max_score=0;
-			for(std::size_t i=1;i<=seq1.size();i++)
-			{
-				scores_matrix[i][0]=((i==1) ? scorer.gap_start() : (scores_matrix[i-1][0]+scorer.gap_extension()));
-				directions_matrix[i][0]=1;
-			}
-			for(std::size_t j=1;j<=seq2.size();j++)
-			{
-				scores_matrix[0][j]=((j==1) ? scorer.gap_start() : (scores_matrix[0][j-1]+scorer.gap_extension()));
-				directions_matrix[0][j]=2;
-			}
-			for(std::size_t i=1;i<=seq1.size();i++)
-			{
-				for(std::size_t j=1;j<=seq2.size();j++)
-				{
-					const typename T::value_type& v1=seq1[i-1];
-					const typename T::value_type& v2=seq2[j-1];
-					const int match_score=scores_matrix[i-1][j-1]+scorer.match(v1, v2);
-					const int deletion_score=scores_matrix[i-1][j]+(directions_matrix[i-1][j]!=1 ? scorer.gap_start() : scorer.gap_extension());
-					const int insertion_score=scores_matrix[i][j-1]+(directions_matrix[i][j-1]!=2 ? scorer.gap_start() : scorer.gap_extension());
-					const int max_score=std::max(match_score, std::max(deletion_score, insertion_score));
-					scores_matrix[i][j]=max_score;
-					directions_matrix[i][j]=(max_score==insertion_score ? 2 : (max_score==deletion_score ? 1 : 0));
-					overall_max_score=std::max(overall_max_score, max_score);
-				}
-			}
-
-			int i=static_cast<int>(seq1.size());
-			int j=static_cast<int>(seq2.size());
-			while(i>0 && j>0)
-			{
-				const int dir=directions_matrix[i][j];
-				if(dir==0)
-				{
-					i--;
-					j--;
-					alignment.push_back(std::make_pair(i, j));
-				}
-				else if(dir==1)
-				{
-					i--;
-					alignment.push_back(std::make_pair(i, -1));
-				}
-				else
-				{
-					j--;
-					alignment.push_back(std::make_pair(-1, j));
-				}
-			}
-			while(i>0)
-			{
-				i--;
-				alignment.push_back(std::make_pair(i, -1));
-			}
-			while(j>0)
-			{
-				j--;
-				alignment.push_back(std::make_pair(-1, j));
-			}
-			std::reverse(alignment.begin(), alignment.end());
-		}
-		return alignment;
-	}
-
-	static bool print_sequence_alignment(const std::string& seq1, const std::string& seq2, const std::vector< std::pair<int, int> >& alignment, std::ostream& output)
-	{
-		for(std::size_t i=0;i<alignment.size();i++)
-		{
-			if(alignment[i].first>=static_cast<int>(seq1.size()) || alignment[i].second>=static_cast<int>(seq2.size()))
-			{
-				return false;
-			}
-		}
-
-		for(std::size_t i=0;i<alignment.size();i++)
-		{
-			const int j=alignment[i].first;
-			output << (j>=0 ? seq1.at(j) : '-');
-		}
-		output << "\n";
-
-		for(std::size_t i=0;i<alignment.size();i++)
-		{
-			const int j=alignment[i].second;
-			output << (j>=0 ? seq2.at(j) : '-');
-		}
-		output << "\n";
-
-		return true;
-	}
-
-	template<typename Scorer>
-	static bool print_global_sequence_alignment(const std::string& seq1, const std::string& seq2, const Scorer& scorer, std::ostream& output)
-	{
-		return print_sequence_alignment(seq1, seq2, construct_global_sequence_alignment(seq1, seq2, scorer), output);
-	}
-
-	static std::vector< std::pair<int, int> > read_sequence_alignment(const std::string& alignment_seq1, const std::string& alignment_seq2)
-	{
-		const std::size_t alignment_size=std::min(alignment_seq1.size(), alignment_seq2.size());
-		std::vector< std::pair<int, int> > alignment(alignment_size, std::pair<int, int>(-1, -1));
-		int pos1=0;
-		int pos2=0;
-		for(std::size_t i=0;i<alignment_size;i++)
-		{
-			if(alignment_seq1.at(i)!='-')
-			{
-				alignment[i].first=pos1;
-				pos1++;
-			}
-			if(alignment_seq2.at(i)!='-')
-			{
-				alignment[i].second=pos2;
-				pos2++;
-			}
-		}
-		return alignment;
-	}
-};
 
 std::string collect_string_without_gaps(const std::string& input)
 {
@@ -273,7 +45,7 @@ std::string collect_sequence_string_from_residue_ids_map(const T& residue_ids_ma
 	std::ostringstream output;
 	for(typename T::const_iterator it=residue_ids_map.begin();it!=residue_ids_map.end();++it)
 	{
-		output << LettersCoding::convert_residue_codes_big_to_small(it->second.name);
+		output << protein::sequence_tools::LettersCoding::convert_residue_codes_big_to_small(it->second.name);
 	}
 	return output.str();
 }
@@ -346,11 +118,11 @@ void x_renumber_residues_in_inter_atom_contacts(const auxiliaries::CommandLineOp
 		std::clog << input_alignment_string_1 << "\n";
 		std::clog << input_alignment_string_2 << "\n";
 
-		const PairwiseSequenceAlignment::SimpleScorer scorer(match_score, mismatch_score, gap_start_score, gap_extension_score);
+		const protein::sequence_tools::PairwiseSequenceAlignment::SimpleScorer scorer(match_score, mismatch_score, gap_start_score, gap_extension_score);
 		std::vector< std::pair<int, int> > alignments[3];
-		alignments[0]=PairwiseSequenceAlignment::construct_global_sequence_alignment(sequence_string_from_atoms_1, sequence_string_from_input_alignment_1, scorer);
-		alignments[1]=PairwiseSequenceAlignment::read_sequence_alignment(input_alignment_string_1, input_alignment_string_2);
-		alignments[2]=PairwiseSequenceAlignment::construct_global_sequence_alignment(sequence_string_from_input_alignment_2, sequence_string_from_atoms_2, scorer);
+		alignments[0]=protein::sequence_tools::PairwiseSequenceAlignment::construct_global_sequence_alignment(sequence_string_from_atoms_1, sequence_string_from_input_alignment_1, scorer);
+		alignments[1]=protein::sequence_tools::PairwiseSequenceAlignment::read_sequence_alignment(input_alignment_string_1, input_alignment_string_2);
+		alignments[2]=protein::sequence_tools::PairwiseSequenceAlignment::construct_global_sequence_alignment(sequence_string_from_input_alignment_2, sequence_string_from_atoms_2, scorer);
 
 		for(int j=0;j<2;j++)
 		{
@@ -376,8 +148,8 @@ void x_renumber_residues_in_inter_atom_contacts(const auxiliaries::CommandLineOp
 		}
 
 		std::clog << "Result alignment:\n";
-		PairwiseSequenceAlignment::print_sequence_alignment(sequence_string_from_atoms_1, sequence_string_from_input_alignment_1, alignments[0], std::clog);
-		PairwiseSequenceAlignment::print_sequence_alignment(sequence_string_from_input_alignment_2, sequence_string_from_atoms_2, alignments[2], std::clog);
+		protein::sequence_tools::PairwiseSequenceAlignment::print_sequence_alignment(sequence_string_from_atoms_1, sequence_string_from_input_alignment_1, alignments[0], std::clog);
+		protein::sequence_tools::PairwiseSequenceAlignment::print_sequence_alignment(sequence_string_from_input_alignment_2, sequence_string_from_atoms_2, alignments[2], std::clog);
 
 		const std::vector< std::pair<protein::ResidueID, std::vector<std::size_t> > > filtered_residue_ids_indices_1=filter_residue_ids_map_by_chain_id(residue_ids_indices_1, chain_name_1);
 		const std::vector< std::pair<protein::ResidueID, std::vector<std::size_t> > > filtered_residue_ids_indices_2=filter_residue_ids_map_by_chain_id(residue_ids_indices_2, chain_name_2);
