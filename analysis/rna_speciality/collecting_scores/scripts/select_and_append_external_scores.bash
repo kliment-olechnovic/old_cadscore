3d054149dc2a37d3aa69bf8c0cb7f23235632034
kliment <kliment@localhost>
2013-06-07 15:21:20 +0300

    Started adding some RNA scoring test.

--- analysis/rna_speciality/collecting_scores/scripts/select_and_append_external_scores.bash
+++ analysis/rna_speciality/collecting_scores/scripts/select_and_append_external_scores.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-INPUT_FILE="./input/external_scores"
-SELECTION_FILE="./output/selected_external_scores"
-MERGED_SCORES_FILE="./output/merged_scores"
+INPUT_FILE="./input/rp_external_scores"
+SELECTION_FILE="./output/rp/selected_external_scores"
+MERGED_SCORES_FILE="./output/rp/merged_scores"
 
 echo "target model ext_rmsd ext_di_all ext_inf_all ext_inf_wc ext_inf_nwc ext_inf_stacking ext_clash_score" > $SELECTION_FILE.uf
 cat $INPUT_FILE | tail -n +2 | awk '{ print $1 "_solution_0 " $1 "_" $2 "_" $3 " " $4 " " $6 " " $8 " " $10 " " $12 " " $14 " " $16 }' | sort -r >> $SELECTION_FILE.uf
@@ -10,10 +10,10 @@ cat $SELECTION_FILE.uf | column -t > $SELECTION_FILE
 rm $SELECTION_FILE.uf
 
 R --vanilla << EOF
-ct=read.table("./output/calculated_scores", header=TRUE, stringsAsFactors=FALSE);
-et=read.table("./output/selected_external_scores", header=TRUE, stringsAsFactors=FALSE);
+ct=read.table("./output/rp/calculated_scores", header=TRUE, stringsAsFactors=FALSE);
+et=read.table("./output/rp/selected_external_scores", header=TRUE, stringsAsFactors=FALSE);
 mt=merge(ct, et);
-write.table(mt, "./output/merged_scores.uf", quote=FALSE, row.names=FALSE, col.names=TRUE);
+write.table(mt, "./output/rp/merged_scores.uf", quote=FALSE, row.names=FALSE, col.names=TRUE);
 EOF
 cat $MERGED_SCORES_FILE.uf | column -t > $MERGED_SCORES_FILE
 rm $MERGED_SCORES_FILE.uf
