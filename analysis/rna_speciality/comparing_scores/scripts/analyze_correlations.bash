4524954ec21b4b6849c6cd33f7a70e5c2c5f2770
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2013-06-09 18:06:56 +0300

    Started adding some RNA scoring test.

--- analysis/rna_speciality/comparing_scores/scripts/analyze_correlations.bash
+++ analysis/rna_speciality/comparing_scores/scripts/analyze_correlations.bash
@@ -3,3 +3,7 @@
 R --vanilla --args ../collecting_scores/output/rp/merged_scores ./output/rp < ./scripts/analyze_correlations.R
 cat ./output/rp/cor_table.txt | column -t > ./output/rp/cor_table_f.txt
 mv ./output/rp/cor_table_f.txt ./output/rp/cor_table.txt
+
+R --vanilla --args ../collecting_scores/output/decoys/calculated_scores ./output/decoys < ./scripts/analyze_correlations.R
+cat ./output/decoys/cor_table.txt | column -t > ./output/decoys/cor_table_f.txt
+mv ./output/decoys/cor_table_f.txt ./output/decoys/cor_table.txt
