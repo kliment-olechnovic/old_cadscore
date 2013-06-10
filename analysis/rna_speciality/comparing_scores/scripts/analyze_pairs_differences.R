e0c1fa6ae30519b1084c2a857302909b1e1be387
kliment <kliment@localhost>
2013-06-10 18:01:08 +0300

    Started adding some RNA scoring test.

--- analysis/rna_speciality/comparing_scores/scripts/analyze_pairs_differences.R
+++ analysis/rna_speciality/comparing_scores/scripts/analyze_pairs_differences.R
@@ -1,13 +1,13 @@
-input_table_file="../collecting_scores/output/decoys/calculated_scores";
-output_directory="./output/decoys";
+input_table_file="../collecting_scores/output/rp/merged_scores";
+output_directory="./output/rp";
 
 t=read.table(input_table_file, header=TRUE, stringsAsFactors=FALSE);
 t=t[which(t$m_res_used/t$t_res_used>0.95),];
+t=t[which(t$target!="4_solution_0"),];
 targets=union(t$target, t$target);
 
-scorename_a="rna_rmsd";
+scorename_a="ext_rmsd";
 scorename_b="SS";
-sel_size=120;
 
 diffs_a=c();
 diffs_b=c();
@@ -17,27 +17,19 @@ rights=c();
 for(target in targets)
 {
 st=t[which(t$target==target),];
-sel_ai=rev(order(st[, scorename_a]))[1:sel_size];
-sel_aj=order(st[, scorename_a])[1:sel_size];
-sel_b=rev(order(st[, scorename_b]))[1:sel_size];
-sti=st[intersect(sel_ai, sel_b),];
-stj=st[intersect(sel_aj, sel_b),];
-if(length(sti[[1]])>0 && length(stj[[1]])>0)
+for(i in 1:length(st[[1]]))
 {
-for(i in 1:length(sti[[1]]))
-{
-  for(j in 1:length(stj[[1]]))
+  for(j in (i+1):length(st[[1]]))
   {
-    diffs_a=c(diffs_a, sti[i, scorename_a]-stj[j, scorename_a]);
-    diffs_b=c(diffs_b, sti[i, scorename_b]-stj[j, scorename_b]);
-    lefts=c(lefts, sti[i, "model"]);
-    rights=c(rights, stj[j, "model"]);
+    diffs_a=c(diffs_a, st[i, scorename_a]-st[j, scorename_a]);
+    diffs_b=c(diffs_b, st[i, scorename_b]-st[j, scorename_b]);
+    lefts=c(lefts, st[i, "model"]);
+    rights=c(rights, st[j, "model"]);
   }
-}
 }
 }
 
 plot(diffs_a, diffs_b);
 
-sel=intersect(which(diffs_a>4), which(abs(diffs_b)<0.01));
+sel=intersect(which(diffs_a>10), which(diffs_b>0));
 data.frame(left=lefts[sel], right=rights[sel]);
