92c72ed06c4167eee6c694bf6c2b15e3fff4c9ec
kliment <kliment@localhost>
2013-06-11 13:54:29 +0300

    Started adding some RNA scoring test.

--- analysis/rna_speciality/comparing_scores/scripts/analyze_pairs_differences.R
+++ analysis/rna_speciality/comparing_scores/scripts/analyze_pairs_differences.R
@@ -1,35 +1,97 @@
-input_table_file="../collecting_scores/output/rp/merged_scores";
-output_directory="./output/rp";
+cst=read.table("../collecting_scores/output/decoys/calculated_scores", header=TRUE, stringsAsFactors=FALSE);
+cst=cst[which(cst$m_res_used/cst$t_res_used>0.95),];
+mst=read.table("../collecting_scores/input/decoys_molprobity_scores", header=TRUE, stringsAsFactors=FALSE);
 
-t=read.table(input_table_file, header=TRUE, stringsAsFactors=FALSE);
-t=t[which(t$m_res_used/t$t_res_used>0.95),];
-t=t[which(t$target!="4_solution_0"),];
+t=merge(cst, mst);
+
+t=t[which(is.finite(t$rna_rmsd)),];
+t=t[which(is.finite(t$rna_inf_norv)),];
+t$rna_di=t$rna_rmsd/t$rna_inf_norv;
+t=t[which(is.finite(t$rna_di)),];
+
 targets=union(t$target, t$target);
 
-scorename_a="ext_rmsd";
-scorename_b="SS";
-
-diffs_a=c();
-diffs_b=c();
-lefts=c();
-rights=c();
+mins_SS=c();
+mins_rna_inf_norv=c();
+diffs_AA=c();
+diffs_SS=c();
+diffs_rna_inf_norv=c();
+diffs_rna_rmsd=c();
+diffs_rna_di=c();
+diffs_MP_clashscore=c();
+diffs_MP_pct_badangles=c();
 
 for(target in targets)
 {
 st=t[which(t$target==target),];
-for(i in 1:length(st[[1]]))
+N=length(st[[1]]);
+st=st[sample(1:N, 50),];
+N=length(st[[1]]);
+for(i in 1:N)
 {
-  for(j in (i+1):length(st[[1]]))
+  for(j in (i+1):N)
   {
-    diffs_a=c(diffs_a, st[i, scorename_a]-st[j, scorename_a]);
-    diffs_b=c(diffs_b, st[i, scorename_b]-st[j, scorename_b]);
-    lefts=c(lefts, st[i, "model"]);
-    rights=c(rights, st[j, "model"]);
+	mins_SS=c(mins_SS, min(st$SS[i], st$SS[j]));
+	mins_rna_inf_norv=c(mins_rna_inf_norv, min(st$rna_inf_norv[i], st$rna_inf_norv[j]));
+	diffs_AA=c(diffs_AA, st$AA[i]-st$AA[j]);
+	diffs_SS=c(diffs_SS, st$SS[i]-st$SS[j]);
+	diffs_rna_inf_norv=c(diffs_rna_inf_norv, st$rna_inf_norv[i]-st$rna_inf_norv[j]);
+	diffs_rna_rmsd=c(diffs_rna_rmsd, st$rna_rmsd[j]-st$rna_rmsd[i]);
+	diffs_rna_di=c(diffs_rna_di, st$rna_di[j]-st$rna_di[i]);
+	diffs_MP_clashscore=c(diffs_MP_clashscore, st$MP_clashscore[j]-st$MP_clashscore[i]);
+	diffs_MP_pct_badangles=c(diffs_MP_pct_badangles, st$MP_pct_badangles[j]-st$MP_pct_badangles[i]);
   }
 }
 }
 
-plot(diffs_a, diffs_b);
+ids=1:length(diffs_AA);
+vs=data.frame(
+  v_AA=sign(diffs_AA)*ids, 
+  v_SS=sign(diffs_SS)*ids, 
+  v_rna_inf_norv=sign(diffs_rna_inf_norv)*ids, 
+  v_rna_rmsd=sign(diffs_rna_rmsd)*ids, 
+  v_rna_di=sign(diffs_rna_di)*ids, 
+  v_MP_clashscore=sign(diffs_MP_clashscore)*ids,
+  v_MP_pct_badangles=sign(diffs_MP_pct_badangles)*ids,
+  m_SS=mins_SS,
+  m_rna_inf_norv=mins_rna_inf_norv,
+  d_AA=diffs_AA, 
+  d_SS=diffs_SS, 
+  d_rna_inf_norv=diffs_rna_inf_norv, 
+  d_rna_rmsd=diffs_rna_rmsd, 
+  d_rna_di=diffs_rna_di, 
+  d_MP_clashscore=diffs_MP_clashscore,
+  d_MP_pct_badangles=diffs_MP_pct_badangles);
 
-sel=intersect(which(diffs_a>10), which(diffs_b>0));
-data.frame(left=lefts[sel], right=rights[sel]);
+##############
+
+svs=vs;
+svs=svs[which(abs(svs$m_SS)>0.8),];
+svs=svs[which(abs(svs$m_rna_inf_norv)>0.8),];
+svs=svs[which(abs(svs$d_MP_clashscore)>sd(t$MP_clashscore)),];
+
+length(setdiff(intersect(svs$v_MP_clashscore, svs$v_SS), svs$v_rna_inf_norv));
+length(setdiff(intersect(svs$v_MP_clashscore, svs$v_rna_inf_norv), svs$v_SS));
+
+length(setdiff(intersect(svs$v_MP_clashscore, svs$v_SS), svs$v_rna_rmsd));
+length(setdiff(intersect(svs$v_MP_clashscore, svs$v_rna_rmsd), svs$v_SS));
+
+length(setdiff(intersect(svs$v_MP_clashscore, svs$v_SS), svs$v_rna_di));
+length(setdiff(intersect(svs$v_MP_clashscore, svs$v_rna_di), svs$v_SS));
+
+##############
+
+svs=vs;
+svs=svs[which(abs(svs$m_SS)>0.8),];
+svs=svs[which(abs(svs$m_rna_inf_norv)>0.8),];
+svs=svs[which(abs(svs$d_MP_pct_badangles)>sd(t$d_MP_pct_badangles)),];
+
+length(setdiff(intersect(svs$v_MP_pct_badangles, svs$v_SS), svs$v_rna_inf_norv));
+length(setdiff(intersect(svs$v_MP_pct_badangles, svs$v_rna_inf_norv), svs$v_SS));
+
+length(setdiff(intersect(svs$v_MP_pct_badangles, svs$v_SS), svs$v_rna_rmsd));
+length(setdiff(intersect(svs$v_MP_pct_badangles, svs$v_rna_rmsd), svs$v_SS));
+
+length(setdiff(intersect(svs$v_MP_pct_badangles, svs$v_SS), svs$v_rna_di));
+length(setdiff(intersect(svs$v_MP_pct_badangles, svs$v_rna_di), svs$v_SS));
+
