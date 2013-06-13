b142d1b2d690f1bc2c0dae98079f3142f7205d0b
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2013-06-13 11:08:09 +0300

    Started adding some RNA scoring test.

--- analysis/rna_speciality/comparing_scores/scripts/analyze_pairs_differences.R
+++ analysis/rna_speciality/comparing_scores/scripts/analyze_pairs_differences.R
@@ -1,13 +1,21 @@
 rt=read.table("./output/decoys/pairs_differences_table.bak", header=TRUE, stringsAsFactors=FALSE);
 
+rt=rt[which(abs(rt$diffs_AA)>0),];
+rt=rt[which(abs(rt$diffs_SS)>0),];
+rt=rt[which(abs(rt$diffs_rna_inf_norv)>0),];
+rt=rt[which(abs(rt$diffs_rna_rmsd)>0),];
+rt=rt[which(abs(rt$diffs_rna_di)>0),];
+rt=rt[which(abs(rt$diffs_GDT_C3)>0),];
+rt=rt[which(abs(rt$diffs_MP_clashscore)>0),];
+rt=rt[which(abs(rt$diffs_MP_pct_badangles)>0),];
+
 t=rt;
 
 mp_signs_diffs=sign(t$diffs_MP_clashscore)-sign(t$diffs_MP_pct_badangles);
 t=t[which(mp_signs_diffs==0),];
 
-t=t[which(t$mins_rna_rmsd<1),];
-t=t[which(abs(t$diffs_MP_clashscore)>1),];
-t=t[which(abs(t$diffs_MP_pct_badangles)>1),];
+t=t[which(t$mins_rna_inf_norv>0.9),];
+#t=t[which(t$maxs_rna_di<5),];
 
 N=length(t[[1]]);
 N
@@ -24,6 +32,16 @@ svs=data.frame(
 		MP_pct_badangles=sign(t$diffs_MP_pct_badangles)*ids);
 
 ##############
+
+length(svs$SS);
+length(intersect(svs$SS, svs$AA));
+length(intersect(svs$SS, svs$rna_inf_norv));
+length(intersect(svs$SS, svs$GDT_C3));
+length(intersect(svs$SS, svs$rna_rmsd));
+length(intersect(svs$SS, svs$rna_di));
+length(intersect(svs$rna_inf_norv, svs$rna_rmsd));
+length(intersect(svs$rna_inf_norv, svs$rna_di));
+length(intersect(svs$rna_rmsd, svs$rna_di));
 
 length(intersect(setdiff(svs$SS, svs$rna_inf_norv), svs$MP_clashscore));
 length(intersect(setdiff(svs$rna_inf_norv, svs$SS), svs$MP_clashscore));
