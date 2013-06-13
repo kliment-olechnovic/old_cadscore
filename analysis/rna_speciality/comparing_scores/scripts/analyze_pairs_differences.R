23df0312ed4a745b2c8a614f4f3c7132be729e04
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2013-06-13 08:36:33 +0300

    Started adding some RNA scoring test.

--- analysis/rna_speciality/comparing_scores/scripts/analyze_pairs_differences.R
+++ analysis/rna_speciality/comparing_scores/scripts/analyze_pairs_differences.R
@@ -1,6 +1,10 @@
 rt=read.table("./output/decoys/pairs_differences_table.bak", header=TRUE, stringsAsFactors=FALSE);
 
 t=rt;
+
+mp_signs_diffs=sign(t$diffs_MP_clashscore)-sign(t$diffs_MP_pct_badangles);
+t=t[which(mp_signs_diffs==0),];
+
 t=t[which(t$mins_rna_rmsd<1),];
 t=t[which(abs(t$diffs_MP_clashscore)>1),];
 t=t[which(abs(t$diffs_MP_pct_badangles)>1),];
@@ -10,13 +14,13 @@ N
 ids=1:N;
 
 svs=data.frame(
-	AA=sign(t$diffs_AA)*ids, 
-	SS=sign(t$diffs_SS)*ids, 
-	rna_inf_norv=sign(t$diffs_rna_inf_norv)*ids, 
-	rna_rmsd=sign(t$diffs_rna_rmsd)*ids, 
-	rna_di=sign(t$diffs_rna_di)*ids, 
-	MP_clashscore=sign(t$diffs_MP_clashscore)*ids,
-	MP_pct_badangles=sign(t$diffs_MP_pct_badangles)*ids);
+		AA=sign(t$diffs_AA)*ids, 
+		SS=sign(t$diffs_SS)*ids, 
+		rna_inf_norv=sign(t$diffs_rna_inf_norv)*ids, 
+		rna_rmsd=sign(t$diffs_rna_rmsd)*ids, 
+		rna_di=sign(t$diffs_rna_di)*ids, 
+		MP_clashscore=sign(t$diffs_MP_clashscore)*ids,
+		MP_pct_badangles=sign(t$diffs_MP_pct_badangles)*ids);
 
 ##############
 
@@ -28,14 +32,3 @@ length(intersect(setdiff(svs$rna_rmsd, svs$SS), svs$MP_clashscore));
 
 length(intersect(setdiff(svs$SS, svs$rna_di), svs$MP_clashscore));
 length(intersect(setdiff(svs$rna_di, svs$SS), svs$MP_clashscore));
-
-##############
-
-length(intersect(setdiff(svs$SS, svs$rna_inf_norv), svs$MP_pct_badangles));
-length(intersect(setdiff(svs$rna_inf_norv, svs$SS), svs$MP_pct_badangles));
-
-length(intersect(setdiff(svs$SS, svs$rna_rmsd), svs$MP_pct_badangles));
-length(intersect(setdiff(svs$rna_rmsd, svs$SS), svs$MP_pct_badangles));
-
-length(intersect(setdiff(svs$SS, svs$rna_di), svs$MP_pct_badangles));
-length(intersect(setdiff(svs$rna_di, svs$SS), svs$MP_pct_badangles));
