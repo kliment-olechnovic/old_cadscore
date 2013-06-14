5f06016813fe3f19f3ce77733da9c8afab3c38df
kliment <kliment@localhost>
2013-06-14 14:06:56 +0300

    Enhanced drawing capabilities - added wire-frame CGO generation.

--- scripts/Voroprot2_print_interfaces_as_PyMol_graphics.bash
+++ scripts/Voroprot2_print_interfaces_as_PyMol_graphics.bash
@@ -85,10 +85,11 @@ SELECTION_COLORING_MODE=""
 COMBINED_RESIDUE_CONTACTS_FILE=""
 COMBINED_RESIDUE_CONTACTS_FILE_USAGE=""
 RESIDUE_GROUPS=""
+DRAW_OUTLINE=""
 OUTPUT_NAMES_PREFIX=""
 OPEN_IN_PYMOL=false
 
-while getopts "hei:f:s:c:d:v:w:g:n:p" OPTION
+while getopts "hei:f:s:c:d:v:w:g:on:p" OPTION
 do
   case $OPTION in
     h)
@@ -128,6 +129,9 @@ do
     g)
       RESIDUE_GROUPS="--groups "$OPTARG
       ;;
+    o)
+      DRAW_OUTLINE="--outline"
+      ;;
     n)
       OUTPUT_NAMES_PREFIX="--output-names-prefix "$OPTARG
       ;;
@@ -161,7 +165,7 @@ SCRIPT_FILE="$TMP_DIR/script.py"
 
 if [ -z "$COMBINED_RESIDUE_CONTACTS_FILE" ]
 then
-  $VOROPROT --mode collect-atoms < "$INPUT_FILE" | $VOROPROT --mode print-inter-chain-interface-graphics $FACE_COLORING_MODE $SELECTION_COLORING_MODE $RESIDUE_GROUPS $OUTPUT_NAMES_PREFIX > "$SCRIPT_FILE"
+  $VOROPROT --mode collect-atoms < "$INPUT_FILE" | $VOROPROT --mode print-inter-chain-interface-graphics $FACE_COLORING_MODE $SELECTION_COLORING_MODE $RESIDUE_GROUPS $OUTPUT_NAMES_PREFIX $DRAW_OUTLINE > "$SCRIPT_FILE"
 else
   ( $VOROPROT --mode collect-atoms < "$INPUT_FILE" ; cat "$COMBINED_RESIDUE_CONTACTS_FILE" ) | $VOROPROT --mode print-inter-chain-interface-graphics --face-coloring $COMBINED_RESIDUE_CONTACTS_FILE_USAGE $SELECTION_COLORING_MODE $RESIDUE_GROUPS $OUTPUT_NAMES_PREFIX > "$SCRIPT_FILE"
 fi
