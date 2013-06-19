858a488fbc211b5d0a7d5762b11381826048999d
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2013-06-19 11:16:19 +0300

    Started adding scripts for the analysis of area summaries.

---
+++ analysis/rna_speciality/collect_area_summaries/scripts/select_areas_by_pdb_ids.bash
@@ -0,0 +1,2 @@
+R --vanilla --args ./output/all_areas ./input/protein_pdb_ids ./output/protein_areas < ./scripts/select_areas_by_pdb_ids.R
+R --vanilla --args ./output/all_areas ./input/rna_pdb_ids ./output/rna_areas < ./scripts/select_areas_by_pdb_ids.R
