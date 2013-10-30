bb652c5b67dfcfa458302ada7fd3909dc961007e
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2013-10-30 14:39:16 +0200

    Improved RNA Molprobity analysis scripts.

--- analysis/rna_speciality/comparing_scores/scripts/analyze_pairs_differences.R
+++ analysis/rna_speciality/comparing_scores/scripts/analyze_pairs_differences.R
@@ -1,7 +1,9 @@
 rt=read.table("./output/decoys/pairs_differences_table.bak", header=TRUE, stringsAsFactors=FALSE);
 
 rt=rt[which(abs(rt$diffs_AA)>0),];
+rt=rt[which(abs(rt$diffs_AS)>0),];
 rt=rt[which(abs(rt$diffs_SS)>0),];
+rt=rt[which(abs(rt$diffs_MM)>0),];
 rt=rt[which(abs(rt$diffs_rna_inf_norv)>0),];
 rt=rt[which(abs(rt$diffs_rna_rmsd)>0),];
 rt=rt[which(abs(rt$diffs_rna_di)>0),];
@@ -14,6 +16,11 @@ mp_signs_diffs=sign(rt$diffs_MP_clashscore)-sign(rt$diffs_MP_pct_badangles);
 rt=rt[which(mp_signs_diffs==0),];
 
 #########################################
+
+cadscore_variants=c("AA", "AS", "SS", "MM");
+
+for(cadscore_variant in cadscore_variants)
+{
 
 used_thresholds=c();
 set_sizes=c();
@@ -31,7 +38,7 @@ for(threshold in thresholds)
 	N=length(t[[1]]);
 	ids=1:N;
 	
-	SS=sign(t$diffs_SS)*ids;
+	CAD_score=sign(t[,paste("diffs_", cadscore_variant, sep="")])*ids;
 	MP_score=sign(t$diffs_MP_clashscore)*ids;
 	
 	ev=data.frame(RNA_INF=sign(t$diffs_rna_inf_norv)*ids, RMSD=sign(t$diffs_rna_rmsd)*ids, RNA_DI=sign(t$diffs_rna_di)*ids, GDT_C3=sign(t$diffs_GDT_C3)*ids);
@@ -41,30 +48,32 @@ for(threshold in thresholds)
 		used_thresholds=c(used_thresholds, threshold);
 		set_sizes=c(set_sizes, N);
 		second_score_names=c(second_score_names, ev_name);
-		subset_names=c(subset_names, paste("1. MP and CAD_SS and ", ev_name, sep=""));
-		subset_sizes=c(subset_sizes, length(intersect(MP_score, intersect(SS, ev[, ev_name]))));
+		subset_names=c(subset_names, paste("1. MP and CAD-score and ", ev_name, sep=""));
+		subset_sizes=c(subset_sizes, length(intersect(MP_score, intersect(CAD_score, ev[, ev_name]))));
 		
 		used_thresholds=c(used_thresholds, threshold);
 		set_sizes=c(set_sizes, N);
 		second_score_names=c(second_score_names, ev_name);
-		subset_names=c(subset_names, paste("2. MP and CAD_SS without ", ev_name, sep=""));
-		subset_sizes=c(subset_sizes, length(intersect(MP_score, setdiff(SS, ev[, ev_name]))));
+		subset_names=c(subset_names, paste("2. MP and CAD-score without ", ev_name, sep=""));
+		subset_sizes=c(subset_sizes, length(intersect(MP_score, setdiff(CAD_score, ev[, ev_name]))));
 		
 		used_thresholds=c(used_thresholds, threshold);
 		set_sizes=c(set_sizes, N);
 		second_score_names=c(second_score_names, ev_name);
-		subset_names=c(subset_names, paste("3. MP and ", ev_name, " without CAD_SS", sep=""));
-		subset_sizes=c(subset_sizes, length(intersect(MP_score, setdiff(ev[, ev_name], SS))));
+		subset_names=c(subset_names, paste("3. MP and ", ev_name, " without CAD-score", sep=""));
+		subset_sizes=c(subset_sizes, length(intersect(MP_score, setdiff(ev[, ev_name], CAD_score))));
 		
 		used_thresholds=c(used_thresholds, threshold);
 		set_sizes=c(set_sizes, N);
 		second_score_names=c(second_score_names, ev_name);
-		subset_names=c(subset_names, paste("4. MP without CAD_SS without ", ev_name, sep=""));
-		subset_sizes=c(subset_sizes, length(setdiff(MP_score, union(SS, ev[, ev_name]))));
+		subset_names=c(subset_names, paste("4. MP without CAD-score without ", ev_name, sep=""));
+		subset_sizes=c(subset_sizes, length(setdiff(MP_score, union(CAD_score, ev[, ev_name]))));
 	}
 }
 
 fractions=subset_sizes/set_sizes;
 
 result=data.frame(min_RNA_INF=used_thresholds, set_size=set_sizes, second_score_name=second_score_names, subset_name=subset_names, subset_size=subset_sizes, fraction=fractions);
-write.table(result, "./output/decoys/molprobity_results.txt", quote=TRUE, row.names=FALSE);
+write.table(result, paste("./output/decoys/molprobity_results_", cadscore_variant, ".txt", sep=""), quote=TRUE, row.names=FALSE);
+
+}
--- analysis/rna_speciality/comparing_scores/scripts/collect_pairs_differences.R
+++ analysis/rna_speciality/comparing_scores/scripts/collect_pairs_differences.R
@@ -8,7 +8,7 @@ t=t[which(is.finite(t$rna_di)),];
 
 targets=union(sort(t$target), sort(t$target));
 
-sample_size=400;
+sample_size=200;
 max_vector_size=0;
 for(target in targets)
 {
@@ -20,14 +20,18 @@ for(target in targets)
 }
 
 mins_AA=numeric(max_vector_size);
+mins_AS=numeric(max_vector_size);
 mins_SS=numeric(max_vector_size);
+mins_MM=numeric(max_vector_size);
 mins_rna_inf_norv=numeric(max_vector_size);
 mins_rna_rmsd=numeric(max_vector_size);
 maxs_rna_rmsd=numeric(max_vector_size);
 maxs_rna_di=numeric(max_vector_size);
 mins_GDT_C3=numeric(max_vector_size);
 diffs_AA=numeric(max_vector_size);
+diffs_AS=numeric(max_vector_size);
 diffs_SS=numeric(max_vector_size);
+diffs_MM=numeric(max_vector_size);
 diffs_rna_inf_norv=numeric(max_vector_size);
 diffs_rna_rmsd=numeric(max_vector_size);
 diffs_rna_di=numeric(max_vector_size);
@@ -49,14 +53,18 @@ for(target in targets)
 			for(j in (i+1):N)
 			{
 				mins_AA[k]=min(st$AA[i], st$AA[j]);
+				mins_AS[k]=min(st$AS[i], st$AS[j]);
 				mins_SS[k]=min(st$SS[i], st$SS[j]);
+				mins_MM[k]=min(st$MM[i], st$MM[j]);
 				mins_rna_inf_norv[k]=st$rna_inf_norv[j];
 				mins_rna_rmsd[k]=min(st$rna_rmsd[i], st$rna_rmsd[j]);
 				maxs_rna_rmsd[k]=max(st$rna_rmsd[i], st$rna_rmsd[j]);
 				maxs_rna_di[k]=max(st$rna_di[i], st$rna_di[j]);
 				mins_GDT_C3[k]=min(st$GDT_C3[i], st$GDT_C3[j]);
 				diffs_AA[k]=st$AA[i]-st$AA[j];
+				diffs_AS[k]=st$AS[i]-st$AS[j];
 				diffs_SS[k]=st$SS[i]-st$SS[j];
+				diffs_MM[k]=st$MM[i]-st$MM[j];
 				diffs_rna_inf_norv[k]=st$rna_inf_norv[i]-st$rna_inf_norv[j];
 				diffs_rna_rmsd[k]=st$rna_rmsd[j]-st$rna_rmsd[i];
 				diffs_rna_di[k]=st$rna_di[j]-st$rna_di[i];
@@ -71,14 +79,18 @@ for(target in targets)
 
 pdt=data.frame(
 	mins_AA=mins_AA,
+	mins_AS=mins_AS,
 	mins_SS=mins_SS,
+	mins_MM=mins_MM,
 	mins_rna_inf_norv=mins_rna_inf_norv,
 	mins_rna_rmsd=mins_rna_rmsd,
 	maxs_rna_rmsd=maxs_rna_rmsd,
 	maxs_rna_di=maxs_rna_di,
 	mins_GDT_C3=mins_GDT_C3,
-	diffs_AA=diffs_AA, 
-	diffs_SS=diffs_SS, 
+	diffs_AA=diffs_AA,
+	diffs_AS=diffs_AS,
+	diffs_SS=diffs_SS,
+	diffs_MM=diffs_MM,
 	diffs_rna_inf_norv=diffs_rna_inf_norv, 
 	diffs_rna_rmsd=diffs_rna_rmsd, 
 	diffs_rna_di=diffs_rna_di,
