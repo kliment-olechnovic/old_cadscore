ac874c17a688cdd23a7ccc24e286feca59d15cfe
kliment <kliment@localhost>
2013-10-31 09:55:33 +0200

    Improved RNA Molprobity analysis scripts.

--- analysis/rna_speciality/comparing_scores/scripts/analyze_pairs_differences.R
+++ analysis/rna_speciality/comparing_scores/scripts/analyze_pairs_differences.R
@@ -10,7 +10,6 @@ rt=rt[which(abs(rt$diffs_rna_di)>0),];
 rt=rt[which(abs(rt$diffs_GDT_C3)>0),];
 rt=rt[which(abs(rt$diffs_MP_clashscore)>0),];
 rt=rt[which(abs(rt$diffs_MP_pct_badangles)>0),];
-rt=rt[which(rt$maxs_rna_rmsd<5),];
 
 mp_signs_diffs=sign(rt$diffs_MP_clashscore)-sign(rt$diffs_MP_pct_badangles);
 rt=rt[which(mp_signs_diffs==0),];
@@ -22,18 +21,26 @@ cadscore_variants=c("AA", "AS", "SS", "MM");
 for(cadscore_variant in cadscore_variants)
 {
 
-used_thresholds=c();
+used_rmsd_thresholds=c();
+used_rna_inf_thresholds=c();
 set_sizes=c();
 second_score_names=c();
 subset_names=c();
 subset_sizes=c();
 
-thresholds=seq(0.1, 0.90, 0.01);
+rmsd_thresholds=seq(2, 6, 0.5);
+rmsd_thresholds=c(rmsd_thresholds, 1000);
 
-for(threshold in thresholds)
+rna_inf_thresholds=seq(0.3, 0.9, 0.03);
+
+for(rmsd_threshold in rmsd_thresholds)
+{
+
+for(rna_inf_threshold in rna_inf_thresholds)
 {
 	t=rt;
-	t=t[which(t$mins_rna_inf_norv>threshold),];
+	t=t[which(t$mins_rna_inf_norv>rna_inf_threshold),];
+	t=t[which(t$maxs_rna_rmsd<rmsd_threshold),];
 	
 	N=length(t[[1]]);
 	ids=1:N;
@@ -45,25 +52,29 @@ for(threshold in thresholds)
 	
 	for(ev_name in colnames(ev))
 	{
-		used_thresholds=c(used_thresholds, threshold);
+		used_rmsd_thresholds=c(used_rmsd_thresholds, rmsd_threshold);
+		used_rna_inf_thresholds=c(used_rna_inf_thresholds, rna_inf_threshold);
 		set_sizes=c(set_sizes, N);
 		second_score_names=c(second_score_names, ev_name);
 		subset_names=c(subset_names, paste("1. MP and CAD-score and ", ev_name, sep=""));
 		subset_sizes=c(subset_sizes, length(intersect(MP_score, intersect(CAD_score, ev[, ev_name]))));
 		
-		used_thresholds=c(used_thresholds, threshold);
+		used_rmsd_thresholds=c(used_rmsd_thresholds, rmsd_threshold);
+		used_rna_inf_thresholds=c(used_rna_inf_thresholds, rna_inf_threshold);
 		set_sizes=c(set_sizes, N);
 		second_score_names=c(second_score_names, ev_name);
 		subset_names=c(subset_names, paste("2. MP and CAD-score without ", ev_name, sep=""));
 		subset_sizes=c(subset_sizes, length(intersect(MP_score, setdiff(CAD_score, ev[, ev_name]))));
 		
-		used_thresholds=c(used_thresholds, threshold);
+		used_rmsd_thresholds=c(used_rmsd_thresholds, rmsd_threshold);
+		used_rna_inf_thresholds=c(used_rna_inf_thresholds, rna_inf_threshold);
 		set_sizes=c(set_sizes, N);
 		second_score_names=c(second_score_names, ev_name);
 		subset_names=c(subset_names, paste("3. MP and ", ev_name, " without CAD-score", sep=""));
 		subset_sizes=c(subset_sizes, length(intersect(MP_score, setdiff(ev[, ev_name], CAD_score))));
 		
-		used_thresholds=c(used_thresholds, threshold);
+		used_rmsd_thresholds=c(used_rmsd_thresholds, rmsd_threshold);
+		used_rna_inf_thresholds=c(used_rna_inf_thresholds, rna_inf_threshold);
 		set_sizes=c(set_sizes, N);
 		second_score_names=c(second_score_names, ev_name);
 		subset_names=c(subset_names, paste("4. MP without CAD-score without ", ev_name, sep=""));
@@ -71,9 +82,11 @@ for(threshold in thresholds)
 	}
 }
 
+}
+
 fractions=subset_sizes/set_sizes;
 
-result=data.frame(min_RNA_INF=used_thresholds, set_size=set_sizes, second_score_name=second_score_names, subset_name=subset_names, subset_size=subset_sizes, fraction=fractions);
+result=data.frame(max_RMSD=used_rmsd_thresholds, min_RNA_INF=used_rna_inf_thresholds, set_size=set_sizes, second_score_name=second_score_names, subset_name=subset_names, subset_size=subset_sizes, fraction=fractions);
 write.table(result, paste("./output/decoys/molprobity_results_", cadscore_variant, ".txt", sep=""), quote=TRUE, row.names=FALSE);
 
 }
