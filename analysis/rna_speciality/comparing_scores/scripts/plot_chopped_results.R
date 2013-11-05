9ca54ca7eef927c9b92f33174dbb100bb00c0b77
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2013-11-05 12:38:23 +0200

    Started chopping analysis.

--- analysis/rna_speciality/comparing_scores/scripts/plot_chopped_results.R
+++ analysis/rna_speciality/comparing_scores/scripts/plot_chopped_results.R
@@ -4,11 +4,20 @@ t=read.table(input_table_file, header=TRUE, stringsAsFactors=FALSE);
 
 models=union(t$model, t$model);
 
-score_names=c("AA", "SS", "rna_inf_norv");
+score_names=c("AA", "SS", "TM_score", "rna_inf_norv");
 mishandles=rep(0, length(score_names));
 
 for(score_name in score_names)
 {
+	plot(x=c(0, max(t$chopped)), y=c(0, 1), type="n", main=paste("Chopped models ",  score_name, " scores", sep=""), xlab="One end chopped percents", ylab="Score");
+	
+	for(model in models)
+	{
+		st=t[which(t$model==model),];
+		st=st[order(st$chopped),];
+		points(x=st$chopped, y=st[,score_name], type="l", col="black", lwd=0.1);
+	}
+
 	plot(x=c(0, max(t$chopped)), y=c(0, 1), type="n", main=paste("Chopped models badly adjusted ",  score_name, " scores", sep=""), xlab="One end chopped percents", ylab="Score");
 	
 	for(model in models)
