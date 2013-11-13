773df41a7e0effa346b1eb6dc7fb282237cb9226
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2013-11-13 10:58:57 +0200

    Added rp chopped scores.

---
+++ analysis/rna_speciality/comparing_scores/scripts/plot_chopped_results_for_rp.R
@@ -0,0 +1,56 @@
+t=read.table("../collecting_scores/output/rp/calculated_scores_for_chopped", header=TRUE, stringsAsFactors=FALSE);
+
+models=union(t$model, t$model);
+
+score_names=c("AA", "SS", "TM_score", "rna_inf_norv");
+mishandles=rep(0, length(score_names));
+
+for(score_name in score_names)
+{
+	plot(x=c(0, max(t$chopped)), y=c(0, 1), type="n", main=paste("Chopped models ",  score_name, " scores", sep=""), xlab="One end chopped percents", ylab="Score");
+	
+	for(model in models)
+	{
+		st=t[which(t$model==model),];
+		st=st[order(st$chopped),];
+		points(x=st$chopped, y=st[,score_name], type="l", col="black", lwd=0.1);
+	}
+
+	plot(x=c(0, max(t$chopped)), y=c(0, 1), type="n", main=paste("Chopped models badly adjusted ",  score_name, " scores", sep=""), xlab="One end chopped percents", ylab="Score");
+	
+	for(model in models)
+	{
+		st=t[which(t$model==model),];
+		st=st[order(st$chopped),];
+		n=length(st[[1]]);
+		broken=FALSE;
+		for(i in 1:(n-1))
+		{
+			j=i+1;
+			if(st[i,score_name]<st[j,score_name])
+			{
+				broken=TRUE
+			}
+		}
+		if(broken==TRUE)
+		{
+			for(i in 1:(n-1))
+			{
+				j=i+1;
+				col="blue";
+				lwd=0.1;
+				if(st[i,score_name]<st[j,score_name])
+				{
+					col="red";
+					lwd=2.0;
+				}
+				points(x=st$chopped[c(i,j)], y=st[c(i,j),score_name], type="l", col=col, lwd=lwd);
+			}
+			mishandles[which(score_names==score_name)]=mishandles[which(score_names==score_name)]+1;
+		}
+	}
+}
+
+length(models);
+score_names
+mishandles
